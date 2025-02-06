a3244cbb1ae427ff95b8a166ef5bd2b7e66adf4f
Markus Göllnitz <camelcasenick@bewares.it>
2025-02-06 14:18:23 +0100

    libview: reduce code area ignoring deprecation warnings
    Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

--- libview/pps-view.c
+++ libview/pps-view.c
@@ -48,8 +48,6 @@
 #include "pps-view-marshal.h"
 #include "pps-view-page.h"
 
-G_GNUC_BEGIN_IGNORE_DEPRECATIONS
-
 enum {
 	SIGNAL_SCROLL,
 	SIGNAL_HANDLE_LINK,
@@ -2415,6 +2413,8 @@ _pps_view_set_focused_element (PpsView *view,
 }
 
 /*** Forms ***/
+G_GNUC_BEGIN_IGNORE_DEPRECATIONS
+
 static PpsMapping *
 get_form_field_mapping_at_location (PpsView *view,
                                     gdouble x,
@@ -3033,6 +3033,8 @@ pps_view_handle_form_field (PpsView *view,
 	if (PPS_IS_FORM_FIELD_BUTTON (field))
 		pps_view_form_field_button_toggle (view, field);
 }
+
+G_GNUC_END_IGNORE_DEPRECATIONS
 
 /* Media */
 static PpsMapping *
@@ -4177,6 +4179,7 @@ pps_view_scroll_event (GtkEventControllerScroll *self, gdouble dx, gdouble dy, G
 }
 
 /* This is based on the deprecated function gtk_draw_insertion_cursor. */
+G_GNUC_BEGIN_IGNORE_DEPRECATIONS
 static void
 draw_caret_cursor (PpsView *view,
                    GtkSnapshot *snapshot)
@@ -4202,6 +4205,7 @@ draw_caret_cursor (PpsView *view,
 
 	gtk_snapshot_restore (snapshot);
 }
+G_GNUC_END_IGNORE_DEPRECATIONS
 
 static gboolean
 should_draw_caret_cursor (PpsView *view)
@@ -4239,12 +4243,14 @@ draw_focus (PpsView *view,
 	if (!pps_view_get_focused_area (view, &rect))
 		return;
 
+	G_GNUC_BEGIN_IGNORE_DEPRECATIONS
 	gtk_snapshot_render_focus (snapshot,
 	                           gtk_widget_get_style_context (widget),
 	                           rect.x,
 	                           rect.y,
 	                           rect.width,
 	                           rect.height);
+	G_GNUC_END_IGNORE_DEPRECATIONS
 }
 
 static void
@@ -8228,5 +8234,3 @@ pps_view_stop_signature_rect (PpsView *view)
 	g_signal_emit (view, signals[SIGNAL_SIGNATURE_RECT], 0, start->page_index, &rect);
 	gtk_widget_queue_draw (GTK_WIDGET (view));
 }
-
-G_GNUC_END_IGNORE_DEPRECATIONS
