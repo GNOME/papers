GNOME/papers

commit 5f932dd4e0077fd6b90c979520a9452b5ec1b004

libdocument: Remove refcount from PpsBackendInfo
It's no longer useful at all.

Signed-off-by: Qiu Wenbo <qiuwenbo@kylinos.com.cn>

libdocument/pps-document-factory.c
@@ -159,8 +159,8 @@ pps_document_factory_new_document_for_mime_type (const gchar *mime_type,
 	document = g_object_new (backend_type, NULL);
 
 	g_object_set_data_full (G_OBJECT (document), BACKEND_DATA_KEY,
-	                        _pps_backend_info_ref (info),
-	                        (GDestroyNotify) _pps_backend_info_unref);
+	                        info,
+	                        (GDestroyNotify) _pps_backend_info_free);
 
 	return document;
 }
@@ -269,7 +269,7 @@ _pps_document_factory_init (void)
 void
 _pps_document_factory_shutdown (void)
 {
-	g_clear_list (&pps_backends_list, (GDestroyNotify) _pps_backend_info_unref);
+	g_clear_list (&pps_backends_list, (GDestroyNotify) _pps_backend_info_free);
 
 	g_clear_pointer (&pps_module_hash, g_hash_table_unref);
 	g_clear_pointer (&pps_backends_dir, g_free);

libdocument/pps-backend-info.c
@@ -24,43 +24,16 @@
 #define PPS_BACKENDS_GROUP "Papers Backend"
 #define PPS_BACKENDS_EXTENSION ".papers-backend"
 
-/*
- * _pps_backend_info_free:
- * @info:
- *
- * Increases refcount of @info by 1.
- */
-PpsBackendInfo *
-_pps_backend_info_ref (PpsBackendInfo *info)
-{
-	g_return_val_if_fail (info != NULL, NULL);
-	g_return_val_if_fail (info->ref_count >= 1, NULL);
-
-	g_atomic_int_inc (&info->ref_count);
-	return info;
-}
-
-/*
- * _pps_backend_info_free:
- * @info:
- *
- * Decreases refcount of @info by 1, and frees @info if the refcount reaches 0.
- */
 void
-_pps_backend_info_unref (PpsBackendInfo *info)
+_pps_backend_info_free (PpsBackendInfo *info)
 {
 	if (info == NULL)
-		return;
-
-	g_return_if_fail (info->ref_count >= 1);
-
-	if (!g_atomic_int_dec_and_test (&info->ref_count))
 		return;
 
 	g_free (info->module_name);
 	g_free (info->type_desc);
 	g_strfreev (info->mime_types);
-	g_slice_free (PpsBackendInfo, info);
+	g_free (info);
 }
 
 /**
@@ -83,8 +56,7 @@ _pps_backend_info_new_from_file (const char *file,
 	if (!g_key_file_load_from_file (backend_file, file, G_KEY_FILE_NONE, error))
 		goto err;
 
-	info = g_slice_new0 (PpsBackendInfo);
-	info->ref_count = 1;
+	info = g_new0 (PpsBackendInfo, 1);
 
 	info->module_name = g_key_file_get_string (backend_file, PPS_BACKENDS_GROUP,
 	                                           "Module", error);
@@ -110,7 +82,6 @@ _pps_backend_info_new_from_file (const char *file,
 
 err:
 	g_key_file_free (backend_file);
-	_pps_backend_info_unref (info);
 	return NULL;
 }
 

libdocument/pps-backend-info.h
@@ -30,19 +30,14 @@ G_BEGIN_DECLS
 typedef struct _PpsBackendInfo PpsBackendInfo;
 
 struct _PpsBackendInfo {
-	/* These two fields must be first for API/ABI compat with PpsTypeInfo */
 	gchar *type_desc;
 	gchar **mime_types;
-
-	volatile int ref_count;
 
 	gchar *module_name;
 	gboolean resident;
 };
 
-PpsBackendInfo *_pps_backend_info_ref (PpsBackendInfo *info);
-
-void _pps_backend_info_unref (PpsBackendInfo *info);
+void _pps_backend_info_free (PpsBackendInfo *info);
 
 GList *_pps_backend_info_load_from_dir (const char *path);
 

commit c26a1c9e661314adb656a5b93f4f0fed13344ae3

libdocument: document-factory: do not automatically unfree backend info
When a document is finalised, its backend info should not
be freed as the backend info is still owned by the
document factory.

Otherwise, this would lead to doubly freeing the backend
info.

Fixes: 5f932dd4e0077fd6b90c979520a9452b5ec1b004 ("libdocument: Remove refcount from PpsBackendInfo")

Closes: https://gitlab.gnome.org/GNOME/Incubator/papers/-/issues/361

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

libdocument/pps-document-factory.c
@@ -154,9 +154,7 @@ pps_document_factory_new_document_for_mime_type (const gchar *mime_type,
 
 	document = g_object_new (backend_type, NULL);
 
-	g_object_set_data_full (G_OBJECT (document), BACKEND_DATA_KEY,
-	                        info,
-	                        (GDestroyNotify) _pps_backend_info_free);
+	g_object_set_data (G_OBJECT (document), BACKEND_DATA_KEY, info);
 
 	return document;
 }