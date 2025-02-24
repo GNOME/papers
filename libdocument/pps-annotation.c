GNOME/papers

commit b57aa47d660e430dd487e4f2d4578e5318999c07

libdocument: Add two hidden and border-width properties to annotations
The hidden flag to control visibility and the border width. The hidden flag
is saved by poppler, the border width is not ATM. It is enabled for free
text annots in future commits (should be generalized to other annots if useful).

libdocument/pps-annotation.c
@@ -35,7 +35,9 @@ typedef struct
 	gchar *name;
 	gchar *modified;
 	GdkRGBA rgba;
+	gboolean hidden;
 	PpsRectangle area;
+	gdouble border_width;
 } PpsAnnotationPrivate;
 
 G_DEFINE_ABSTRACT_TYPE_WITH_PRIVATE (PpsAnnotation, pps_annotation, G_TYPE_OBJECT);
@@ -117,7 +119,9 @@ enum {
 	PROP_ANNOT_NAME,
 	PROP_ANNOT_MODIFIED,
 	PROP_ANNOT_RGBA,
-	PROP_ANNOT_AREA
+	PROP_ANNOT_AREA,
+	PROP_ANNOT_HIDDEN,
+	PROP_ANNOT_BORDER_WIDTH
 };
 
 /* PpsAnnotationMarkup */
@@ -202,6 +206,11 @@ pps_annotation_set_property (GObject *object,
 	case PROP_ANNOT_AREA:
 		pps_annotation_set_area (annot, g_value_get_boxed (value));
 		break;
+	case PROP_ANNOT_HIDDEN:
+		pps_annotation_set_hidden (annot, g_value_get_boolean (value));
+		break;
+	case PROP_ANNOT_BORDER_WIDTH:
+		pps_annotation_set_border_width (annot, g_value_get_double (value));
 	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
 	}
@@ -232,6 +241,11 @@ pps_annotation_get_property (GObject *object,
 	case PROP_ANNOT_AREA:
 		g_value_set_boxed (value, &priv->area);
 		break;
+	case PROP_ANNOT_HIDDEN:
+		g_value_set_boolean (value, priv->hidden);
+		break;
+	case PROP_ANNOT_BORDER_WIDTH:
+		g_value_set_double (value, priv->border_width);
 	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
 	}
@@ -308,6 +322,39 @@ pps_annotation_class_init (PpsAnnotationClass *klass)
 	                                                     PPS_TYPE_RECTANGLE,
 	                                                     G_PARAM_READWRITE |
 	                                                         G_PARAM_STATIC_STRINGS));
+
+	/**
+	 * PpsAnnotation:border-width:
+	 *
+	 * The border width of the annotation. This is only partially implemented, as there is no way
+	 * to set the color. Thus, it may only be used for padding at this moment.
+	 *
+	 * Since: 48.0
+	 */
+	g_object_class_install_property (g_object_class,
+	                                 PROP_ANNOT_BORDER_WIDTH,
+	                                 g_param_spec_double ("border-width",
+	                                                      "Border Width",
+	                                                      "The annotation border width",
+	                                                      0., G_MAXDOUBLE, 0.,
+	                                                      G_PARAM_READWRITE |
+	                                                          G_PARAM_STATIC_STRINGS));
+
+	/**
+	 * PpsAnnotation:hidden:
+	 *
+	 * A flag to hide an annotation from the view.
+	 *
+	 * Since: 48.0
+	 */
+	g_object_class_install_property (g_object_class,
+	                                 PROP_ANNOT_BORDER_WIDTH,
+	                                 g_param_spec_boolean ("hidden",
+	                                                       "Hidden Flag",
+	                                                       "Whether the annotation is hidden or not",
+	                                                       FALSE,
+	                                                       G_PARAM_READWRITE |
+	                                                           G_PARAM_STATIC_STRINGS));
 }
 
 PpsAnnotationType
@@ -670,6 +717,82 @@ pps_annotation_set_area (PpsAnnotation *annot,
 		g_object_notify (G_OBJECT (annot), "area");
 
 	return TRUE;
+}
+
+/**
+ * pps_annotation_set_hidden:
+ * @annot: a #PpsAnnotation
+ * @hidden: a boolean
+ *
+ * Set whether the annotation is hidden or not.
+ *
+ * Returns: %TRUE if the visibility of the annotation has been changed, %FALSE otherwise
+ *
+ * Since: 48.0
+ */
+gboolean
+pps_annotation_set_hidden (PpsAnnotation *annot, const gboolean hidden)
+{
+	PpsAnnotationPrivate *priv = GET_ANNOT_PRIVATE (annot);
+	if (priv->hidden != hidden) {
+		priv->hidden = hidden;
+		g_object_notify (G_OBJECT (annot), "hidden");
+		return TRUE;
+	}
+	return FALSE;
+}
+
+/**
+ * pps_annotation_get_hidden:
+ * @annot: a #PpsAnnotation
+ *
+ * Gets the hidden flag of @annot, i.e. whether it is hidden or not.
+ *
+ * Since: 48.0
+ */
+gboolean
+pps_annotation_get_hidden (PpsAnnotation *annot)
+{
+	PpsAnnotationPrivate *priv = GET_ANNOT_PRIVATE (annot);
+	return priv->hidden;
+}
+
+/**
+ * pps_annotation_set_border_width:
+ * @annot: a #PpsAnnotation
+ * @width: double
+ *
+ * Set the area of the annotation to @area.
+ *
+ * Returns: %TRUE if the border width has been changed, %FALSE otherwise
+ *
+ * Since: 48.0
+ */
+gboolean
+pps_annotation_set_border_width (PpsAnnotation *annot, const gdouble width)
+{
+	PpsAnnotationPrivate *priv = GET_ANNOT_PRIVATE (annot);
+	if (priv->border_width != width) {
+		priv->border_width = width;
+		g_object_notify (G_OBJECT (annot), "border-width");
+		return TRUE;
+	}
+	return FALSE;
+}
+
+/**
+ * pps_annotation_get_border_width:
+ * @annot: a #PpsAnnotation
+ *
+ * Gets the border width of @annot.
+ *
+ * Since: 48.0
+ */
+gdouble
+pps_annotation_get_border_width (PpsAnnotation *annot)
+{
+	PpsAnnotationPrivate *priv = GET_ANNOT_PRIVATE (annot);
+	return priv->border_width;
 }
 
 /* PpsAnnotationMarkup */

libdocument/backend/pdf/pps-poppler.c
@@ -3271,6 +3271,15 @@ pdf_document_annotations_save_annotation (PpsDocumentAnnotations *document_annot
 		poppler_annot_set_rectangle (poppler_annot, &poppler_rect);
 	}
 
+	if (mask & PPS_ANNOTATIONS_SAVE_HIDDEN) {
+		PopplerAnnotFlag flag = poppler_annot_get_flags (poppler_annot);
+		flag = flag & ~POPPLER_ANNOT_FLAG_HIDDEN;
+		if (pps_annotation_get_hidden (annot)) {
+			flag = flag | POPPLER_ANNOT_FLAG_HIDDEN;
+		}
+		poppler_annot_set_flags (poppler_annot, flag);
+	}
+
 	if (PPS_IS_ANNOTATION_MARKUP (annot)) {
 		PpsAnnotationMarkup *pps_markup = PPS_ANNOTATION_MARKUP (annot);
 		PopplerAnnotMarkup *markup = POPPLER_ANNOT_MARKUP (poppler_annot);

libdocument/pps-annotation.h
@@ -120,6 +120,16 @@ PPS_PUBLIC
 gboolean pps_annotation_set_modified (PpsAnnotation *annot,
                                       const gchar *modified);
 PPS_PUBLIC
+gboolean pps_annotation_set_hidden (PpsAnnotation *annot,
+                                    const gboolean hidden);
+PPS_PUBLIC
+gboolean pps_annotation_get_hidden (PpsAnnotation *annot);
+PPS_PUBLIC
+gboolean pps_annotation_set_border_width (PpsAnnotation *annot,
+                                          const gdouble width);
+PPS_PUBLIC
+gdouble pps_annotation_get_border_width (PpsAnnotation *annot);
+PPS_PUBLIC
 gboolean pps_annotation_set_modified_from_time_t (PpsAnnotation *annot,
                                                   time_t utime);
 PPS_PUBLIC

libdocument/pps-document-annotations.h
@@ -43,25 +43,26 @@ typedef enum {
 	PPS_ANNOTATIONS_SAVE_CONTENTS = 1 << 0,
 	PPS_ANNOTATIONS_SAVE_COLOR = 1 << 1,
 	PPS_ANNOTATIONS_SAVE_AREA = 1 << 2,
+	PPS_ANNOTATIONS_SAVE_HIDDEN = 1 << 3,
 
 	/* Markup Annotations */
-	PPS_ANNOTATIONS_SAVE_LABEL = 1 << 3,
-	PPS_ANNOTATIONS_SAVE_OPACITY = 1 << 4,
-	PPS_ANNOTATIONS_SAVE_POPUP_RECT = 1 << 5,
-	PPS_ANNOTATIONS_SAVE_POPUP_IS_OPEN = 1 << 6,
+	PPS_ANNOTATIONS_SAVE_LABEL = 1 << 4,
+	PPS_ANNOTATIONS_SAVE_OPACITY = 1 << 5,
+	PPS_ANNOTATIONS_SAVE_POPUP_RECT = 1 << 6,
+	PPS_ANNOTATIONS_SAVE_POPUP_IS_OPEN = 1 << 7,
 
 	/* Text Annotations */
-	PPS_ANNOTATIONS_SAVE_TEXT_IS_OPEN = 1 << 7,
-	PPS_ANNOTATIONS_SAVE_TEXT_ICON = 1 << 8,
+	PPS_ANNOTATIONS_SAVE_TEXT_IS_OPEN = 1 << 8,
+	PPS_ANNOTATIONS_SAVE_TEXT_ICON = 1 << 9,
 
 	/* Attachment Annotations */
-	PPS_ANNOTATIONS_SAVE_ATTACHMENT = 1 << 9,
+	PPS_ANNOTATIONS_SAVE_ATTACHMENT = 1 << 10,
 
 	/* Text Markup Annotations */
-	PPS_ANNOTATIONS_SAVE_TEXT_MARKUP_TYPE = 1 << 10,
+	PPS_ANNOTATIONS_SAVE_TEXT_MARKUP_TYPE = 1 << 11,
 
 	/* Save all */
-	PPS_ANNOTATIONS_SAVE_ALL = (1 << 11) - 1
+	PPS_ANNOTATIONS_SAVE_ALL = (1 << 12) - 1
 } PpsAnnotationsSaveMask;
 
 typedef enum {

commit faa1d4ea423c4f6633f16e64f1ca8022efdb2803

libdocument: annotation: add missing break in case
Fixes: b57aa47d660e430dd487e4f2d4578e5318999c07 ("libdocument: Add two hidden and border-width properties to annotations")

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

libdocument/pps-annotation.c
@@ -242,6 +242,7 @@ pps_annotation_set_property (GObject *object,
 		break;
 	case PROP_ANNOT_BORDER_WIDTH:
 		pps_annotation_set_border_width (annot, g_value_get_double (value));
+		break;
 	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
 	}
@@ -277,6 +278,7 @@ pps_annotation_get_property (GObject *object,
 		break;
 	case PROP_ANNOT_BORDER_WIDTH:
 		g_value_set_double (value, priv->border_width);
+		break;
 	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
 	}