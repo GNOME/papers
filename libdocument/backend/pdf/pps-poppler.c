GNOME/papers

commit 7112994b9dac7e306c9fe7a1d8bddf91c6d3033c

libdocument, backends: Add the free text annotation type
Add the free text annotation type to libdocument plus loading/saving free text annotations in the poppler backend.

libdocument/backend/pdf/pps-poppler.c
@@ -2549,6 +2549,12 @@ poppler_annot_color_to_gdk_rgba (PopplerAnnot *poppler_annot, GdkRGBA *color)
 	PopplerColor *poppler_color;
 
 	poppler_color = poppler_annot_get_color (poppler_annot);
+#ifdef HAVE_FREE_TEXT
+	if (POPPLER_IS_ANNOT_FREE_TEXT (poppler_annot) && !poppler_color) {
+		color->alpha = 0.0;
+		return;
+	}
+#endif
 	poppler_color_to_gdk_rgba (poppler_color, color);
 	g_free (poppler_color);
 }
@@ -2621,6 +2627,7 @@ poppler_annot_can_have_popup_window (PopplerAnnot *poppler_annot)
 {
 	switch (poppler_annot_get_annot_type (poppler_annot)) {
 	case POPPLER_ANNOT_TEXT:
+	case POPPLER_ANNOT_FREE_TEXT:
 	case POPPLER_ANNOT_LINE:
 	case POPPLER_ANNOT_SQUARE:
 	case POPPLER_ANNOT_CIRCLE:
@@ -2706,6 +2713,9 @@ pps_annot_from_poppler_annot (PopplerAnnot *poppler_annot,
 	case POPPLER_ANNOT_SQUIGGLY:
 		pps_annot = pps_annotation_text_markup_squiggly_new (page);
 		break;
+	case POPPLER_ANNOT_STAMP:
+		pps_annot = pps_annotation_stamp_new (page);
+		break;
 	case POPPLER_ANNOT_LINK:
 	case POPPLER_ANNOT_WIDGET:
 	case POPPLER_ANNOT_MOVIE:
@@ -2719,10 +2729,44 @@ pps_annot_from_poppler_annot (PopplerAnnot *poppler_annot,
 		if (action && action->type == POPPLER_ACTION_RENDITION)
 			break;
 	}
+#ifdef HAVE_FREE_TEXT
+	case POPPLER_ANNOT_FREE_TEXT: {
+		PopplerAnnotFreeText *poppler_ft = POPPLER_ANNOT_FREE_TEXT (poppler_annot);
+		PpsAnnotationFreeText *pps_annot_ft;
+		g_autoptr (PangoFontDescription) font;
+		g_autoptr (PopplerFontDescription) poppler_font;
+		g_autoptr (PopplerColor) font_color;
+		GdkRGBA font_rgba = { 0, 0, 0, 1.0 };
+		gdouble border_width;
+
+		pps_annot = pps_annotation_free_text_new (page);
+		pps_annot_ft = PPS_ANNOTATION_FREE_TEXT (pps_annot);
+
+		/* Font */
+		poppler_font = poppler_annot_free_text_get_font_desc (poppler_ft);
+		font = pango_font_description_new ();
+		pango_font_description_set_family (font, poppler_font->font_name);
+		pango_font_description_set_weight (font, poppler_font->weight);
+		pango_font_description_set_style (font, poppler_font->style);
+		pango_font_description_set_stretch (font, poppler_font->stretch);
+		pango_font_description_set_size (font, poppler_font->size_pt * PANGO_SCALE);
+		pps_annotation_free_text_set_font_description (pps_annot_ft, font);
+
+		/* Font color */
+		font_color = poppler_annot_free_text_get_font_color (poppler_ft);
+		poppler_color_to_gdk_rgba (font_color, &font_rgba);
+		pps_annotation_free_text_set_font_rgba (pps_annot_ft, &font_rgba);
+
+		/* Border width. This should be generalized to other annotations in the future. */
+		poppler_annot_get_border_width (poppler_annot, &border_width);
+		pps_annotation_set_border_width (pps_annot, border_width);
+	} break;
+#else
+	case POPPLER_ANNOT_FREE_TEXT:
+#endif
 		/* Fall through */
 	case POPPLER_ANNOT_3D:
 	case POPPLER_ANNOT_CARET:
-	case POPPLER_ANNOT_FREE_TEXT:
 	case POPPLER_ANNOT_LINE:
 	case POPPLER_ANNOT_SOUND:
 	case POPPLER_ANNOT_SQUARE:
@@ -2791,6 +2835,13 @@ pps_annot_from_poppler_annot (PopplerAnnot *poppler_annot,
 
 		poppler_annot_color_to_gdk_rgba (poppler_annot, &color);
 		pps_annotation_set_rgba (pps_annot, &color);
+
+		PopplerAnnotFlag flag = poppler_annot_get_flags (poppler_annot);
+		if (flag & POPPLER_ANNOT_FLAG_HIDDEN) {
+			pps_annotation_set_hidden (pps_annot, TRUE);
+		} else {
+			pps_annotation_set_hidden (pps_annot, FALSE);
+		}
 
 		if (poppler_annot_can_have_popup_window (poppler_annot)) {
 			PopplerAnnotMarkup *markup;
@@ -3104,6 +3155,27 @@ pdf_document_annotations_add_annotation (PpsDocumentAnnotations *document_annota
 		poppler_annot_text_set_icon (POPPLER_ANNOT_TEXT (poppler_annot),
 		                             get_poppler_annot_text_icon (icon));
 	} break;
+#ifdef HAVE_FREE_TEXT
+	case PPS_ANNOTATION_TYPE_FREE_TEXT: {
+		PpsAnnotationFreeText *annot_ft = PPS_ANNOTATION_FREE_TEXT (annot);
+		g_autoptr (PangoFontDescription) font;
+		g_autoptr (PopplerFontDescription) poppler_font;
+		gdouble size;
+		poppler_annot = poppler_annot_free_text_new (self->document, &poppler_rect);
+
+		/* Fonts */
+		font = pps_annotation_free_text_get_font_description (annot_ft);
+		size = pango_font_description_get_size (font) / PANGO_SCALE;
+		poppler_font = poppler_font_description_new (pango_font_description_get_family (font));
+		poppler_font->weight = pango_font_description_get_weight (font);
+		poppler_font->stretch = pango_font_description_get_stretch (font);
+		poppler_font->style = pango_font_description_get_style (font);
+		poppler_font->size_pt = size;
+		poppler_annot_free_text_set_font_desc (POPPLER_ANNOT_FREE_TEXT (poppler_annot), poppler_font);
+
+		poppler_annot_set_border_width (poppler_annot, pps_annotation_get_border_width (annot));
+	} break;
+#endif
 	case PPS_ANNOTATION_TYPE_TEXT_MARKUP: {
 		GArray *quads;
 		PopplerRectangle bbox;
@@ -3255,16 +3327,28 @@ pdf_document_annotations_save_annotation (PpsDocumentAnnotations *document_annot
 
 	if (mask & PPS_ANNOTATIONS_SAVE_COLOR) {
 		PopplerColor color;
-		GdkRGBA pps_color;
+		g_autoptr (GdkRGBA) pps_color = NULL;
 
-		pps_annotation_get_rgba (annot, &pps_color);
-		gdk_rgba_to_poppler_color (&pps_color, &color);
+		pps_annotation_get_rgba (annot, pps_color);
+		gdk_rgba_to_poppler_color (pps_color, &color);
 
-		if (pps_color.alpha > 0.) {
+		if (pps_color->alpha > 0.) {
 			poppler_annot_set_color (poppler_annot, &color);
 		} else {
 			poppler_annot_set_color (poppler_annot, NULL);
 		}
+#ifdef HAVE_FREE_TEXT
+		if (PPS_IS_ANNOTATION_FREE_TEXT (annot)) {
+			if (pps_color) {
+				gdk_rgba_free (pps_color);
+			}
+			pps_color = pps_annotation_free_text_get_font_rgba (PPS_ANNOTATION_FREE_TEXT (annot));
+			gdk_rgba_to_poppler_color (pps_color, &color);
+
+			poppler_annot_free_text_set_font_color (
+			    POPPLER_ANNOT_FREE_TEXT (poppler_annot), &color);
+		}
+#endif
 	}
 
 	if (mask & PPS_ANNOTATIONS_SAVE_AREA && !PPS_IS_ANNOTATION_TEXT_MARKUP (annot)) {
@@ -3325,6 +3409,26 @@ pdf_document_annotations_save_annotation (PpsDocumentAnnotations *document_annot
 		if (mask & PPS_ANNOTATIONS_SAVE_POPUP_IS_OPEN)
 			poppler_annot_markup_set_popup_is_open (markup, pps_annotation_markup_get_popup_is_open (pps_markup));
 	}
+
+#ifdef HAVE_FREE_TEXT
+	if (mask & PPS_ANNOTATIONS_SAVE_FREE_TEXT_FONT && PPS_IS_ANNOTATION_FREE_TEXT (annot)) {
+		PpsAnnotationFreeText *annot_ft = PPS_ANNOTATION_FREE_TEXT (annot);
+		PopplerAnnotFreeText *poppler_annot_ft = POPPLER_ANNOT_FREE_TEXT (poppler_annot);
+		g_autoptr (PangoFontDescription) font;
+		g_autoptr (PopplerFontDescription) poppler_font;
+		gdouble size;
+
+		font = pps_annotation_free_text_get_font_description (annot_ft);
+		size = pango_font_description_get_size (font) / PANGO_SCALE;
+		poppler_font = poppler_font_description_new (pango_font_description_get_family (font));
+
+		poppler_font->weight = pango_font_description_get_weight (font);
+		poppler_font->stretch = pango_font_description_get_stretch (font);
+		poppler_font->style = pango_font_description_get_style (font);
+		poppler_font->size_pt = size;
+		poppler_annot_free_text_set_font_desc (poppler_annot_ft, poppler_font);
+	}
+#endif
 
 	if (PPS_IS_ANNOTATION_TEXT (annot)) {
 		PpsAnnotationText *pps_text = PPS_ANNOTATION_TEXT (annot);

libdocument/pps-annotation.c
@@ -71,6 +71,22 @@ G_DEFINE_TYPE_WITH_CODE (PpsAnnotationText,
                              G_ADD_PRIVATE (PpsAnnotationText));
 #define GET_ANNOT_TEXT_PRIVATE(o) pps_annotation_text_get_instance_private (o)
 
+/* PpsAnnotationFreeText*/
+typedef struct {
+	PangoFontDescription *font_desc;
+	GdkRGBA font_rgba;
+} PpsAnnotationFreeTextPrivate;
+
+struct _PpsAnnotationFreeText {
+	PpsAnnotation parent;
+};
+
+G_DEFINE_TYPE_WITH_CODE (PpsAnnotationFreeText,
+                         pps_annotation_free_text,
+                         PPS_TYPE_ANNOTATION,
+                         G_ADD_PRIVATE (PpsAnnotationFreeText));
+#define GET_ANNOT_FREE_TEXT_PRIVATE(o) pps_annotation_free_text_get_instance_private (o)
+
 /* PpsAnnotationAttachment */
 typedef struct
 {
@@ -149,6 +165,13 @@ enum {
 /* PpsAnnotationTextMarkup */
 enum {
 	PROP_TEXT_MARKUP_TYPE = PROP_MARKUP_POPUP_IS_OPEN + 1
+};
+
+/* PpsAnnotationFreeText */
+enum {
+	PROP_ANNOT_FREE_TEXT_0,
+	PROP_ANNOT_FREE_TEXT_FONT_DESC,
+	PROP_ANNOT_FREE_TEXT_FONT_RGBA
 };
 
 /* PpsAnnotation */
@@ -1301,6 +1324,241 @@ pps_annotation_text_set_is_open (PpsAnnotationText *text,
 	g_object_notify (G_OBJECT (text), "is_open");
 
 	return TRUE;
+}
+
+/* PpsAnnotationFreeText */
+
+static void
+pps_annotation_free_text_init (PpsAnnotationFreeText *annot)
+{
+	PpsAnnotationPrivate *priv = GET_ANNOT_PRIVATE (PPS_ANNOTATION (annot));
+
+	priv->type = PPS_ANNOTATION_TYPE_FREE_TEXT;
+}
+
+static void
+pps_annotation_free_text_set_property (GObject *object,
+                                       guint prop_id,
+                                       const GValue *value,
+                                       GParamSpec *pspec)
+{
+	PpsAnnotationFreeText *annot = PPS_ANNOTATION_FREE_TEXT (object);
+
+	switch (prop_id) {
+	case PROP_ANNOT_FREE_TEXT_FONT_DESC:
+		pps_annotation_free_text_set_font_description (annot, g_value_get_boxed (value));
+		break;
+	case PROP_ANNOT_FREE_TEXT_FONT_RGBA:
+		pps_annotation_free_text_set_font_rgba (annot, g_value_get_boxed (value));
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+	}
+}
+
+static void
+pps_annotation_free_text_get_property (GObject *object,
+                                       guint prop_id,
+                                       GValue *value,
+                                       GParamSpec *pspec)
+{
+	PpsAnnotationFreeTextPrivate *priv = GET_ANNOT_FREE_TEXT_PRIVATE (PPS_ANNOTATION_FREE_TEXT (object));
+
+	switch (prop_id) {
+	case PROP_ANNOT_FREE_TEXT_FONT_RGBA:
+		g_value_set_boxed (value, &priv->font_rgba);
+		break;
+	case PROP_ANNOT_FREE_TEXT_FONT_DESC:
+		g_value_set_boxed (value, &priv->font_desc);
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+	}
+}
+
+static void
+pps_annotation_free_text_dispose (GObject *object)
+{
+	PpsAnnotationFreeTextPrivate *priv = GET_ANNOT_FREE_TEXT_PRIVATE (PPS_ANNOTATION_FREE_TEXT (object));
+
+	g_clear_pointer (&priv->font_desc, pango_font_description_free);
+
+	G_OBJECT_CLASS (pps_annotation_free_text_parent_class)->dispose (object);
+}
+
+static void
+pps_annotation_free_text_class_init (PpsAnnotationFreeTextClass *klass)
+{
+	GObjectClass *g_object_class = G_OBJECT_CLASS (klass);
+	g_object_class->set_property = pps_annotation_free_text_set_property;
+	g_object_class->get_property = pps_annotation_free_text_get_property;
+	g_object_class->dispose = pps_annotation_free_text_dispose;
+
+	g_object_class_install_property (g_object_class,
+	                                 PROP_ANNOT_FREE_TEXT_FONT_DESC,
+	                                 g_param_spec_boxed ("font-desc", NULL, NULL,
+	                                                     PANGO_TYPE_FONT_DESCRIPTION,
+	                                                     G_PARAM_READWRITE |
+	                                                         G_PARAM_STATIC_STRINGS));
+
+	g_object_class_install_property (g_object_class,
+	                                 PROP_ANNOT_FREE_TEXT_FONT_RGBA,
+	                                 g_param_spec_boxed ("font-rgba", NULL, NULL,
+	                                                     GDK_TYPE_RGBA,
+	                                                     G_PARAM_READWRITE |
+	                                                         G_PARAM_STATIC_STRINGS));
+}
+
+/**
+ * pps_annotation_free_text_new:
+ * @page: a #PpsPage
+ *
+ * Creates a new free text annotation in the page. Font shall be set afterwards.
+ *
+ * Returns: a new free text annotation
+ *
+ * Since: 48.0
+ */
+PpsAnnotation *
+pps_annotation_free_text_new (PpsPage *page)
+{
+	return PPS_ANNOTATION (g_object_new (PPS_TYPE_ANNOTATION_FREE_TEXT, "page", page, NULL));
+}
+
+/**
+ * pps_annotation_free_text_set_font_description:
+ * @annot: an #PpsAnnotationFreeText
+ * @font_desc: a #PangoFontDescription
+ *
+ * Set the font of the free text annotation to annotation to @font_desc.
+ *
+ * Returns: %TRUE if the font description has been changed, %FALSE otherwise
+ *
+ * Since: 48.0
+ */
+gboolean
+pps_annotation_free_text_set_font_description (PpsAnnotationFreeText *annot,
+                                               const PangoFontDescription *font_desc)
+{
+	PpsAnnotationFreeTextPrivate *priv = GET_ANNOT_FREE_TEXT_PRIVATE (PPS_ANNOTATION_FREE_TEXT (annot));
+	if (priv->font_desc && pango_font_description_equal (priv->font_desc, font_desc)) {
+		return FALSE;
+	}
+	g_clear_pointer (&priv->font_desc, pango_font_description_free);
+	priv->font_desc = pango_font_description_copy (font_desc);
+
+	g_object_notify (G_OBJECT (annot), "font-desc");
+	return TRUE;
+}
+
+/**
+ * pps_annotation_free_text_get_font_description:
+ * @annot: an #PpsAnnotationFreeText
+ *
+ * Returns a copy of the font descption used by the annotation.
+ *
+ * Returns: (transfer full): the font description used to display the annotation.
+ *
+ * Since: 48.0
+ */
+PangoFontDescription *
+pps_annotation_free_text_get_font_description (PpsAnnotationFreeText *annot)
+{
+	PpsAnnotationFreeTextPrivate *priv = GET_ANNOT_FREE_TEXT_PRIVATE (PPS_ANNOTATION_FREE_TEXT (annot));
+
+	return pango_font_description_copy (priv->font_desc);
+}
+
+/**
+ * pps_annotation_free_text_set_font_rgba:
+ * @annot: an #PpsAnnotationFreeText
+ * @rgba: a #GdkRGBA
+ *
+ * Set the text color of the annotation to @rgba.
+ *
+ * Returns: %TRUE if the color has been changed, %FALSE otherwise
+ *
+ * Since: 48.0
+ */
+gboolean
+pps_annotation_free_text_set_font_rgba (PpsAnnotationFreeText *annot, const GdkRGBA *rgba)
+{
+	PpsAnnotationFreeTextPrivate *priv = GET_ANNOT_FREE_TEXT_PRIVATE (PPS_ANNOTATION_FREE_TEXT (annot));
+
+	if (gdk_rgba_equal (&(priv->font_rgba), rgba)) {
+		return FALSE;
+	}
+
+	priv->font_rgba = *rgba;
+
+	g_object_notify (G_OBJECT (annot), "font-rgba");
+
+	return TRUE;
+}
+
+/**
+ * pps_annotation_free_text_get_font_rgba:
+ * @annot: an #PpsAnnotationFreeText
+ *
+ * Gets the text color of @annot.
+ *
+ * Returns: (transfer full): the font RGBA, must be freed by the caller
+ *
+ * Since: 48.0
+ */
+GdkRGBA *
+pps_annotation_free_text_get_font_rgba (PpsAnnotationFreeText *annot)
+{
+	PpsAnnotationFreeTextPrivate *priv = GET_ANNOT_FREE_TEXT_PRIVATE (PPS_ANNOTATION_FREE_TEXT (annot));
+
+	return gdk_rgba_copy (&priv->font_rgba);
+}
+
+/**
+ * pps_annotation_free_text_auto_resize:
+ * @annot: an #PpsAnnotationFreeText
+ * @ctx: a valid #PangoContext
+ *
+ * Resize the annotation so as all the text fits in its rect, according to Pango metrics.
+ * This should typically be called every time the content is changed unless the free text
+ * annotation is supposed to be fixed width for instance.
+ *
+ * Since: 48.0
+ */
+void
+pps_annotation_free_text_auto_resize (PpsAnnotationFreeText *annot, PangoContext *ctx)
+{
+	PpsAnnotationFreeTextPrivate *priv = GET_ANNOT_FREE_TEXT_PRIVATE (PPS_ANNOTATION_FREE_TEXT (annot));
+
+	gint width, height, border_width;
+	PpsRectangle rect;
+
+	/* The text is going to be measured with a pango layout with the same settings as the annot. */
+	g_autoptr (PangoLayout) layout = pango_layout_new (ctx);
+	gint font_size = pango_font_description_get_size (priv->font_desc);
+	g_autoptr (PangoAttrList) list = pango_attr_list_new ();
+	// font_size is in px, convert it to pt with /0.75
+	PangoAttribute *attr = pango_attr_line_height_new_absolute (font_size / 0.75);
+
+	const gchar *contents = pps_annotation_get_contents (PPS_ANNOTATION (annot));
+	pango_layout_set_text (layout, contents, -1);
+
+	pango_attr_list_change (list, attr);
+	pango_layout_set_attributes (layout, list);
+	pango_layout_set_font_description (layout, priv->font_desc);
+
+	pango_layout_get_size (layout, &width, &height);
+
+	/* Updating the annot size based on the current position. */
+	pps_annotation_get_area (PPS_ANNOTATION (annot), &rect);
+	/* poppler adds a padding of the same extent as the border width, so the actual width/height must
+	be increased by 4*border_width
+	+ 2 is necessary because poppler does not layout characters exactly as pango unfortunately */
+	border_width = pps_annotation_get_border_width (PPS_ANNOTATION (annot));
+	rect.x2 = rect.x1 + width / PANGO_SCALE * 0.75 + 4 * border_width + 2;
+	/* We add 1/4 of a line below so descending characters are shown.*/
+	rect.y2 = rect.y1 + height / PANGO_SCALE * 0.75 + 4 * border_width + 0.25 * font_size / PANGO_SCALE + 2;
+	pps_annotation_set_area (PPS_ANNOTATION (annot), &rect);
 }
 
 /* PpsAnnotationAttachment */

libdocument/pps-annotation.h
@@ -54,6 +54,12 @@ G_DECLARE_INTERFACE (PpsAnnotationMarkup, pps_annotation_markup, PPS, ANNOTATION
 PPS_PUBLIC
 G_DECLARE_FINAL_TYPE (PpsAnnotationText, pps_annotation_text, PPS, ANNOTATION_TEXT, PpsAnnotation);
 
+/* PpsAnnotationFreeText */
+#define PPS_TYPE_ANNOTATION_FREE_TEXT (pps_annotation_free_text_get_type ())
+PPS_PUBLIC
+G_DECLARE_FINAL_TYPE (
+    PpsAnnotationFreeText, pps_annotation_free_text, PPS, ANNOTATION_FREE_TEXT, PpsAnnotation);
+
 /* PpsAnnotationAttachment */
 #define PPS_TYPE_ANNOTATION_ATTACHMENT (pps_annotation_attachment_get_type ())
 PPS_PUBLIC
@@ -67,6 +73,7 @@ G_DECLARE_FINAL_TYPE (PpsAnnotationTextMarkup, pps_annotation_text_markup, PPS, 
 typedef enum {
 	PPS_ANNOTATION_TYPE_UNKNOWN,
 	PPS_ANNOTATION_TYPE_TEXT,
+	PPS_ANNOTATION_TYPE_FREE_TEXT,
 	PPS_ANNOTATION_TYPE_ATTACHMENT,
 	PPS_ANNOTATION_TYPE_TEXT_MARKUP
 } PpsAnnotationType;
@@ -188,6 +195,23 @@ gboolean pps_annotation_text_get_is_open (PpsAnnotationText *text);
 PPS_PUBLIC
 gboolean pps_annotation_text_set_is_open (PpsAnnotationText *text,
                                           gboolean is_open);
+
+/* PpsAnnotationFreeText */
+PPS_PUBLIC
+PpsAnnotation *pps_annotation_free_text_new (PpsPage *page);
+PPS_PUBLIC
+gboolean pps_annotation_free_text_set_font_description (PpsAnnotationFreeText *annot,
+                                                        const PangoFontDescription *font_desc);
+PPS_PUBLIC
+PangoFontDescription *pps_annotation_free_text_get_font_description (PpsAnnotationFreeText *annot);
+PPS_PUBLIC
+gboolean pps_annotation_free_text_set_font_rgba (PpsAnnotationFreeText *annot,
+                                                 const GdkRGBA *rgba);
+PPS_PUBLIC
+GdkRGBA *pps_annotation_free_text_get_font_rgba (PpsAnnotationFreeText *annot);
+PPS_PUBLIC
+void pps_annotation_free_text_auto_resize (PpsAnnotationFreeText *annot,
+                                           PangoContext *ctx);
 
 /* PpsAnnotationAttachment */
 PPS_PUBLIC

libdocument/pps-document-annotations.h
@@ -61,8 +61,11 @@ typedef enum {
 	/* Text Markup Annotations */
 	PPS_ANNOTATIONS_SAVE_TEXT_MARKUP_TYPE = 1 << 11,
 
+	/* Free Text Annotations */
+	PPS_ANNOTATIONS_SAVE_FREE_TEXT_FONT = 1 << 12,
+
 	/* Save all */
-	PPS_ANNOTATIONS_SAVE_ALL = (1 << 12) - 1
+	PPS_ANNOTATIONS_SAVE_ALL = (1 << 13) - 1
 } PpsAnnotationsSaveMask;
 
 typedef enum {

shell-rs/papers-document/Gir.toml
@@ -22,6 +22,7 @@ external_libraries = [
 generate = [
     "PapersDocument.Annotation",
     "PapersDocument.AnnotationAttachment",
+    "PapersDocument.AnnotationFreeText",
     "PapersDocument.AnnotationMarkup",
     "PapersDocument.AnnotationText",
     "PapersDocument.AnnotationTextIcon",

commit 36579d4355197bf57271ed90c6de727851d008f3

libdocument: backend: pdf: move POPPLER_ANNOT_SCREEN in switch for correct fallthrough
Fixes: 7112994b9dac7e306c9fe7a1d8bddf91c6d3033c ("libdocument, backends: Add the free text annotation type")

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

libdocument/backend/pdf/pps-poppler.c
@@ -2719,14 +2719,6 @@ pps_annot_from_poppler_annot (PopplerAnnot *poppler_annot,
 	case POPPLER_ANNOT_MOVIE:
 		/* Ignore link, widgets and movie annots since they are already handled */
 		break;
-	case POPPLER_ANNOT_SCREEN: {
-		PopplerAction *action;
-
-		/* Ignore screen annots containing a rendition action */
-		action = poppler_annot_screen_get_action (POPPLER_ANNOT_SCREEN (poppler_annot));
-		if (action && action->type == POPPLER_ACTION_RENDITION)
-			break;
-	}
 #ifdef HAVE_FREE_TEXT
 	case POPPLER_ANNOT_FREE_TEXT: {
 		PopplerAnnotFreeText *poppler_ft = POPPLER_ANNOT_FREE_TEXT (poppler_annot);
@@ -2763,10 +2755,19 @@ pps_annot_from_poppler_annot (PopplerAnnot *poppler_annot,
 		poppler_annot_get_border_width (poppler_annot, &border_width);
 		pps_annotation_set_border_width (pps_annot, border_width);
 	} break;
-#else
+#endif
+	case POPPLER_ANNOT_SCREEN: {
+		PopplerAction *action;
+
+		/* Ignore screen annots containing a rendition action */
+		action = poppler_annot_screen_get_action (POPPLER_ANNOT_SCREEN (poppler_annot));
+		if (action && action->type == POPPLER_ACTION_RENDITION)
+			break;
+	}
+	/* Fall through */
+#ifndef HAVE_FREE_TEXT
 	case POPPLER_ANNOT_FREE_TEXT:
 #endif
-		/* Fall through */
 	case POPPLER_ANNOT_3D:
 	case POPPLER_ANNOT_CARET:
 	case POPPLER_ANNOT_LINE: