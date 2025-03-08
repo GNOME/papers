GNOME/papers

commit 15f07cf0d384d639efe5a1ea295755666fa9035f

libview: view: render caret, focus and signing rect outside of page loop
These are only for one page, and as such do not need to
be iterated over. They will not be ported over to the
per-page widget in its first iteration, but everything
inside the loop after this will be.

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

libview/pps-view.c
@@ -4112,32 +4112,25 @@ draw_caret_cursor (PpsView *view,
 }
 
 static gboolean
-should_draw_caret_cursor (PpsView *view,
-                          gint page)
+should_draw_caret_cursor (PpsView *view)
 {
 	PpsViewPrivate *priv = GET_PRIVATE (view);
 	gdouble scale = pps_document_model_get_scale (priv->model);
 
 	return (priv->caret_enabled &&
-	        priv->cursor_page == page &&
 	        priv->cursor_visible &&
 	        gtk_widget_has_focus (GTK_WIDGET (view)) &&
-	        !pps_pixbuf_cache_get_selection_region (priv->pixbuf_cache, page, scale));
+	        !pps_pixbuf_cache_get_selection_region (priv->pixbuf_cache, priv->cursor_page, scale));
 }
 
 static void
 draw_focus (PpsView *view,
             GtkSnapshot *snapshot,
-            gint page,
             GdkRectangle *clip)
 {
 	GtkWidget *widget = GTK_WIDGET (view);
 	GdkRectangle rect;
 	GdkRectangle intersect;
-	PpsViewPrivate *priv = GET_PRIVATE (view);
-
-	if (priv->focused_element_page != page)
-		return;
 
 	if (!gtk_widget_has_focus (widget))
 		return;
@@ -4351,8 +4344,7 @@ draw_debug_borders (PpsView *view,
 
 static void
 draw_signing_rect (PpsView *view,
-                   GtkSnapshot *snapshot,
-                   gint page)
+                   GtkSnapshot *snapshot)
 {
 	PpsViewPrivate *priv = GET_PRIVATE (view);
 	GskRoundedRect outline;
@@ -4432,13 +4424,8 @@ pps_view_snapshot (GtkWidget *widget, GtkSnapshot *snapshot)
 		if (!draw_one_page (view, i, snapshot, &page_area, &clip_rect))
 			continue;
 
-		if (should_draw_caret_cursor (view, i))
-			draw_caret_cursor (view, snapshot);
 		if (pps_search_context_get_active (priv->search_context))
 			highlight_find_results (view, snapshot, i);
-		if (priv->focused_element)
-			draw_focus (view, snapshot, i, &clip_rect);
-		draw_signing_rect (view, snapshot, i);
 
 		if (pps_debug_get_debug_borders ())
 			draw_debug_borders (view, snapshot, i, &clip_rect);
@@ -4446,6 +4433,14 @@ pps_view_snapshot (GtkWidget *widget, GtkSnapshot *snapshot)
 
 	/* snapshot child widgets */
 	GTK_WIDGET_CLASS (pps_view_parent_class)->snapshot (widget, snapshot);
+
+	if (priv->focused_element)
+		draw_focus (view, snapshot, &clip_rect);
+
+	if (should_draw_caret_cursor (view))
+		draw_caret_cursor (view, snapshot);
+
+	draw_signing_rect (view, snapshot);
 
 	gtk_snapshot_pop (snapshot);
 }

commit 81cf494bb95e4994f15e839632cd48a7ec8d8530

libview: view: draw focus only if in view
The view coordinates for multiple pages overlap in
non-continuous mode and only there. Thus, a check is
required to verify the focus element is within the range
of currently shown pages.

Fixes: 15f07cf0d384d639efe5a1ea295755666fa9035f ("libview: view: render caret, focus and signing rect outside of page loop")

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

libview/pps-view.c
@@ -4121,10 +4121,14 @@ static void
 draw_focus (PpsView *view,
             GtkSnapshot *snapshot)
 {
+	PpsViewPrivate *priv = GET_PRIVATE (view);
 	GtkWidget *widget = GTK_WIDGET (view);
 	GdkRectangle rect;
 
 	if (!gtk_widget_has_focus (widget))
+		return;
+
+	if (priv->start_page > priv->focused_element_page || priv->focused_element_page < priv->end_page)
 		return;
 
 	if (!pps_view_get_focused_area (view, &rect))