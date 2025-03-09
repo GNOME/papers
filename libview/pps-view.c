GNOME/papers

commit 3a006886127a6f961d71ac1fd5c467b3743f11e5

libview: view: clear selection when document get's reloaded
If the selection is on a page index that does not exist
after reloading, this can even result in crashes.

Link: https://gitlab.gnome.org/GNOME/Incubator/papers/-/issues/353#note_2372683

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

libview/pps-view.c
@@ -7216,6 +7216,8 @@ pps_view_document_changed_cb (PpsDocumentModel *model,
 
 	pps_view_remove_all (view);
 	clear_caches (view);
+	if (pps_view_has_selection (view))
+		clear_selection (view);
 
 	if (document == NULL)
 		return;

commit 17fed2e729a7f9cf966b75d60ad6ccd0f34eadcd

libview: view: clear selection when reloading without referencing a non-existent page
If the old pixbuf cache is still around, it has stale
start and end pages. As such, it will result in a
critical when trying to get a non-existent page.

Fixes: 3a006886127a6f961d71ac1fd5c467b3743f11e5 ("libview: view: clear selection when document get's reloaded")

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

libview/pps-view.c
@@ -7280,8 +7280,6 @@ pps_view_document_changed_cb (PpsDocumentModel *model,
 
 	pps_view_remove_all (view);
 	clear_caches (view);
-	if (pps_view_has_selection (view))
-		clear_selection (view);
 
 	if (document == NULL)
 		return;
@@ -7291,6 +7289,9 @@ pps_view_document_changed_cb (PpsDocumentModel *model,
 		return;
 
 	setup_caches (view);
+
+	if (pps_view_has_selection (view))
+		clear_selection (view);
 
 	if (priv->caret_enabled)
 		preload_pages_for_caret_navigation (view);