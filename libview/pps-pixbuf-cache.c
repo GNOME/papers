GNOME/papers

commit 9c320c3fa994ff53480892e37067cd4e2660ab5e

libview: pixbuf-cache: clear cache on selection update
If the selection was updated without being cleared, the
cache is kept stale, because for the texture to be
re-generated, it is checked for NULL.

Closes: https://gitlab.gnome.org/GNOME/Incubator/papers/-/issues/180
Closes: https://gitlab.gnome.org/GNOME/Incubator/papers/-/issues/374

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

libview/pps-pixbuf-cache.c
@@ -1115,15 +1115,6 @@ pps_pixbuf_cache_get_selection_region (PpsPixbufCache *pixbuf_cache,
 }
 
 static void
-update_job_selection (CacheJobInfo *job_info,
-                      PpsViewSelection *selection)
-{
-	job_info->points_set = TRUE;
-	job_info->target_points = selection->rect;
-	job_info->selection_style = selection->style;
-}
-
-static void
 clear_job_selection (CacheJobInfo *job_info)
 {
 	job_info->points_set = FALSE;
@@ -1131,6 +1122,17 @@ clear_job_selection (CacheJobInfo *job_info)
 
 	g_clear_object (&job_info->selection_texture);
 	g_clear_pointer (&job_info->selection_region, cairo_region_destroy);
+}
+
+static void
+update_job_selection (CacheJobInfo *job_info,
+                      PpsViewSelection *selection)
+{
+	clear_job_selection (job_info);
+
+	job_info->points_set = TRUE;
+	job_info->target_points = selection->rect;
+	job_info->selection_style = selection->style;
 }
 
 /* This function will reset the selection on pages that no longer have them, and

commit 3c7f9efd11178cae28c51a6a74f7106830048947

libview: pixbuf-cache: mark selection as stale instead of clearing it
Clearing the cache prematurely bears the risk of
selection flickering. That issue will be even more
pronounced with per-page widgets due to changes in redraw
timing.

Fixes: 9c320c3fa994ff53480892e37067cd4e2660ab5e ("libview: pixbuf-cache: clear cache on selection update")

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

libview/pps-pixbuf-cache.c
@@ -29,6 +29,7 @@ typedef struct _CacheJobInfo {
 	PpsRectangle target_points;
 	PpsSelectionStyle selection_style;
 	gboolean points_set;
+	gboolean selection_stale;
 
 	GdkTexture *selection_texture;
 	gdouble selection_scale;
@@ -664,6 +665,7 @@ add_job (PpsPixbufCache *pixbuf_cache,
 		                                           job_info->selection_style,
 		                                           &text, &base);
 	}
+	job_info->selection_stale = FALSE;
 
 	g_signal_connect (job, "finished",
 	                  G_CALLBACK (job_finished_cb),
@@ -707,7 +709,8 @@ add_job_if_needed (PpsPixbufCache *pixbuf_cache,
 	if (job_info->texture &&
 	    job_info->device_scale == device_scale &&
 	    gdk_texture_get_width (job_info->texture) == width * device_scale &&
-	    gdk_texture_get_height (job_info->texture) == height * device_scale)
+	    gdk_texture_get_height (job_info->texture) == height * device_scale &&
+	    (!job_info->points_set || !job_info->selection_stale))
 		return;
 
 	/* Free old surfaces for non visible pages */
@@ -872,6 +875,8 @@ new_selection_surface_needed (PpsPixbufCache *pixbuf_cache,
                               gint page,
                               gfloat scale)
 {
+	if (job_info->selection_stale)
+		return TRUE;
 	if (job_info->selection_texture)
 		return job_info->selection_scale != scale;
 	return job_info->points_set;
@@ -883,6 +888,8 @@ new_selection_region_needed (PpsPixbufCache *pixbuf_cache,
                              gint page,
                              gfloat scale)
 {
+	if (job_info->selection_stale)
+		return TRUE;
 	if (job_info->selection_region)
 		return job_info->selection_region_scale != scale;
 	return job_info->points_set;
@@ -1118,6 +1125,7 @@ pps_pixbuf_cache_get_selection_region (PpsPixbufCache *pixbuf_cache,
 static void
 clear_job_selection (CacheJobInfo *job_info)
 {
+	job_info->selection_stale = FALSE;
 	job_info->points_set = FALSE;
 	job_info->selection_points.x1 = -1;
 
@@ -1129,8 +1137,7 @@ static void
 update_job_selection (CacheJobInfo *job_info,
                       PpsViewSelection *selection)
 {
-	clear_job_selection (job_info);
-
+	job_info->selection_stale = TRUE;
 	job_info->points_set = TRUE;
 	job_info->target_points = selection->rect;
 	job_info->selection_style = selection->style;
@@ -1144,6 +1151,9 @@ void
 pps_pixbuf_cache_set_selection_list (PpsPixbufCache *pixbuf_cache,
                                      GList *selection_list)
 {
+	gdouble scale = pps_document_model_get_scale (pixbuf_cache->model);
+	gint rotation = pps_document_model_get_rotation (pixbuf_cache->model);
+	PpsRenderAnnotsFlags annot_flags = pps_pixbuf_cache_get_annot_flags (pixbuf_cache);
 	PpsViewSelection *selection;
 	GList *list = selection_list;
 	int page;
@@ -1221,6 +1231,8 @@ pps_pixbuf_cache_set_selection_list (PpsPixbufCache *pixbuf_cache,
 			clear_job_selection (pixbuf_cache->next_job + i);
 		page++;
 	}
+
+	pps_pixbuf_cache_add_jobs_if_needed (pixbuf_cache, rotation, scale, annot_flags);
 }
 
 /* Returns what the pixbuf cache thinks is */