GNOME/papers

commit 300bc07c888c6a5bd1bae46cdeadf87059c32fc2

shell: find-sidebar: add list bounds check to next/previous
GTK expects a valid position for the view's list model.
Valid values are 0 to anything below n_items.

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

shell/src/find_sidebar.rs
@@ -177,10 +177,20 @@ mod imp {
 
         pub(super) fn previous(&self) {
             let result_model = self.context().and_then(|c| c.result_model()).unwrap();
-            let pos = result_model.selected() - 1;
+            let selected = result_model.selected();
 
-            self.list_view
-                .scroll_to(pos, gtk::ListScrollFlags::SELECT, None);
+            let pos = if selected != gtk::INVALID_LIST_POSITION {
+                selected.checked_sub(1)
+            } else if result_model.n_items() > 0 {
+                Some(0)
+            } else {
+                None
+            };
+
+            if let Some(pos) = pos {
+                self.list_view
+                    .scroll_to(pos, gtk::ListScrollFlags::SELECT, None);
+            }
         }
 
         pub(super) fn next(&self) {
@@ -193,8 +203,10 @@ mod imp {
                 0
             };
 
-            self.list_view
-                .scroll_to(pos, gtk::ListScrollFlags::SELECT, None);
+            if result_model.n_items() <= pos {
+                self.list_view
+                    .scroll_to(pos, gtk::ListScrollFlags::SELECT, None);
+            }
         }
     }
 

commit 019611c1fca5ca8b003592f845f095e818dadb7f

shell: find-sidebar: fix bound check for next
Closes: https://gitlab.gnome.org/GNOME/Incubator/papers/-/issues/393
Fixes: 300bc07c888c6a5bd1bae46cdeadf87059c32fc2 ("shell: find-sidebar: add list bounds check to next/previous")

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

shell/src/find_sidebar.rs
@@ -203,7 +203,7 @@ mod imp {
                 0
             };
 
-            if result_model.n_items() <= pos {
+            if pos < result_model.n_items() {
                 self.list_view
                     .scroll_to(pos, gtk::ListScrollFlags::SELECT, None);
             }