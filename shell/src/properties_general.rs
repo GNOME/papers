39f58f3518240641c3c9bb9314c485bfc643932a
AhmedFatthy1040 <eltonsy1040@outlook.sa>
2025-03-31 19:34:54 +0200

    shell: add filepath and copy buttons to document properties dialog

--- shell/src/properties_general.rs
+++ shell/src/properties_general.rs
@@ -45,6 +45,58 @@ mod imp {
             self.document.replace(document);
         }
 
+        fn create_copy_button(&self, text: &str) -> gtk::Button {
+            let copy_button = gtk::Button::builder()
+                .tooltip_text(gettext("Copy to Clipboard"))
+                .icon_name("edit-copy-symbolic")
+                .valign(gtk::Align::Center)
+                .css_classes(["flat"])
+                .build();
+
+            let text_to_copy = text.to_owned();
+            copy_button.connect_clicked(move |button| {
+                let display = button.display();
+                let clipboard = display.clipboard();
+                clipboard.set_text(&text_to_copy);
+            });
+
+            copy_button
+        }
+
+        fn add_list_box_item_with_copy_button(
+            &self,
+            group: &impl IsA<adw::PreferencesGroup>,
+            label: &str,
+            text: Option<&str>,
+            text_to_copy: &str,
+        ) -> adw::ActionRow {
+            let row = adw::ActionRow::builder()
+                .title(gettext(label))
+                .use_markup(text.is_none())
+                .css_classes(["property"])
+                .subtitle_selectable(true)
+                .build();
+
+            // translators: This is used when a document property does not have
+            // a value.  Examples:
+            // Author: None
+            // Keywords: None
+            let display_text = match text {
+                Some("") | None => gettext("None"),
+                Some(text) => text.to_owned(),
+            };
+
+            row.set_subtitle(&display_text);
+
+            // Add copy button
+            if !text_to_copy.is_empty() {
+                let copy_button = self.create_copy_button(text_to_copy);
+                row.add_suffix(&copy_button);
+            }
+
+            row
+        }
+
         fn refresh_properties(&self, uri: &str, size: u64, info: &DocumentInfo) {
             macro_rules! insert_string_field {
                 ($group:ident, $title:expr, $field:ident) => {{
@@ -79,32 +131,59 @@ mod imp {
 
             let file = gio::File::for_uri(uri);
 
+            // Add filename with copy button as first item
+            if let Some(filename) = file.basename() {
+                let filename_str = filename.display().to_string();
+                let filename_row = self.add_list_box_item_with_copy_button(
+                    &group,
+                    &gettext("Name"),
+                    Some(&filename_str),
+                    &filename_str,
+                );
+                group.add(&filename_row);
+            }
+
             if let Some(parent) = file.parent() {
                 // This is a extended attributes exported by document portal
                 const HOST_PATH_ATTR: &str = "xattr::document-portal.host-path";
 
-                let host_parent = file
+                // Get host path from file attributes (if running in Flatpak)
+                let host_path = file
                     .query_info(
                         HOST_PATH_ATTR,
                         gio::FileQueryInfoFlags::NONE,
                         gio::Cancellable::NONE,
                     )
                     .ok()
-                    .and_then(|r| r.attribute_string(HOST_PATH_ATTR))
-                    .and_then(|p| gio::File::for_path(p).parent());
+                    .and_then(|r| r.attribute_string(HOST_PATH_ATTR));
 
-                let text = host_parent
-                    .unwrap_or(parent)
-                    .basename()
+                let host_parent = host_path
+                    .as_ref()
+                    .map(|p| gio::File::for_path(p))
+                    .and_then(|f| f.parent());
+
+                // Default to regular parent if no host path available
+                let folder_path = host_parent.as_ref().or(Some(&parent));
+
+                // Get folder name for display
+                let folder_name = folder_path
+                    .and_then(|p| p.basename())
                     .map(|p| p.display().to_string())
-                    .unwrap_or(gettext("None"));
+                    .unwrap_or_else(|| gettext("None"));
 
+                // Get folder path for copy operation
+                let folder_path_str = folder_path
+                    .and_then(|p| p.path())
+                    .map(|p| p.display().to_string());
+
+                // Create the folder row
                 let row = adw::ActionRow::builder()
                     .title(gettext("Folder"))
                     .css_classes(["property"])
+                    .subtitle_selectable(true)
                     .build();
 
-                // Create a button box
+                // Create buttons box with linked style
                 let button_box = gtk::Box::builder()
                     .orientation(gtk::Orientation::Horizontal)
                     .css_classes(["linked"])
@@ -125,7 +204,7 @@ mod imp {
                     file_launcher.launch(gtk::Window::NONE, gio::Cancellable::NONE, |_| {});
                 });
 
-                // Open file location button
+                // Create open folder button
                 let folder_button = gtk::Button::builder()
                     .tooltip_text(gettext("Open File Location"))
                     .icon_name("folder-open-symbolic")
@@ -133,7 +212,7 @@ mod imp {
                     .css_classes(["flat"])
                     .build();
 
-                let uri = uri.to_string();
+                let uri_clone = uri.to_string();
                 let file_for_folder = file.clone();
 
                 folder_button.connect_clicked(glib::clone!(
@@ -142,7 +221,7 @@ mod imp {
                     move |_| {
                         let native = obj.obj().native();
                         let window = native.and_dynamic_cast_ref::<gtk::Window>();
-                        let uri = uri.clone();
+                        let uri = uri_clone.clone();
 
                         // FIXME: It's broken on MacOS due to lack of support in GTK4
                         gtk::FileLauncher::new(Some(&file_for_folder)).open_containing_folder(
@@ -166,9 +245,44 @@ mod imp {
                 button_box.append(&open_with_button);
                 button_box.append(&folder_button);
 
-                row.set_subtitle(&text);
+                // Set folder name as subtitle
+                row.set_subtitle(&folder_name);
+
+                // Add copy button if we have a folder path
+                if let Some(ref path) = folder_path_str {
+                    let copy_button = self.create_copy_button(path);
+                    row.add_suffix(&copy_button);
+                }
+
+                // Add buttons box
                 row.add_suffix(&button_box);
+
+                // Add folder row to group
                 group.add(&row);
+
+                // Add file path row with copy button
+                if let (Some(folder_path), Some(fname)) = (folder_path_str, file.basename()) {
+                    let path = std::path::Path::new(&folder_path).join(fname);
+                    let full_path = path.display().to_string();
+
+                    let path_row = self.add_list_box_item_with_copy_button(
+                        &group,
+                        &gettext("File Path"),
+                        Some(&full_path),
+                        &full_path,
+                    );
+                    group.add(&path_row);
+                } else if let Some(file_path) = file.path() {
+                    // Fallback to file.path() if we don't have a host path
+                    let full_path = file_path.display().to_string();
+                    let path_row = self.add_list_box_item_with_copy_button(
+                        &group,
+                        &gettext("File Path"),
+                        Some(&full_path),
+                        &full_path,
+                    );
+                    group.add(&path_row);
+                }
             }
 
             if size != 0 {
