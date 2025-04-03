9b158fcbe6472d42989f246a62618b457919b54c
Hari Rana <theevilskeleton@riseup.net>
2025-04-03 10:46:12 -0400

    shell: actions: Make Ctrl+F always refocus the search entry
    Currently, the Ctrl+F shortcut closes the find page when the
    user activates it while being focused on a list item.
    This is an unintended behavior, as a user who expects to refocus
    on the search entry from the sidebar would accidentally
    hide it instead.

--- shell/src/document_view/actions.rs
+++ shell/src/document_view/actions.rs
@@ -587,25 +587,15 @@ impl imp::PpsDocumentView {
                     self,
                     move |_, _, _| {
                         let selected_text = obj.view.selected_text().filter(|t| !t.is_empty());
-
+                        obj.document_action_group
+                            .change_action_state("toggle-find", &true.into());
                         if let Some(selected_text) = selected_text {
                             obj.search_context
                                 .borrow()
                                 .as_ref()
                                 .unwrap()
                                 .set_search_term(&selected_text);
-                            obj.document_action_group
-                                .change_action_state("toggle-find", &true.into());
                             obj.find_restart();
-                        } else {
-                            let active = obj
-                                .sidebar_stack
-                                .visible_child()
-                                .is_some_and(|w| w == *obj.find_sidebar)
-                                && obj.find_sidebar.focus_child().is_some();
-
-                            obj.document_action_group
-                                .change_action_state("toggle-find", &(!active).into());
                         }
                     }
                 ))
