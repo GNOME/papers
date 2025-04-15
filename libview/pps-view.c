GNOME/papers

commit 41997df260b1fa0e36d6963c38828b8a1cad21da

libview: view: simplify pps_view_goto_dest
Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

libview/pps-view.c
@@ -1967,25 +1967,19 @@ pps_view_goto_dest (PpsView *view, PpsLinkDest *dest)
 {
 	PpsViewPrivate *priv = GET_PRIVATE (view);
 	PpsLinkDestType type;
+	g_autoptr (PpsLinkDest) final_dest = NULL;
 
 	type = pps_link_dest_get_dest_type (dest);
 
 	if (type == PPS_LINK_DEST_TYPE_NAMED) {
-		PpsLinkDest *dest2;
 		const gchar *named_dest;
 
 		named_dest = pps_link_dest_get_named_dest (dest);
-		dest2 = pps_document_links_find_link_dest (PPS_DOCUMENT_LINKS (pps_document_model_get_document (priv->model)),
-		                                           named_dest);
-		if (dest2) {
-			goto_dest (view, dest2);
-			g_object_unref (dest2);
-		}
-
-		return;
+		final_dest = pps_document_links_find_link_dest (PPS_DOCUMENT_LINKS (pps_document_model_get_document (priv->model)),
+		                                                named_dest);
 	}
 
-	goto_dest (view, dest);
+	goto_dest (view, final_dest ? final_dest : dest);
 }
 
 static void

commit 50df638cfa5ebc8c5d538768dc0c6e403226293b

libview: Don't crash with not existing named dest
Fixes: 41997df2 ("libview: view: simplify pps_view_goto_dest ")

Fixes #409

Signed-off-by: Qiu Wenbo <qiuwenbo@kylinos.com.cn>

libview/pps-view.c
@@ -1983,6 +1983,9 @@ pps_view_goto_dest (PpsView *view, PpsLinkDest *dest)
 		named_dest = pps_link_dest_get_named_dest (dest);
 		final_dest = pps_document_links_find_link_dest (PPS_DOCUMENT_LINKS (pps_document_model_get_document (priv->model)),
 		                                                named_dest);
+
+		if (!final_dest)
+			return;
 	}
 
 	goto_dest (view, final_dest ? final_dest : dest);
@@ -2215,6 +2218,8 @@ handle_link_preview (PpsView *view)
 	if (type == PPS_LINK_DEST_TYPE_NAMED) {
 		dest = pps_document_links_find_link_dest (PPS_DOCUMENT_LINKS (pps_document_model_get_document (priv->model)),
 		                                          pps_link_dest_get_named_dest (dest));
+		if (!dest)
+			return G_SOURCE_REMOVE;
 	}
 
 	/* Handle the case that page doesn't exist */