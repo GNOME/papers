8a1f354305091839d9804a9784fe2692eb9bd577
Qiu Wenbo <qiuwenbo@kylinos.com.cn>
2025-04-21 11:27:14 +0800

    papers: Switch to use the SPDX identifier
    Signed-off-by: Qiu Wenbo <qiuwenbo@kylinos.com.cn>

--- libview/pps-pixbuf-cache.c
+++ libview/pps-pixbuf-cache.c
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #include "pps-pixbuf-cache.h"
 #include "pps-colors.h"
 #include "pps-job-scheduler.h"
--- nautilus/pps-nautilus-extension.c
+++ nautilus/pps-nautilus-extension.c
@@ -1,27 +1,14 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (C) 2000, 2001 Eazel Inc.
  * Copyright (C) 2003  Andrew Sobala <aes@gnome.org>
  * Copyright (C) 2005  Bastien Nocera <hadess@hadess.net>
  * Copyright (C) 2024  Qiu Wenbo <qiuwenbo@kylinos.com.cn>
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- *
- * The Pps project hereby grant permission for non-gpl compatible GStreamer
- * plugins to be used and distributed together with GStreamer and Pps. This
+ * The Papers project hereby grant permission for non-gpl compatible GStreamer
+ * plugins to be used and distributed together with GStreamer and Papers. This
  * permission are above and beyond the permissions granted by the GPL license
- * Pps is covered by.
+ * Papers is covered by.
  *
  * Monday 7th February 2005: Christian Schaller: Add exemption clause.
  * See license_change file for details.
