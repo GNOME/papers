30d0dd87ade87c771f1cee0952fd82a4cdcd3c9c
nibon7 <nibon7@163.com>
2025-05-23 13:33:20 +0800

    libview: add test case for encrypted document
    Encrypted document from https://gitlab.freedesktop.org/poppler/test/-/raw/master/unittestcases/PasswordEncrypted.pdf

--- libview/tests/test-pps-jobs.c
+++ libview/tests/test-pps-jobs.c
@@ -43,11 +43,49 @@ get_annots (void)
 	g_assert (g_list_length (annots) == 2);
 }
 
+static void
+load_encrypted (void)
+{
+	PpsDocument *document;
+	PpsJob *job = pps_job_load_new ();
+	gchar *file_path = SRCDIR "/PasswordEncrypted.pdf";
+	GFile *file = NULL;
+	gchar *uri = NULL;
+	gboolean hasBackend;
+	gint n_pages;
+
+	file = g_file_new_for_path (file_path);
+	uri = g_file_get_uri (file);
+	hasBackend = pps_init ();
+
+	g_assert_nonnull (file);
+	g_assert_nonnull (uri);
+	g_assert_nonnull (job);
+	g_assert_true (hasBackend);
+
+	pps_job_load_set_uri (PPS_JOB_LOAD (job), uri);
+
+	pps_job_run (job);
+
+	pps_job_reset (job);
+
+	pps_job_load_set_password (PPS_JOB_LOAD (job), "password");
+
+	pps_job_run (job);
+
+	document = pps_job_load_get_loaded_document (PPS_JOB_LOAD (job));
+	n_pages = pps_document_get_n_pages (document);
+
+	g_assert_nonnull (document);
+	g_assert (n_pages == 1);
+}
+
 gint
 main (gint argc,
       gchar *argv[])
 {
 	g_test_init (&argc, &argv, NULL);
 	g_test_add_func ("/libview-jobs/get_annots", get_annots);
+	g_test_add_func ("/libview-jobs/load_encrypted", load_encrypted);
 	return g_test_run ();
 }
