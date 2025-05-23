GNOME/papers

commit 062473632b32fbff7949e7de611a2debd75be953

Do not load documents from pps_document_factory functions
These functions are designed to provide the raw class, and not
actually load documents. The behavior is not documented and can cause
issues, like in the nautilus extension, where the document is then
loaded again! This API is also synchronous and can lock the main
thread of apps. So its a much worse alternative to PpsJobLoad

libview/pps-jobs.c
@@ -782,6 +782,7 @@ typedef struct _PpsJobLoadPrivate {
 	GPasswordSave password_save;
 	PpsDocumentLoadFlags flags;
 	PpsDocument *loaded_document;
+	PpsDocument *document;
 } PpsJobLoadPrivate;
 
 G_DEFINE_TYPE_WITH_PRIVATE (PpsJobLoad, pps_job_load, PPS_TYPE_JOB)
@@ -811,6 +812,7 @@ pps_job_load_dispose (GObject *object)
 	g_clear_pointer (&priv->uri, g_free);
 	g_clear_pointer (&priv->password, g_free);
 	g_clear_object (&priv->loaded_document);
+	g_clear_object (&priv->document);
 
 	G_OBJECT_CLASS (pps_job_load_parent_class)->dispose (object);
 }
@@ -851,18 +853,16 @@ pps_job_load_run (PpsJob *job)
 	/* This job may already have a document even if the job didn't complete
 	   because, e.g., a password is required - if so, just reload rather than
 	   creating a new instance */
-	if (priv->loaded_document) {
-		PpsDocument *loaded_doc = priv->loaded_document;
-
+	if (priv->document) {
 		if (priv->password) {
-			pps_document_security_set_password (PPS_DOCUMENT_SECURITY (loaded_doc),
+			pps_document_security_set_password (PPS_DOCUMENT_SECURITY (priv->document),
 			                                    priv->password);
 		}
 
 		pps_job_reset (job);
 
 		if (priv->uri) {
-			pps_document_load_full (loaded_doc,
+			pps_document_load_full (priv->document,
 			                        priv->uri,
 			                        priv->flags,
 			                        &error);
@@ -873,29 +873,36 @@ pps_job_load_run (PpsJob *job)
 			 */
 			int fd = pps_dupfd (priv->fd, &error);
 			if (fd != -1)
-				pps_document_load_fd (loaded_doc,
+				pps_document_load_fd (priv->document,
 				                      fd,
 				                      priv->flags,
 				                      &error);
 		}
 	} else {
 		if (priv->uri) {
-			priv->loaded_document =
-			    pps_document_factory_get_document_full (priv->uri,
-			                                            priv->flags,
-			                                            &error);
+			priv->document = pps_document_factory_get_document (priv->uri, &error);
+			if (priv->document != NULL &&
+			    pps_document_load_full (priv->document, priv->uri,
+			                            priv->flags,
+			                            &error))
+				priv->loaded_document = g_object_ref (priv->document);
+
 		} else {
-			/* We need to dup the FD since we may need to pass it again
-			 * if the document is reloaded, as pps_document calls
-			 * consume it.
-			 */
-			int fd = pps_dupfd (priv->fd, &error);
-			if (fd != -1)
-				priv->loaded_document =
-				    pps_document_factory_get_document_for_fd (fd,
-				                                              priv->mime_type,
-				                                              priv->flags,
-				                                              &error);
+			priv->document = pps_document_factory_get_document_for_fd (priv->fd,
+			                                                           priv->mime_type,
+			                                                           &error);
+			if (priv->document != NULL) {
+				/* We need to dup the FD since we may need to pass it again
+				 * if the document is reloaded, as pps_document_load calls
+				 * consume it.
+				 */
+				int fd = pps_dupfd (priv->fd, &error);
+				if (fd != -1 && pps_document_load_fd (priv->document,
+				                                      fd,
+				                                      priv->flags,
+				                                      &error))
+					priv->loaded_document = g_object_ref (priv->document);
+			}
 		}
 	}
 

libdocument/pps-document-factory.c
@@ -254,9 +254,8 @@ _pps_document_factory_shutdown (void)
 }
 
 /**
- * pps_document_factory_get_document_full:
+ * pps_document_factory_get_document:
  * @uri: an URI
- * @flags: flags from #PpsDocumentLoadFlags
  * @error: a #GError location to store an error, or %NULL
  *
  * Creates a #PpsDocument for the document at @uri; or, if no backend handling
@@ -268,12 +267,10 @@ _pps_document_factory_shutdown (void)
  * Returns: (transfer full): a new #PpsDocument, or %NULL
  */
 PpsDocument *
-pps_document_factory_get_document_full (const char *uri,
-                                        PpsDocumentLoadFlags flags,
-                                        GError **error)
+pps_document_factory_get_document (const char *uri,
+                                   GError **error)
 {
-	g_autoptr (PpsDocument) document;
-	int result;
+	g_autoptr (PpsDocument) document = NULL;
 	PpsCompressionType compression;
 	gchar *uri_unc = NULL;
 	GError *err = NULL;
@@ -302,18 +299,7 @@ pps_document_factory_get_document_full (const char *uri,
 			                        uri_unc,
 			                        (GDestroyNotify) free_uncompressed_uri);
 
-		result = pps_document_load_full (document, uri, flags, &err);
-		if (result == FALSE) {
-			g_assert (err);
-			if (g_error_matches (err, PPS_DOCUMENT_ERROR, PPS_DOCUMENT_ERROR_ENCRYPTED) ||
-			    g_error_matches (err, PPS_DOCUMENT_ERROR, PPS_DOCUMENT_ERROR_UNSUPPORTED_CONTENT)) {
-				g_propagate_error (error, err);
-				return g_steal_pointer (&document);
-			}
-			/* else fall through, deal with errors outside loop */
-		} else {
-			return g_steal_pointer (&document);
-		}
+		return g_steal_pointer (&document);
 	}
 
 	g_assert (err != NULL);
@@ -322,31 +308,9 @@ pps_document_factory_get_document_full (const char *uri,
 }
 
 /**
- * pps_document_factory_get_document:
- * @uri: an URI
- * @error: a #GError location to store an error, or %NULL
- *
- * Creates a #PpsDocument for the document at @uri; or, if no backend handling
- * the document's type is found, or an error occurred on opening the document,
- * returns %NULL and fills in @error.
- * If the document is encrypted, it is returned but also @error is set to
- * %PPS_DOCUMENT_ERROR_ENCRYPTED.
- *
- * Returns: (transfer full): a new #PpsDocument, or %NULL
- */
-PpsDocument *
-pps_document_factory_get_document (const char *uri, GError **error)
-{
-	return pps_document_factory_get_document_full (uri,
-	                                               PPS_DOCUMENT_LOAD_FLAG_NONE,
-	                                               error);
-}
-
-/**
  * pps_document_factory_get_document_for_fd:
  * @fd: a file descriptor
  * @mime_type: the mime type
- * @flags: flags from #PpsDocumentLoadFlags
  * @error: (allow-none): a #GError location to store an error, or %NULL
  *
  * Synchronously creates a #PpsDocument for the document from @fd using the backend
@@ -370,33 +334,18 @@ pps_document_factory_get_document (const char *uri, GError **error)
 PpsDocument *
 pps_document_factory_get_document_for_fd (int fd,
                                           const char *mime_type,
-                                          PpsDocumentLoadFlags flags,
                                           GError **error)
 {
-	g_autoptr (PpsDocument) document = NULL;
-
 	g_return_val_if_fail (fd != -1, NULL);
 	g_return_val_if_fail (error == NULL || *error == NULL, NULL);
 
 	if (mime_type == NULL) {
 		g_set_error_literal (error, G_IO_ERROR, G_IO_ERROR_NOT_SUPPORTED,
 		                     "Cannot query mime type from file descriptor");
-		close (fd);
 		return NULL;
 	}
 
-	document = pps_document_factory_new_document_for_mime_type (mime_type, error);
-	if (document == NULL) {
-		close (fd);
-		return NULL;
-	}
-
-	if (!pps_document_load_fd (document, fd, flags, error)) {
-		/* fd is now consumed */
-		return NULL;
-	}
-
-	return g_steal_pointer (&document);
+	return pps_document_factory_new_document_for_mime_type (mime_type, error);
 }
 
 static void

libdocument/pps-document-factory.h
@@ -22,13 +22,8 @@ void _pps_document_factory_shutdown (void);
 PPS_PUBLIC
 PpsDocument *pps_document_factory_get_document (const char *uri, GError **error);
 PPS_PUBLIC
-PpsDocument *pps_document_factory_get_document_full (const char *uri,
-                                                     PpsDocumentLoadFlags flags,
-                                                     GError **error);
-PPS_PUBLIC
 PpsDocument *pps_document_factory_get_document_for_fd (int fd,
                                                        const char *mime_type,
-                                                       PpsDocumentLoadFlags flags,
                                                        GError **error);
 
 PPS_PUBLIC

thumbnailer/papers-thumbnailer.c
@@ -99,7 +99,7 @@ get_local_path (GFile *file)
 static PpsDocument *
 papers_thumbnailer_get_document (GFile *file)
 {
-	PpsDocument *document = NULL;
+	g_autoptr (PpsDocument) document = NULL;
 	g_autofree gchar *uri = NULL;
 	g_autofree gchar *path = NULL;
 	g_autoptr (GFile) tmp_file = NULL;
@@ -132,7 +132,12 @@ papers_thumbnailer_get_document (GFile *file)
 		uri = g_filename_to_uri (path, NULL, NULL);
 	}
 
-	document = pps_document_factory_get_document_full (uri, PPS_DOCUMENT_LOAD_FLAG_NO_CACHE, &error);
+	document = pps_document_factory_get_document (uri, &error);
+	if (document != NULL)
+		pps_document_load_full (document, uri,
+		                        PPS_DOCUMENT_LOAD_FLAG_NO_CACHE,
+		                        &error);
+
 	if (tmp_file) {
 		if (document) {
 			g_object_weak_ref (G_OBJECT (document),
@@ -143,8 +148,8 @@ papers_thumbnailer_get_document (GFile *file)
 		}
 	}
 	if (error) {
-		if (error->domain == PPS_DOCUMENT_ERROR &&
-		    error->code == PPS_DOCUMENT_ERROR_ENCRYPTED) {
+		if (g_error_matches (error, PPS_DOCUMENT_ERROR,
+		                     PPS_DOCUMENT_ERROR_ENCRYPTED)) {
 			/* FIXME: Create a thumb for cryp docs */
 			return NULL;
 		}
@@ -152,7 +157,7 @@ papers_thumbnailer_get_document (GFile *file)
 		return NULL;
 	}
 
-	return document;
+	return g_steal_pointer (&document);
 }
 
 static gboolean

commit 1a5a448a5c1ae7085123e59b2736bfee9e132c4c

libview: Update loaded_document if the encrypted document loads
We found a crash when opening encrypted pdf files after commit
062473632b32 ("Do not load documents from pps_document_factory functions").
`priv->loaded_document` should be updated if we successfully load the
document.

```console
thread 'main' panicked at shell/src/window.rs:863:66:
called `Option::unwrap()` on a `None` value
note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace

thread 'main' panicked at library/core/src/panicking.rs:218:5:
panic in a function that cannot unwind
stack backtrace:
   0:     0x5d3dc8d699ae - <std::sys::backtrace::BacktraceLock::print::DisplayBacktrace as core::fmt::Display>::fmt::h01335b120f10a173
   1:     0x5d3dc8d8e2b3 - core::fmt::write::hae7f4d812dd997ff
   2:     0x5d3dc8d65463 - std::io::Write::write_fmt::h71c148656f8ecfb4
   3:     0x5d3dc8d697f2 - std::sys::backtrace::BacktraceLock::print::ha36866943e50b285
   4:     0x5d3dc8d6a894 - std::panicking::default_hook::{{closure}}::h120972094b1addb6
   5:     0x5d3dc8d6a672 - std::panicking::default_hook::h48776a4e6f63486c
   6:     0x5d3dc8d6afe2 - std::panicking::rust_panic_with_hook::hae94debc1194f829
   7:     0x5d3dc8d6ad56 - std::panicking::begin_panic_handler::{{closure}}::hebc6f080bf37d09a
   8:     0x5d3dc8d69ee9 - std::sys::backtrace::__rust_end_short_backtrace::hb7cf2b18d55cb87e
   9:     0x5d3dc8d6aa1f - rust_begin_unwind
  10:     0x5d3dc811d08c - core::panicking::panic_nounwind_fmt::h4183847acfdb7b84
  11:     0x5d3dc811d11b - core::panicking::panic_nounwind::h5f546ab3b094a1c8
  12:     0x5d3dc811d2c6 - core::panicking::panic_cannot_unwind::hecd80579e1084ff3
  13:     0x5d3dc84727ed - papers_view::auto::job::JobExt::connect_finished::finished_trampoline::h1bb28a4dd4a0306d
                               at /data/source/papers/rust/papers-view/src/auto/job.rs:149:9
  14:     0x7a6c80a04210 - g_closure_invoke
  15:     0x7a6c80a18963 - <unknown>
  16:     0x7a6c80a1a37c - <unknown>
  17:     0x7a6c80a201f6 - g_signal_emit_valist
  18:     0x7a6c80a202bb - g_signal_emit
  19:     0x7a6c8084fb23 - emit_finished
                               at /data/source/papers/build/../libview/pps-job.c:136:3
  20:     0x7a6c808f0f7c - <unknown>
  21:     0x7a6c808f32a5 - <unknown>
  22:     0x7a6c808f54d7 - <unknown>
  23:     0x7a6c808f5c40 - g_main_context_iteration
  24:     0x7a6c80b32d75 - g_application_run
  25:     0x5d3dc827ca2e - gio::application::ApplicationExtManual::run_with_args::habf4d91e29c08bff
                               at /data/source/papers/build/cargo-home/registry/src/index.crates.io-1949cf8c6b5b557f/gio-0.20.9/src/application.rs:29:13
  26:     0x5d3dc827cb19 - gio::application::ApplicationExtManual::run::h889d9930915b1507
                               at /data/source/papers/build/cargo-home/registry/src/index.crates.io-1949cf8c6b5b557f/gio-0.20.9/src/application.rs:22:9
  27:     0x5d3dc83744fd - papers::main::h7d9ff3f57a2d24e6
                               at /data/source/papers/shell/src/main.rs:57:5
			         28:     0x5d3dc83071cb - core::ops::function::FnOnce::call_once::h0ecc954560c5d0b0
                               at /usr/lib/rust/1.85.1/lib/rustlib/src/rust/library/core/src/ops/function.rs:250:5
  29:     0x5d3dc81322be - std::sys::backtrace::__rust_begin_short_backtrace::h1889ab9b84f56fc9
                               at /usr/lib/rust/1.85.1/lib/rustlib/src/rust/library/std/src/sys/backtrace.rs:152:18
  30:     0x5d3dc81500d1 - std::rt::lang_start::{{closure}}::h0aeaf8e9a7d9b5b0
                               at /usr/lib/rust/1.85.1/lib/rustlib/src/rust/library/std/src/rt.rs:195:18
  31:     0x5d3dc8d5e447 - std::rt::lang_start_internal::h3a0ebe45b8521e47
  32:     0x5d3dc81500aa - std::rt::lang_start::h61679e6fdb4d118e
                               at /usr/lib/rust/1.85.1/lib/rustlib/src/rust/library/std/src/rt.rs:194:17
  33:     0x5d3dc8374b9e - main
  34:     0x7a6c8055c42e - <unknown>
  35:     0x7a6c8055c4e9 - __libc_start_main
  36:     0x5d3dc811d8a5 - _start
  37:                0x0 - <unknown>
thread caused non-unwinding panic. aborting.
```

Fixes: 062473632b32

libview/pps-jobs.c
@@ -861,15 +861,16 @@ pps_job_load_run (PpsJob *job)
 		pps_job_reset (job);
 
 		if (priv->uri) {
-			pps_document_load (priv->document, priv->uri, &error);
+			if (pps_document_load (priv->document, priv->uri, &error))
+				g_set_object (&priv->loaded_document, priv->document);
 		} else {
 			/* We need to dup the FD since we may need to pass it again
 			 * if the document is reloaded, as pps_document calls
 			 * consume it.
 			 */
 			int fd = pps_dupfd (priv->fd, &error);
-			if (fd != -1)
-				pps_document_load_fd (priv->document, fd, &error);
+			if (fd != -1 && pps_document_load_fd (priv->document, fd, &error))
+				g_set_object (&priv->loaded_document, priv->document);
 		}
 	} else {
 		if (priv->uri) {