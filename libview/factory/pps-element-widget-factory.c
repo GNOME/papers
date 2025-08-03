GNOME/papers

commit 30e97488a830cda664bb0a9b07e3c1fd1a00ecc2

libview: add a widget factory to handle in-document widgets

libview/factory/pps-element-widget-factory.c
@@ -1,0 +1,229 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/* pps-element-widget-factory.c
+ * this file is part of papers, a gnome document viewer
+ *
+ * Copyright (C) 2025 Lucas Baudin <lbaudin@gnome.org>
+ */
+
+#include "pps-element-widget-factory.h"
+#include "pps-overlay.h"
+#include "pps-view-page.h"
+
+typedef struct
+{
+	PpsDocumentModel *model;
+	PpsPixbufCache *pixbuf_cache;
+
+	/* This array of PpsViewPage is owned by the PpsView */
+	GPtrArray *page_widgets;
+} PpsElementWidgetFactoryPrivate;
+
+G_DEFINE_TYPE_WITH_PRIVATE (PpsElementWidgetFactory, pps_element_widget_factory, G_TYPE_OBJECT)
+
+#define GET_PRIVATE(o) pps_element_widget_factory_get_instance_private (o)
+
+static void
+pps_element_widget_factory_init (PpsElementWidgetFactory *factory)
+{
+}
+
+static void
+pps_element_widget_factory_dispose (GObject *object)
+{
+	PpsElementWidgetFactory *factory = PPS_ELEMENT_WIDGET_FACTORY (object);
+	PpsElementWidgetFactoryPrivate *priv = GET_PRIVATE (factory);
+	PpsElementWidgetFactoryClass *factory_class = PPS_ELEMENT_WIDGET_FACTORY_GET_CLASS (factory);
+
+	for (gint i = 0; i < priv->page_widgets->len; i++) {
+		PpsViewPage *page = g_ptr_array_index (priv->page_widgets, i);
+		GtkWidget *child = gtk_widget_get_first_child (GTK_WIDGET (page));
+
+		while (child) {
+			GtkWidget *next = gtk_widget_get_next_sibling (child);
+			if (factory_class->is_managed (factory, child)) {
+				gtk_widget_unparent (child);
+			}
+			child = next;
+		}
+	}
+
+	g_clear_object (&priv->model);
+	g_clear_object (&priv->pixbuf_cache);
+
+	G_OBJECT_CLASS (pps_element_widget_factory_parent_class)->dispose (object);
+}
+
+static void
+job_finished_cb (PpsPixbufCache *pixbuf_cache,
+                 int finished_page,
+                 PpsElementWidgetFactory *factory)
+{
+	PpsElementWidgetFactoryPrivate *priv = GET_PRIVATE (factory);
+
+	for (int i = 0; i < priv->page_widgets->len; i++) {
+		PpsViewPage *page = g_ptr_array_index (priv->page_widgets, i);
+
+		if (pps_view_page_get_page (page) == finished_page) {
+			GtkWidget *child = gtk_widget_get_first_child (GTK_WIDGET (page));
+			while (child) {
+				if (PPS_IS_OVERLAY (child)) {
+					pps_overlay_update_visibility_from_state (
+					    PPS_OVERLAY (child),
+					    pps_pixbuf_cache_rendered_state (priv->pixbuf_cache, finished_page));
+				}
+				child = gtk_widget_get_next_sibling (child);
+			}
+			break;
+		}
+	}
+}
+
+static void
+acquire_widgets (PpsElementWidgetFactory *factory, PpsViewPage *view_page)
+{
+	PpsElementWidgetFactoryPrivate *priv = GET_PRIVATE (factory);
+	GtkWidget *view_page_widget = GTK_WIDGET (view_page);
+	int page_index = pps_view_page_get_page (view_page);
+
+	if (page_index < 0) {
+		return;
+	}
+
+	GList *all_widgets =
+	    PPS_ELEMENT_WIDGET_FACTORY_GET_CLASS (factory)->widgets_for_page (factory, page_index);
+
+	while (all_widgets) {
+		GtkWidget *child = GTK_WIDGET (all_widgets->data);
+		/* in case there is a race and the widget is still in another PpsViewPage */
+		if (gtk_widget_get_parent (child)) {
+			g_object_ref (child);
+			gtk_widget_unparent (child);
+			gtk_widget_set_parent (child, view_page_widget);
+			g_object_unref (child);
+		} else {
+			gtk_widget_set_parent (child, view_page_widget);
+		}
+		pps_overlay_update_visibility_from_state (
+		    PPS_OVERLAY (child),
+		    pps_pixbuf_cache_rendered_state (priv->pixbuf_cache, page_index));
+		all_widgets = all_widgets->next;
+	}
+
+	g_list_free (all_widgets);
+
+	gtk_widget_queue_resize (view_page_widget);
+	gtk_widget_queue_draw (view_page_widget);
+}
+
+static void
+on_page_changed (PpsViewPage *view_page,
+                 GParamSpec *spec,
+                 PpsElementWidgetFactory *factory)
+{
+	GtkWidget *child;
+	GtkWidget *view_page_widget = GTK_WIDGET (view_page);
+	PpsElementWidgetFactoryClass *factory_class = PPS_ELEMENT_WIDGET_FACTORY_GET_CLASS (factory);
+
+	child = gtk_widget_get_first_child (view_page_widget);
+
+	while (child) {
+		GtkWidget *next = gtk_widget_get_next_sibling (child);
+		if (factory_class->is_managed (factory, child)) {
+			gtk_widget_unparent (child);
+		}
+		child = next;
+	}
+
+	if (pps_view_page_get_page (view_page) >= 0) {
+		acquire_widgets (factory, view_page);
+	}
+}
+
+void
+pps_element_widget_factory_query_reload (PpsElementWidgetFactory *factory)
+{
+	PpsElementWidgetFactoryPrivate *priv = GET_PRIVATE (factory);
+
+	for (int i = 0; i < priv->page_widgets->len; i++) {
+		PpsViewPage *page = g_ptr_array_index (priv->page_widgets, i);
+		acquire_widgets (factory, page);
+	}
+}
+
+void
+pps_element_widget_factory_new_widget_for_page (PpsElementWidgetFactory *factory,
+                                                guint page_index,
+                                                GtkWidget *widget)
+{
+	PpsElementWidgetFactoryPrivate *priv = GET_PRIVATE (factory);
+
+	for (int i = 0; i < priv->page_widgets->len; i++) {
+		PpsViewPage *page = g_ptr_array_index (priv->page_widgets, i);
+
+		if (pps_view_page_get_page (page) == page_index) {
+			gtk_widget_set_parent (widget, GTK_WIDGET (page));
+			pps_overlay_update_visibility_from_state (
+			    PPS_OVERLAY (widget),
+			    pps_pixbuf_cache_rendered_state (priv->pixbuf_cache, page_index));
+			break;
+		}
+	}
+}
+
+void
+pps_element_widget_factory_widget_removed (PpsElementWidgetFactory *factory,
+                                           guint page_index,
+                                           GtkWidget *widget)
+{
+	gtk_widget_unparent (widget);
+}
+
+void
+pps_element_widget_factory_setup (PpsElementWidgetFactory *factory,
+                                  PpsDocumentModel *model,
+                                  PpsAnnotationsContext *annots_context,
+                                  PpsPixbufCache *pixbuf_cache,
+                                  GPtrArray *page_widgets,
+                                  PpsPageCache *page_cache)
+{
+	PpsElementWidgetFactoryPrivate *priv = GET_PRIVATE (factory);
+
+	priv->page_widgets = page_widgets;
+
+	for (gint i = 0; i < page_widgets->len; i++) {
+		g_signal_connect (g_ptr_array_index (priv->page_widgets, i),
+		                  "notify::page", G_CALLBACK (on_page_changed), factory);
+	}
+
+	if (priv->model != NULL)
+		g_signal_handlers_disconnect_by_data (priv->model, factory);
+	if (priv->pixbuf_cache != NULL)
+		g_signal_handlers_disconnect_by_data (priv->pixbuf_cache, factory);
+
+	g_set_object (&priv->model, model);
+	g_set_object (&priv->pixbuf_cache, pixbuf_cache);
+
+	g_signal_connect (priv->pixbuf_cache, "job-finished",
+	                  G_CALLBACK (job_finished_cb), factory);
+
+	PPS_ELEMENT_WIDGET_FACTORY_GET_CLASS (factory)->setup (factory,
+	                                                       model,
+	                                                       annots_context,
+	                                                       pixbuf_cache,
+	                                                       page_widgets,
+	                                                       page_cache);
+}
+
+static void
+pps_element_widget_factory_class_init (PpsElementWidgetFactoryClass *page_class)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (page_class);
+
+	object_class->dispose = pps_element_widget_factory_dispose;
+}
+
+PpsElementWidgetFactory *
+pps_element_widget_factory_new (void)
+{
+	return g_object_new (PPS_TYPE_ELEMENT_WIDGET_FACTORY, NULL);
+}

libview/factory/pps-element-widget-factory.h
@@ -1,0 +1,56 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/* pps-element-widget-factory.h
+ * this file is part of papers, a gnome document viewer
+ *
+ * Copyright (C) 2025 Lucas Baudin <lbaudin@gnome.org>
+ */
+
+#pragma once
+
+#include <papers-document.h>
+#if !defined(__PPS_PAPERS_VIEW_H_INSIDE__) && !defined(PAPERS_COMPILATION)
+#error "Only <papers-view.h> can be included directly."
+#endif
+
+#include "context/pps-annotations-context.h"
+#include "context/pps-document-model.h"
+#include "pps-pixbuf-cache.h"
+#include "pps-view-page.h"
+
+G_BEGIN_DECLS
+
+#define PPS_TYPE_ELEMENT_WIDGET_FACTORY (pps_element_widget_factory_get_type ())
+
+G_DECLARE_DERIVABLE_TYPE (PpsElementWidgetFactory, pps_element_widget_factory, PPS, ELEMENT_WIDGET_FACTORY, GObject)
+
+struct _PpsElementWidgetFactoryClass {
+	GObjectClass parent_class;
+
+	GList *(*widgets_for_page) (PpsElementWidgetFactory *factory, guint page_index);
+
+	gboolean (*is_managed) (PpsElementWidgetFactory *factory, GtkWidget *widget);
+
+	void (*setup) (PpsElementWidgetFactory *factory,
+	               PpsDocumentModel *model,
+	               PpsAnnotationsContext *annot_context,
+	               PpsPixbufCache *pixbuf_cache,
+	               GPtrArray *page_widgets,
+	               PpsPageCache *page_cache);
+};
+
+PpsElementWidgetFactory *pps_element_widget_factory_new (void);
+
+void
+pps_element_widget_factory_setup (PpsElementWidgetFactory *factory,
+                                  PpsDocumentModel *model,
+                                  PpsAnnotationsContext *annot_context,
+                                  PpsPixbufCache *pixbuf_cache,
+                                  GPtrArray *page_widgets,
+                                  PpsPageCache *page_cache);
+
+/* These three methods are used by descendants of the class and should not be used outside. */
+void pps_element_widget_factory_query_reload (PpsElementWidgetFactory *factory);
+void pps_element_widget_factory_new_widget_for_page (PpsElementWidgetFactory *factory, guint page_index, GtkWidget *widget);
+void pps_element_widget_factory_widget_removed (PpsElementWidgetFactory *factory, guint page_index, GtkWidget *widget);
+
+G_END_DECLS

libview/meson.build
@@ -2,6 +2,7 @@ libview_inc = include_directories('.')
 
 # For MacOS build
 context_inc = include_directories('context')
+factory_inc = include_directories('factory')
 
 include_subdir = join_paths(pps_include_subdir, 'libview')
 
@@ -15,6 +16,10 @@ context_headers = files(
   'context/pps-search-result.h',
   'context/pps-undo-context.h',
   'context/pps-undo-handler.h'
+)
+
+factory_headers = files (
+  'factory/pps-element-widget-factory.h',
 )
 
 headers = files(
@@ -50,6 +55,7 @@ sources = files(
   'context/pps-search-result.c',
   'context/pps-undo-context.c',
   'context/pps-undo-handler.c',
+  'factory/pps-element-widget-factory.c',
   'pps-annotation-window.c',
   'pps-colors.c',
   'pps-debug.c',
@@ -60,6 +66,7 @@ sources = files(
   'pps-job-scheduler.c',
   #'pps-link-accessible.c',
   #'pps-page-accessible.c',
+  'pps-overlay.c',
   'pps-page-cache.c',
   'pps-pixbuf-cache.c',
   'pps-print-operation.c',
@@ -148,7 +155,7 @@ libppsview = shared_library(
   'ppsview-' + pps_api_version,
   version: pps_view_version,
   sources: sources + marshal_sources + enum_sources,
-  include_directories: [top_inc, context_inc],
+  include_directories: [top_inc, context_inc, factory_inc],
   dependencies: libview_deps,
   c_args: cflags,
   install: true,

libview/pps-overlay.c
@@ -1,0 +1,47 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/* pps-overlay.c
+ * this file is part of papers, a gnome document viewer
+ *
+ * Copyright (C) 2025 Lucas Baudin <lbaudin@gnome.org>
+ */
+
+#include "pps-overlay.h"
+
+G_DEFINE_INTERFACE (PpsOverlay, pps_overlay, G_TYPE_OBJECT)
+
+static void
+pps_overlay_default_init (PpsOverlayInterface *iface)
+{
+	iface->get_area = NULL;
+	iface->update_visibility_from_state = NULL;
+}
+
+/**
+ * pps_overlay_get_area:
+ * @self: a #PpsOverlay instance
+ * @padding: (out): an additional padding for the overlay area (in view units)
+ *
+ * Gets the document area of the overlay.
+ *
+ * Returns: (transfer full): a #PpsRectangle that describes where the overlay shall be positioned
+ */
+PpsRectangle *
+pps_overlay_get_area (PpsOverlay *self, gdouble *padding)
+{
+	g_return_val_if_fail (PPS_IS_OVERLAY (self), NULL);
+	PpsOverlayInterface *iface = PPS_OVERLAY_GET_IFACE (self);
+	g_return_val_if_fail (iface->get_area != NULL, NULL);
+	return iface->get_area (self, padding);
+}
+
+void
+pps_overlay_update_visibility_from_state (PpsOverlay *self, PpsRenderAnnotsFlags state)
+{
+	g_return_if_fail (PPS_IS_OVERLAY (self));
+	PpsOverlayInterface *iface = PPS_OVERLAY_GET_IFACE (self);
+	if (iface->update_visibility_from_state) {
+		iface->update_visibility_from_state (self, state);
+	} else {
+		gtk_widget_set_visible (GTK_WIDGET (self), TRUE);
+	}
+}

libview/pps-overlay.h
@@ -1,0 +1,25 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/* pps-overlay.h
+ * this file is part of papers, a gnome document viewer
+ *
+ * Copyright (C) 2025 Lucas Baudin <lbaudin@gnome.org>
+ */
+
+#pragma once
+
+#include "pps-document-model.h"
+#include "pps-document.h"
+#include <gtk/gtk.h>
+
+#define PPS_TYPE_OVERLAY (pps_overlay_get_type ())
+G_DECLARE_INTERFACE (PpsOverlay, pps_overlay, PPS, OVERLAY, GObject)
+
+struct _PpsOverlayInterface {
+	GTypeInterface parent_iface;
+
+	PpsRectangle *(*get_area) (PpsOverlay *self, gdouble *padding);
+	void (*update_visibility_from_state) (PpsOverlay *self, PpsRenderAnnotsFlags state);
+};
+
+PpsRectangle *pps_overlay_get_area (PpsOverlay *self, gdouble *padding);
+void pps_overlay_update_visibility_from_state (PpsOverlay *self, PpsRenderAnnotsFlags state);

libview/pps-view-page.c
@@ -762,6 +762,7 @@ pps_view_page_dispose (GObject *object)
 	g_clear_object (&priv->page_cache);
 	g_clear_object (&priv->annots_context);
 	g_clear_object (&priv->search_context);
+	g_clear_object (&priv->annots_context);
 
 	G_OBJECT_CLASS (pps_view_page_parent_class)->dispose (object);
 }
@@ -807,12 +808,15 @@ pps_view_page_setup (PpsViewPage *page,
 		g_signal_handlers_disconnect_by_data (priv->pixbuf_cache, page);
 	if (priv->search_context != NULL)
 		g_signal_handlers_disconnect_by_data (priv->search_context, page);
+	if (priv->annots_context != NULL)
+		g_signal_handlers_disconnect_by_data (priv->annots_context, page);
 
 	g_set_object (&priv->model, model);
 	g_set_object (&priv->annots_context, annots_context);
 	g_set_object (&priv->search_context, search_context);
 	g_set_object (&priv->page_cache, page_cache);
 	g_set_object (&priv->pixbuf_cache, pixbuf_cache);
+	g_set_object (&priv->annots_context, annots_context);
 	pps_view_page_set_page (page, -1);
 
 	g_signal_connect_swapped (priv->model, "notify::scale",

libview/pps-view-private.h
@@ -11,6 +11,7 @@
 #endif
 
 #include "adwaita.h"
+#include "factory/pps-element-widget-factory.h"
 #include "pps-form-field.h"
 #include "pps-image.h"
 #include "pps-jobs.h"
@@ -73,6 +74,10 @@ typedef struct
 	gdouble stop_x;
 	gdouble stop_y;
 } SigningInfo;
+
+enum {
+	WIDGET_FACTORY_COUNT
+};
 
 typedef struct _PpsViewPrivate {
 	/* Find */
@@ -171,6 +176,9 @@ typedef struct _PpsViewPrivate {
 	/* Signing Info */
 	SigningInfo signing_info;
 	GtkEventController *signing_drag_gesture;
+
+	/* Widgets Factory */
+	PpsElementWidgetFactory *widget_factories[WIDGET_FACTORY_COUNT];
 } PpsViewPrivate;
 
 struct _PpsViewClass {

libview/pps-view.c
@@ -740,8 +740,9 @@ view_update_range_and_current_page (PpsView *view)
 				page_index = priv->start_page - start_widget + i;
 		}
 
-		if (page_index != -1)
+		if (page_index != -1) {
 			pps_view_page_set_page (page, page_index);
+		}
 	}
 
 #if 0
@@ -6072,6 +6073,10 @@ pps_view_dispose (GObject *object)
 		g_clear_object (&priv->model);
 	}
 
+	for (int i = 0; i < WIDGET_FACTORY_COUNT; i++) {
+		g_clear_object (&priv->widget_factories[i]);
+	}
+
 	g_clear_object (&priv->pixbuf_cache);
 	g_clear_object (&priv->page_cache);
 	g_clear_object (&priv->scroll_animation_vertical);
@@ -6730,6 +6735,15 @@ setup_caches (PpsView *view)
 	                              PPS_PAGE_DATA_INCLUDE_TEXT |
 	                              PPS_PAGE_DATA_INCLUDE_TEXT_ATTRS |
 	                              PPS_PAGE_DATA_INCLUDE_TEXT_LOG_ATTRS);
+
+	for (int i = 0; i < WIDGET_FACTORY_COUNT; i++) {
+		pps_element_widget_factory_setup (priv->widget_factories[i],
+		                                  priv->model,
+		                                  priv->annots_context,
+		                                  priv->pixbuf_cache,
+		                                  priv->page_widgets,
+		                                  priv->page_cache);
+	}
 }
 
 static void

commit ef417457d02efd8e2c47a0326c1f0339af453772

libview: Don't leak the widget list
Valgrind reports the following memory loss:

```console
==46580== 24 bytes in 1 blocks are definitely lost in loss record 5,102 of 23,088
==46580==    at 0x5A417A8: malloc (vg_replace_malloc.c:446)
==46580==    by 0x6B0DC3A: g_malloc (gmem.c:100)
==46580==    by 0x6B01AAF: g_list_append (glist.c:203)
==46580==    by 0x6C27BE2: pps_form_widget_factory_widgets_for_page (pps-form-widget-factory.c:82)
==46580==    by 0x6C2717C: acquire_widgets (pps-element-widget-factory.c:95)
==46580==    by 0x6C2732F: on_page_changed (pps-element-widget-factory.c:140)
==46580==    by 0x6A5B839: g_closure_invoke (gclosure.c:835)
==46580==    by 0x6A7A42A: signal_emit_unlocked_R.isra.0 (gsignal.c:3902)
==46580==    by 0x6A7C48E: signal_emit_valist_unlocked (gsignal.c:3534)
==46580==    by 0x6A7C708: g_signal_emit_valist (gsignal.c:3277)
==46580==    by 0x6A7C7C3: g_signal_emit (gsignal.c:3597)
==46580==    by 0x6A67BA5: g_object_dispatch_properties_changed.lto_priv.0 (gobject.c:1851)
==46580==
==46580== 24 bytes in 1 blocks are definitely lost in loss record 5,103 of 23,088
==46580==    at 0x5A417A8: malloc (vg_replace_malloc.c:446)
==46580==    by 0x6B0DC3A: g_malloc (gmem.c:100)
==46580==    by 0x6B01AAF: g_list_append (glist.c:203)
==46580==    by 0x6C27BE2: pps_form_widget_factory_widgets_for_page (pps-form-widget-factory.c:82)
==46580==    by 0x6C2717C: acquire_widgets (pps-element-widget-factory.c:95)
==46580==    by 0x6C27389: pps_element_widget_factory_query_reload (pps-element-widget-factory.c:151)
==46580==    by 0x6C27C22: on_page_changed (pps-form-widget-factory.c:92)
==46580==    by 0x6A5B839: g_closure_invoke (gclosure.c:835)
==46580==    by 0x6A7A42A: signal_emit_unlocked_R.isra.0 (gsignal.c:3902)
==46580==    by 0x6A7C48E: signal_emit_valist_unlocked (gsignal.c:3534)
==46580==    by 0x6A7C708: g_signal_emit_valist (gsignal.c:3277)
==46580==    by 0x6A7C7C3: g_signal_emit (gsignal.c:3597)
```

Fixes: 30e97488a830

libview/factory/pps-element-widget-factory.c
@@ -86,12 +86,13 @@ acquire_widgets (PpsElementWidgetFactory *factory, PpsViewPage *view_page)
 	PpsElementWidgetFactoryPrivate *priv = GET_PRIVATE (factory);
 	GtkWidget *view_page_widget = GTK_WIDGET (view_page);
 	int page_index = pps_view_page_get_page (view_page);
+	GList *all_widgets, *widget_list;
 
 	if (page_index < 0) {
 		return;
 	}
 
-	GList *all_widgets =
+	all_widgets = widget_list =
 	    PPS_ELEMENT_WIDGET_FACTORY_GET_CLASS (factory)->widgets_for_page (factory, page_index);
 
 	while (all_widgets) {
@@ -111,7 +112,7 @@ acquire_widgets (PpsElementWidgetFactory *factory, PpsViewPage *view_page)
 		all_widgets = all_widgets->next;
 	}
 
-	g_list_free (all_widgets);
+	g_list_free (widget_list);
 
 	gtk_widget_queue_resize (view_page_widget);
 	gtk_widget_queue_draw (view_page_widget);