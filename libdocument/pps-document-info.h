GNOME/papers

commit 010fd236f44dc181933479b7f7684d785b451f8f

shell, nautilus: Fix memory leak caused by PpsDocumentInfo
The pps_document_get_info is essentially transfer full despite the wrong
GI annotation markup.

Signed-off-by: Qiu Wenbo <qiuwenbo@gnome.org>

libdocument/pps-document.c
@@ -660,7 +660,7 @@ pps_document_get_page_label (PpsDocument *document,
  *
  * Returns the #PpsDocumentInfo for the document.
  *
- * Returns: (transfer none): a #PpsDocumentInfo
+ * Returns: (transfer full): a #PpsDocumentInfo
  */
 PpsDocumentInfo *
 pps_document_get_info (PpsDocument *document)
@@ -764,7 +764,7 @@ const gchar *
 pps_document_get_title (PpsDocument *document)
 {
 	g_return_val_if_fail (PPS_IS_DOCUMENT (document), NULL);
-	PpsDocumentInfo *info = pps_document_get_info (document);
+	g_autofree PpsDocumentInfo *info = pps_document_get_info (document);
 
 	return (info->fields_mask & PPS_DOCUMENT_INFO_TITLE) ?
 		info->title : NULL;

nautilus/pps-nautilus-extension.c
@@ -59,7 +59,7 @@ G_DEFINE_DYNAMIC_TYPE_EXTENDED (PpsDocumentPropertiesModelProvider,
 static GListModel *
 build_properties (PpsDocument *document)
 {
-	PpsDocumentInfo *info = pps_document_get_info (document);
+	g_autofree PpsDocumentInfo *info = pps_document_get_info (document);
 	GListStore *model = g_list_store_new (NAUTILUS_TYPE_PROPERTIES_ITEM);
 	const char *uri = pps_document_get_uri (document);
 	GDateTime *datetime = NULL;

shell/pps-window.c
@@ -306,7 +306,7 @@ pps_window_update_actions_sensitivity (PpsWindow *pps_window)
 	PpsWindowPrivate *priv = GET_PRIVATE (pps_window);
 	PpsDocument *document = priv->document;
 	PpsView     *view = PPS_VIEW (priv->view);
-	const PpsDocumentInfo *info = NULL;
+	g_autofree PpsDocumentInfo *info = NULL;
 	gboolean has_document = FALSE;
 	gboolean ok_to_print = TRUE;
 	gboolean ok_to_copy = TRUE;
@@ -1351,7 +1351,7 @@ static void
 pps_window_set_document_metadata (PpsWindow *window)
 {
 	PpsWindowPrivate *priv = GET_PRIVATE (window);
-	const PpsDocumentInfo *info;
+	g_autofree PpsDocumentInfo *info = NULL;
 
 	if (!priv->metadata)
 		return;
@@ -1372,7 +1372,7 @@ static void
 pps_window_set_document (PpsWindow *pps_window, PpsDocument *document)
 {
 	PpsWindowPrivate *priv = GET_PRIVATE (pps_window);
-	const PpsDocumentInfo *info;
+	g_autofree PpsDocumentInfo *info = NULL;
 
 	if (priv->document == document)
 		return;

commit b0da866c76e9236e2804c12acc9d1c3e4a902a47

libdocument, nautilus: Don't leak PpsDocumentInfo
Valgrind reports the following memory loss:

```console
==88196== 3 bytes in 1 blocks are definitely lost in loss record 74 of 23,081
==88196==    at 0x5A417A8: malloc (vg_replace_malloc.c:446)
==88196==    by 0x6B0DC3A: g_malloc (gmem.c:100)
==88196==    by 0x6B2B59B: g_strdup (gstrfuncs.c:323)
==88196==    by 0x2866E360: g_strdup_inline (gstrfuncs.h:321)
==88196==    by 0x2866E360: pdf_document_get_info (pps-poppler.c:744)
==88196==    by 0x6C90728: pps_document_get_info (pps-document.c:598)
==88196==    by 0x6C909C9: pps_document_get_title (pps-document.c:691)
==88196==    by 0x4160975: papers_document::auto::document::DocumentExt::title (document.rs:297)
==88196==    by 0x4287339: papers::document_view::io::<impl papers::document_view::imp::PpsDocumentView>::doc_title::{{closure}} (io.rs:150)
==88196==    by 0x4371930: core::option::Option<T>::and_then (option.rs:1477)
==88196==    by 0x42520B2: papers::document_view::io::<impl papers::document_view::imp::PpsDocumentView>::doc_title (io.rs:149)
==88196==    by 0x4252187: papers::document_view::io::<impl papers::document_view::imp::PpsDocumentView>::update_title (io.rs:173)
==88196==    by 0x425317A: papers::document_view::io::<impl papers::document_view::imp::PpsDocumentView>::set_document (io.rs:315)
==88196==
==88196== 3 bytes in 1 blocks are definitely lost in loss record 75 of 23,081
==88196==    at 0x5A417A8: malloc (vg_replace_malloc.c:446)
==88196==    by 0x6B0DC3A: g_malloc (gmem.c:100)
==88196==    by 0x6B2B59B: g_strdup (gstrfuncs.c:323)
==88196==    by 0x2866E3CC: g_strdup_inline (gstrfuncs.h:321)
==88196==    by 0x2866E3CC: pdf_document_get_info (pps-poppler.c:747)
==88196==    by 0x6C90728: pps_document_get_info (pps-document.c:598)
==88196==    by 0x6C909C9: pps_document_get_title (pps-document.c:691)
==88196==    by 0x4160975: papers_document::auto::document::DocumentExt::title (document.rs:297)
==88196==    by 0x4287339: papers::document_view::io::<impl papers::document_view::imp::PpsDocumentView>::doc_title::{{closure}} (io.rs:150)
==88196==    by 0x4371930: core::option::Option<T>::and_then (option.rs:1477)
==88196==    by 0x42520B2: papers::document_view::io::<impl papers::document_view::imp::PpsDocumentView>::doc_title (io.rs:149)
==88196==    by 0x4252187: papers::document_view::io::<impl papers::document_view::imp::PpsDocumentView>::update_title (io.rs:173)
==88196==    by 0x425317A: papers::document_view::io::<impl papers::document_view::imp::PpsDocumentView>::set_document (io.rs:315)
==88196==
==88196== 51 bytes in 4 blocks are definitely lost in loss record 13,133 of 23,081
==88196==    at 0x5A417A8: malloc (vg_replace_malloc.c:446)
==88196==    by 0x6B0DC3A: g_malloc (gmem.c:100)
==88196==    by 0x6B2B59B: g_strdup (gstrfuncs.c:323)
==88196==    by 0x6A8AE0E: UnknownInlinedFun (gstrfuncs.h:321)
==88196==    by 0x6A8AE0E: value_lcopy_string.lto_priv.0 (gvaluetypes.c:316)
==88196==    by 0x6A70CE9: UnknownInlinedFun (gobject.c:3294)
==88196==    by 0x6A70CE9: g_object_get_valist (gobject.c:3264)
==88196==    by 0x6A7112C: g_object_get (gobject.c:3389)
==88196==    by 0x2866DDCF: pdf_document_get_info (pps-poppler.c:603)
==88196==    by 0x6C90728: pps_document_get_info (pps-document.c:598)
==88196==    by 0x6C909C9: pps_document_get_title (pps-document.c:691)
==88196==    by 0x4160975: papers_document::auto::document::DocumentExt::title (document.rs:297)
==88196==    by 0x4287339: papers::document_view::io::<impl papers::document_view::imp::PpsDocumentView>::doc_title::{{closure}} (io.rs:150)
==88196==    by 0x4371930: core::option::Option<T>::and_then (option.rs:1477)
==88196==
```

Fixes: 010fd236f44d

libdocument/pps-document-info.h
@@ -192,3 +192,6 @@ PPS_PUBLIC
 const gchar *pps_document_license_get_web_statement (PpsDocumentLicense *license);
 
 G_END_DECLS
+
+/* Automatic Cleanup */
+G_DEFINE_AUTOPTR_CLEANUP_FUNC (PpsDocumentInfo, pps_document_info_free)

libdocument/pps-document.c
@@ -684,13 +684,19 @@ pps_document_get_uri (PpsDocument *document)
 	return priv->uri;
 }
 
+/**
+ * pps_document_get_title:
+ * @document: an #PpsDocument
+ *
+ * Returns: (transfer full):
+ */
 const gchar *
 pps_document_get_title (PpsDocument *document)
 {
 	g_return_val_if_fail (PPS_IS_DOCUMENT (document), NULL);
-	g_autofree PpsDocumentInfo *info = pps_document_get_info (document);
+	g_autoptr (PpsDocumentInfo) info = pps_document_get_info (document);
 
-	return (info->fields_mask & PPS_DOCUMENT_INFO_TITLE) ? info->title : NULL;
+	return (info->fields_mask & PPS_DOCUMENT_INFO_TITLE) ? g_strdup (info->title) : NULL;
 }
 
 gboolean

nautilus/pps-nautilus-extension.c
@@ -37,7 +37,7 @@ G_DEFINE_DYNAMIC_TYPE_EXTENDED (PpsDocumentPropertiesModelProvider,
 static GListModel *
 build_properties (PpsDocument *document)
 {
-	g_autofree PpsDocumentInfo *info = pps_document_get_info (document);
+	g_autoptr (PpsDocumentInfo) info = pps_document_get_info (document);
 	GListStore *model = g_list_store_new (NAUTILUS_TYPE_PROPERTIES_ITEM);
 	const char *uri = pps_document_get_uri (document);
 	GDateTime *datetime = NULL;

rust/papers-document/src/auto/document.rs
@@ -294,7 +294,7 @@ pub trait DocumentExt: IsA<Document> + 'static {
     #[doc(alias = "pps_document_get_title")]
     #[doc(alias = "get_title")]
     fn title(&self) -> Option<glib::GString> {
-        unsafe { from_glib_none(ffi::pps_document_get_title(self.as_ref().to_glib_none().0)) }
+        unsafe { from_glib_full(ffi::pps_document_get_title(self.as_ref().to_glib_none().0)) }
     }
 
     #[doc(alias = "pps_document_get_uri")]

rust/papers-document/src/auto/functions.rs
@@ -180,11 +180,11 @@ pub fn xfer_uri_simple(from: &str, to: &str) -> Result<(), glib::Error> {
 }
 
 #[doc(alias = "pps_xmp_parse")]
-pub fn xmp_parse(xmp: &str, size: usize, info: &mut DocumentInfo) -> bool {
+pub fn xmp_parse(metadata: &str, size: usize, info: &mut DocumentInfo) -> bool {
     assert_initialized_main_thread!();
     unsafe {
         from_glib(ffi::pps_xmp_parse(
-            xmp.to_glib_none().0,
+            metadata.to_glib_none().0,
             size,
             info.to_glib_none_mut().0,
         ))

rust/papers-document/src/auto/mapping_list.rs
@@ -3,7 +3,7 @@
 // from ../pps-girs
 // DO NOT EDIT
 
-use crate::{ffi, Mapping};
+use crate::{ffi, Mapping, Point};
 use glib::translate::*;
 
 glib::wrapper! {
@@ -29,13 +29,18 @@ impl MappingList {
     //}
 
     #[doc(alias = "pps_mapping_list_get")]
-    pub fn get(&self, x: f64, y: f64) -> Option<Mapping> {
-        unsafe { from_glib_none(ffi::pps_mapping_list_get(self.to_glib_none().0, x, y)) }
+    pub fn get(&self, point: &Point) -> Option<Mapping> {
+        unsafe {
+            from_glib_none(ffi::pps_mapping_list_get(
+                self.to_glib_none().0,
+                point.to_glib_none().0,
+            ))
+        }
     }
 
     //#[doc(alias = "pps_mapping_list_get_data")]
     //#[doc(alias = "get_data")]
-    //pub fn data(&self, x: f64, y: f64) -> /*Unimplemented*/Option<Basic: Pointer> {
+    //pub fn data(&self, point: &Point) -> /*Unimplemented*/Option<Basic: Pointer> {
     //    unsafe { TODO: call ffi:pps_mapping_list_get_data() }
     //}
 

rust/papers-document/sys/src/lib.rs
@@ -186,8 +186,7 @@ pub const PPS_TRANSITION_EFFECT_UNCOVER: PpsTransitionEffectType = 10;
 pub const PPS_TRANSITION_EFFECT_FADE: PpsTransitionEffectType = 11;
 
 // Constants
-pub const PPS_MAJOR_VERSION: c_int = 48;
-pub const PPS_MINOR_VERSION: c_int = 0;
+pub const PPS_MAJOR_VERSION: c_int = 49;
 
 // Flags
 pub type PpsDocumentInfoFields = c_uint;
@@ -2340,13 +2339,11 @@ extern "C" {
     ) -> *mut PpsMapping;
     pub fn pps_mapping_list_get(
         mapping_list: *mut PpsMappingList,
-        x: c_double,
-        y: c_double,
+        point: *const PpsPoint,
     ) -> *mut PpsMapping;
     pub fn pps_mapping_list_get_data(
         mapping_list: *mut PpsMappingList,
-        x: c_double,
-        y: c_double,
+        point: *const PpsPoint,
     ) -> gpointer;
     pub fn pps_mapping_list_get_list(mapping_list: *mut PpsMappingList) -> *mut glib::GList;
     pub fn pps_mapping_list_get_page(mapping_list: *mut PpsMappingList) -> c_uint;
@@ -3407,6 +3404,10 @@ extern "C" {
         to: *const c_char,
         error: *mut *mut glib::GError,
     ) -> gboolean;
-    pub fn pps_xmp_parse(xmp: *const c_char, size: size_t, info: *mut PpsDocumentInfo) -> gboolean;
+    pub fn pps_xmp_parse(
+        metadata: *const c_char,
+        size: size_t,
+        info: *mut PpsDocumentInfo,
+    ) -> gboolean;
 
 }

rust/papers-view/src/auto/annotations_context.rs
@@ -44,6 +44,22 @@ pub trait AnnotationsContextExt: IsA<AnnotationsContext> + 'static {
     //    unsafe { TODO: call ffi:pps_annotations_context_add_annotation_sync() }
     //}
 
+    #[cfg(feature = "v49")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "v49")))]
+    #[doc(alias = "pps_annotations_context_get_annot_at_doc_point")]
+    #[doc(alias = "get_annot_at_doc_point")]
+    fn annot_at_doc_point(
+        &self,
+        doc_point: &papers_document::DocumentPoint,
+    ) -> Option<papers_document::Annotation> {
+        unsafe {
+            from_glib_none(ffi::pps_annotations_context_get_annot_at_doc_point(
+                self.as_ref().to_glib_none().0,
+                doc_point.to_glib_none().0,
+            ))
+        }
+    }
+
     #[doc(alias = "pps_annotations_context_get_annots_model")]
     #[doc(alias = "get_annots_model")]
     fn annots_model(&self) -> Option<gio::ListModel> {

rust/papers-view/sys/Cargo.toml
@@ -31,6 +31,9 @@ version = "46"
 [package.metadata.system-deps.papers_view_4_0.v48]
 version = "48"
 
+[package.metadata.system-deps.papers_view_4_0.v49]
+version = "49"
+
 [package.metadata.docs.rs]
 rustc-args = ["--cfg", "docsrs"]
 rustdoc-args = ["--cfg", "docsrs", "--generate-link-to-definition"]
@@ -80,3 +83,4 @@ tempfile = "3"
 v42 = []
 v46 = []
 v48 = ["v46"]
+v49 = ["v48"]

rust/papers-view/sys/src/lib.rs
@@ -981,6 +981,12 @@ extern "C" {
         color: *const gdk::GdkRGBA,
         user_data: gpointer,
     ) -> *mut papers_document::PpsAnnotation;
+    #[cfg(feature = "v49")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "v49")))]
+    pub fn pps_annotations_context_get_annot_at_doc_point(
+        self_: *mut PpsAnnotationsContext,
+        doc_point: *const papers_document::PpsDocumentPoint,
+    ) -> *mut papers_document::PpsAnnotation;
     pub fn pps_annotations_context_get_annots_model(
         self_: *mut PpsAnnotationsContext,
     ) -> *mut gio::GListModel;

rust/papers-document/sys/tests/abi.rs
@@ -1023,8 +1023,7 @@ const RUST_CONSTANTS: &[(&str, &str)] = &[
     ("(gint) PPS_LINK_DEST_TYPE_PAGE_LABEL", "7"),
     ("(gint) PPS_LINK_DEST_TYPE_UNKNOWN", "8"),
     ("(gint) PPS_LINK_DEST_TYPE_XYZ", "1"),
-    ("PPS_MAJOR_VERSION", "48"),
-    ("PPS_MINOR_VERSION", "0"),
+    ("PPS_MAJOR_VERSION", "49"),
     ("(guint) PPS_RENDER_ANNOTS_3D", "16777216"),
     ("(guint) PPS_RENDER_ANNOTS_ALL", "67108863"),
     ("(guint) PPS_RENDER_ANNOTS_CARET", "8192"),

rust/papers-document/sys/tests/constant.c
@@ -155,7 +155,6 @@ int main() {
     PRINT_CONSTANT((gint) PPS_LINK_DEST_TYPE_UNKNOWN);
     PRINT_CONSTANT((gint) PPS_LINK_DEST_TYPE_XYZ);
     PRINT_CONSTANT(PPS_MAJOR_VERSION);
-    PRINT_CONSTANT(PPS_MINOR_VERSION);
     PRINT_CONSTANT((guint) PPS_RENDER_ANNOTS_3D);
     PRINT_CONSTANT((guint) PPS_RENDER_ANNOTS_ALL);
     PRINT_CONSTANT((guint) PPS_RENDER_ANNOTS_CARET);