GNOME/papers

commit b8f21fa94885c78784036f430c2ab1707f365ceb

libdocument: Make PpsCertificateInfo a class
Make PpsCertificateInfo a class as we are adding quite a lot of
members to the class and handling them as properties is
more convenient in Rust.

Also remove some getters as these usually return "const"
values but when getting a property via g_object_get() these
are duplicated/reffed so once this get into Rust it would be
inconsistent as Rust could implement getting of the property
via the getter or via the g_object_get(). This also shrinks API
a little.

libdocument/backend/pdf/pps-poppler.c
@@ -3957,7 +3957,9 @@ find_poppler_certificate_info (PpsCertificateInfo *certificate_info)
 {
 	GList *signing_certificates = poppler_get_available_signing_certificates ();
 	PopplerCertificateInfo *ret = NULL;
-	const char *certificate_id = pps_certificate_info_get_id (certificate_info);
+	g_autofree char *certificate_id = NULL;
+
+	g_object_get (certificate_info, "id", &certificate_id, NULL);
 
 	for (GList *list = signing_certificates; list != NULL && list->data != NULL; list = list->next) {
 		PopplerCertificateInfo *certificate_info = list->data;
@@ -3982,12 +3984,17 @@ pdf_document_signatures_sign (PpsDocumentSignatures *document,
 {
 	PdfDocument *self = PDF_DOCUMENT (document);
 	PopplerSigningData *signing_data = poppler_signing_data_new ();
-	PopplerCertificateInfo *cert_info = find_poppler_certificate_info (pps_signature_get_certificate_info (signature));
+	PopplerCertificateInfo *cert_info;
+	PpsCertificateInfo *cinfo = NULL;
 	PopplerRectangle signing_rect;
 	PpsRectangle *rect;
 	PopplerColor color;
 	gdouble height;
 	GdkRGBA rgba;
+
+	g_object_get (signature, "certificate-info", &cinfo, NULL);
+	cert_info = find_poppler_certificate_info (cinfo);
+	g_object_unref (cinfo);
 
 	g_assert (cert_info);
 
@@ -4091,7 +4098,10 @@ pdf_document_get_available_signing_certifcates (PpsDocumentSignatures *document)
 
 	for (GList *list = signing_certs; list != NULL && list->data != NULL; list = list->next) {
 		PopplerCertificateInfo *info = list->data;
-		PpsCertificateInfo *cert_info = pps_certificate_info_new (poppler_certificate_info_get_id (info), poppler_certificate_info_get_subject_common_name (info));
+		PpsCertificateInfo *cert_info = g_object_new (PPS_TYPE_CERTIFICATE_INFO,
+		                                              "id", poppler_certificate_info_get_id (info),
+		                                              "subject-common-name", poppler_certificate_info_get_subject_common_name (info),
+		                                              NULL);
 
 		ev_certs = g_list_append (ev_certs, cert_info);
 	}
@@ -4113,9 +4123,12 @@ pdf_document_get_certificate_info (PpsDocumentSignatures *document,
 
 	for (list = pdf_document_get_available_signing_certifcates (document); list != NULL && list->data != NULL; list = list->next) {
 		PpsCertificateInfo *cert_info = list->data;
+		g_autofree char *certificate_id = NULL;
 
-		if (g_strcmp0 (pps_certificate_info_get_id (cert_info), id) == 0) {
-			info = pps_certificate_info_copy (cert_info);
+		g_object_get (cert_info, "id", &certificate_id, NULL);
+
+		if (g_strcmp0 (certificate_id, id) == 0) {
+			info = g_object_ref (cert_info);
 			break;
 		}
 	}

libdocument/pps-signature.c
@@ -57,7 +57,8 @@ enum {
 	PROP_SIGNER_NAME,
 	PROP_SIGNATURE_STATUS,
 	PROP_CERTIFICATE_STATUS,
-	PROP_SIGN_TIME
+	PROP_SIGN_TIME,
+	PROP_CERTIFICATE_INFO
 };
 
 G_DEFINE_TYPE_WITH_PRIVATE (PpsSignature, pps_signature, G_TYPE_OBJECT);
@@ -69,7 +70,7 @@ pps_signature_finalize (GObject *object)
 	PpsSignature *self = PPS_SIGNATURE (object);
 	PpsSignaturePrivate *priv = GET_SIG_PRIVATE (self);
 
-	g_clear_pointer (&priv->certificate_info, pps_certificate_info_free);
+	g_clear_object (&priv->certificate_info);
 	g_clear_pointer (&priv->destination_file, g_free);
 	g_clear_pointer (&priv->password, g_free);
 	g_clear_pointer (&priv->signature, g_free);
@@ -126,6 +127,11 @@ pps_signature_set_property (GObject *object,
 			g_date_time_ref (priv->signature_time);
 		break;
 
+	case PROP_CERTIFICATE_INFO:
+		g_clear_object (&priv->certificate_info);
+		priv->certificate_info = g_object_ref (g_value_get_object (value));
+		break;
+
 	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID (object,
 		                                   property_id,
@@ -158,6 +164,10 @@ pps_signature_get_property (GObject *object,
 
 	case PROP_SIGN_TIME:
 		g_value_set_pointer (value, priv->signature_time);
+		break;
+
+	case PROP_CERTIFICATE_INFO:
+		g_value_set_object (value, priv->certificate_info);
 		break;
 
 	default:
@@ -220,25 +230,17 @@ pps_signature_class_init (PpsSignatureClass *klass)
 	                                                         G_PARAM_CONSTRUCT_ONLY |
 	                                                         G_PARAM_STATIC_STRINGS));
 
+	g_object_class_install_property (g_object_class,
+	                                 PROP_CERTIFICATE_INFO,
+	                                 g_param_spec_object ("certificate-info",
+	                                                      "CertificateInfo",
+	                                                      "Information about certificate used for the signature",
+	                                                      PPS_TYPE_CERTIFICATE_INFO,
+	                                                      G_PARAM_READWRITE |
+	                                                          G_PARAM_CONSTRUCT_ONLY |
+	                                                          G_PARAM_STATIC_STRINGS));
+
 	g_object_class->finalize = pps_signature_finalize;
-}
-
-void
-pps_signature_set_certificate_info (PpsSignature *self,
-                                    const PpsCertificateInfo *certificate_info)
-{
-	PpsSignaturePrivate *priv = GET_SIG_PRIVATE (self);
-
-	g_clear_object (&priv->certificate_info);
-	priv->certificate_info = pps_certificate_info_copy (certificate_info);
-}
-
-PpsCertificateInfo *
-pps_signature_get_certificate_info (PpsSignature *self)
-{
-	PpsSignaturePrivate *priv = GET_SIG_PRIVATE (self);
-
-	return priv->certificate_info;
 }
 
 void
@@ -638,50 +640,12 @@ gboolean
 pps_signature_is_valid (PpsSignature *self)
 {
 	PpsSignaturePrivate *priv = GET_SIG_PRIVATE (self);
+	PpsCertificateInfo *certificate_info = NULL;
+	PpsCertificateStatus certificate_status = PPS_CERTIFICATE_STATUS_NOT_VERIFIED;
 
-	return priv->certificate_status == PPS_CERTIFICATE_STATUS_TRUSTED && priv->signature_status == PPS_SIGNATURE_STATUS_VALID;
-}
+	g_object_get (self, "certificate-info", &certificate_info, NULL);
+	if (certificate_info != NULL)
+		g_object_get (certificate_info, "status", &certificate_status, NULL);
 
-/* Certificate Info */
-
-G_DEFINE_BOXED_TYPE (PpsCertificateInfo, pps_certificate_info, pps_certificate_info_copy, pps_certificate_info_free)
-
-PpsCertificateInfo *
-pps_certificate_info_new (const char *id, const char *subject_common_name)
-{
-	PpsCertificateInfo *info = (PpsCertificateInfo *) g_malloc0 (sizeof (PpsCertificateInfo));
-
-	info->id = g_strdup (id);
-	info->subject_common_name = g_strdup (subject_common_name);
-	return info;
-}
-
-PpsCertificateInfo *
-pps_certificate_info_copy (const PpsCertificateInfo *info)
-
-{
-	PpsCertificateInfo *info_copy = (PpsCertificateInfo *) g_malloc0 (sizeof (PpsCertificateInfo));
-
-	info_copy->id = g_strdup (info->id);
-	info_copy->subject_common_name = g_strdup (info->subject_common_name);
-	return info_copy;
-}
-
-void
-pps_certificate_info_free (PpsCertificateInfo *info)
-{
-	g_clear_pointer (&info->id, g_free);
-	g_clear_pointer (&info->subject_common_name, g_free);
-}
-
-const char *
-pps_certificate_info_get_id (const PpsCertificateInfo *info)
-{
-	return info->id;
-}
-
-const char *
-pps_certificate_info_get_subject_common_name (const PpsCertificateInfo *info)
-{
-	return info->subject_common_name;
+	return certificate_status == PPS_CERTIFICATE_STATUS_TRUSTED && priv->signature_status == PPS_SIGNATURE_STATUS_VALID;
 }

libdocument/meson.build
@@ -5,6 +5,7 @@ include_subdir = join_paths(pps_include_subdir, 'libdocument')
 headers = files(
   'pps-annotation.h',
   'pps-attachment.h',
+  'pps-certificate-info.h',
   'pps-document-annotations.h',
   'pps-document-attachments.h',
   'pps-document-factory.h',
@@ -54,6 +55,7 @@ sources = files(
   'pps-annotation.c',
   'pps-attachment.c',
   'pps-backend-info.c',
+  'pps-certificate-info.c',
   'pps-document.c',
   'pps-document-annotations.c',
   'pps-document-attachments.c',

libdocument/pps-certificate-info.c
@@ -1,0 +1,319 @@
+/* pps-certificate-info.c
+ * this file is part of papers, a gnome document viewer
+ *
+ * Copyright (C) 2024 Jan-Michael Brummer <jan-michael.brummer1@volkswagen.de>
+ * Copyright (C) 2024 Marek Kasik <mkasik@redhat.com>
+ *
+ * Papers is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Papers is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "config.h"
+
+#include "pps-certificate-info.h"
+#include "pps-document-signatures.h"
+#include "pps-document-type-builtins.h"
+
+enum {
+	PROP_0,
+	PROP_ID,
+	PROP_SUBJECT_COMMON_NAME,
+	PROP_SUBJECT_EMAIL,
+	PROP_SUBJECT_ORGANIZATION,
+	PROP_ISSUER_COMMON_NAME,
+	PROP_ISSUER_EMAIL,
+	PROP_ISSUER_ORGANIZATION,
+	PROP_ISSUANCE_TIME,
+	PROP_EXPIRATION_TIME,
+	PROP_STATUS
+};
+
+typedef struct {
+	gchar *id;
+
+	gchar *subject_common_name;
+	gchar *subject_email;
+	gchar *subject_organization;
+
+	gchar *issuer_common_name;
+	gchar *issuer_email;
+	gchar *issuer_organization;
+
+	GDateTime *issuance_time;
+	GDateTime *expiration_time;
+
+	PpsCertificateStatus status;
+} PpsCertificateInfoPrivate;
+
+G_DEFINE_TYPE_WITH_PRIVATE (PpsCertificateInfo, pps_certificate_info, G_TYPE_OBJECT);
+#define GET_CERTIFICATE_INFO_PRIVATE(o) pps_certificate_info_get_instance_private (o)
+
+static void
+pps_certificate_info_finalize (GObject *object)
+{
+	PpsCertificateInfo *self = PPS_CERTIFICATE_INFO (object);
+	PpsCertificateInfoPrivate *priv = GET_CERTIFICATE_INFO_PRIVATE (self);
+
+	g_clear_pointer (&priv->id, g_free);
+	g_clear_pointer (&priv->subject_common_name, g_free);
+	g_clear_pointer (&priv->subject_email, g_free);
+	g_clear_pointer (&priv->subject_organization, g_free);
+	g_clear_pointer (&priv->issuer_common_name, g_free);
+	g_clear_pointer (&priv->issuer_email, g_free);
+	g_clear_pointer (&priv->issuer_organization, g_free);
+	g_clear_pointer (&priv->issuance_time, g_date_time_unref);
+	g_clear_pointer (&priv->expiration_time, g_date_time_unref);
+
+	G_OBJECT_CLASS (pps_certificate_info_parent_class)->finalize (object);
+}
+
+static void
+pps_certificate_info_set_property (GObject *object,
+                                   guint property_id,
+                                   const GValue *value,
+                                   GParamSpec *param_spec)
+{
+	PpsCertificateInfo *certificate_info = PPS_CERTIFICATE_INFO (object);
+	PpsCertificateInfoPrivate *priv = pps_certificate_info_get_instance_private (certificate_info);
+
+	switch (property_id) {
+	case PROP_ID:
+		g_free (priv->id);
+		priv->id = g_value_dup_string (value);
+		break;
+
+	case PROP_SUBJECT_COMMON_NAME:
+		priv->subject_common_name = g_value_dup_string (value);
+		break;
+
+	case PROP_SUBJECT_EMAIL:
+		priv->subject_email = g_value_dup_string (value);
+		break;
+
+	case PROP_SUBJECT_ORGANIZATION:
+		priv->subject_organization = g_value_dup_string (value);
+		break;
+
+	case PROP_ISSUER_COMMON_NAME:
+		priv->issuer_common_name = g_value_dup_string (value);
+		break;
+
+	case PROP_ISSUER_EMAIL:
+		priv->issuer_email = g_value_dup_string (value);
+		break;
+
+	case PROP_ISSUER_ORGANIZATION:
+		priv->issuer_organization = g_value_dup_string (value);
+		break;
+
+	case PROP_ISSUANCE_TIME:
+		g_clear_pointer (&priv->issuance_time, g_date_time_unref);
+		priv->issuance_time = g_value_get_boxed (value) != NULL ? g_date_time_ref (g_value_get_boxed (value)) : NULL;
+		break;
+
+	case PROP_EXPIRATION_TIME:
+		g_clear_pointer (&priv->expiration_time, g_date_time_unref);
+		priv->expiration_time = g_value_get_boxed (value) != NULL ? g_date_time_ref (g_value_get_boxed (value)) : NULL;
+		break;
+
+	case PROP_STATUS:
+		priv->status = g_value_get_enum (value);
+		break;
+
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object,
+		                                   property_id,
+		                                   param_spec);
+		break;
+	}
+}
+
+static void
+pps_certificate_info_get_property (GObject *object,
+                                   guint property_id,
+                                   GValue *value,
+                                   GParamSpec *param_spec)
+{
+	PpsCertificateInfo *certificate_info = PPS_CERTIFICATE_INFO (object);
+	PpsCertificateInfoPrivate *priv = pps_certificate_info_get_instance_private (certificate_info);
+
+	switch (property_id) {
+	case PROP_ID:
+		g_value_set_string (value, priv->id);
+		break;
+
+	case PROP_SUBJECT_COMMON_NAME:
+		g_value_set_string (value, priv->subject_common_name);
+		break;
+
+	case PROP_SUBJECT_EMAIL:
+		g_value_set_string (value, priv->subject_email);
+		break;
+
+	case PROP_SUBJECT_ORGANIZATION:
+		g_value_set_string (value, priv->subject_organization);
+		break;
+
+	case PROP_ISSUER_COMMON_NAME:
+		g_value_set_string (value, priv->issuer_common_name);
+		break;
+
+	case PROP_ISSUER_EMAIL:
+		g_value_set_string (value, priv->issuer_email);
+		break;
+
+	case PROP_ISSUER_ORGANIZATION:
+		g_value_set_string (value, priv->issuer_organization);
+		break;
+
+	case PROP_ISSUANCE_TIME:
+		g_value_set_boxed (value, g_date_time_ref (priv->issuance_time));
+		break;
+
+	case PROP_EXPIRATION_TIME:
+		g_value_set_boxed (value, g_date_time_ref (priv->expiration_time));
+		break;
+
+	case PROP_STATUS:
+		g_value_set_enum (value, priv->status);
+		break;
+
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object,
+		                                   property_id,
+		                                   param_spec);
+		break;
+	}
+}
+
+static void
+pps_certificate_info_init (PpsCertificateInfo *self)
+{
+	PpsCertificateInfoPrivate *priv = GET_CERTIFICATE_INFO_PRIVATE (self);
+
+	priv->status = PPS_CERTIFICATE_STATUS_NOT_VERIFIED;
+}
+
+static void
+pps_certificate_info_class_init (PpsCertificateInfoClass *klass)
+{
+	GObjectClass *g_object_class = G_OBJECT_CLASS (klass);
+
+	g_object_class->set_property = pps_certificate_info_set_property;
+	g_object_class->get_property = pps_certificate_info_get_property;
+
+	/* Properties */
+	g_object_class_install_property (g_object_class,
+	                                 PROP_ID,
+	                                 g_param_spec_string ("id",
+	                                                      "Id",
+	                                                      "Id of the certificate",
+	                                                      NULL,
+	                                                      G_PARAM_READWRITE |
+	                                                          G_PARAM_CONSTRUCT_ONLY |
+	                                                          G_PARAM_STATIC_STRINGS));
+
+	g_object_class_install_property (g_object_class,
+	                                 PROP_SUBJECT_COMMON_NAME,
+	                                 g_param_spec_string ("subject-common-name",
+	                                                      "Subject's Common Name",
+	                                                      "The name of the entity owning the certificate",
+	                                                      NULL,
+	                                                      G_PARAM_READWRITE |
+	                                                          G_PARAM_CONSTRUCT_ONLY |
+	                                                          G_PARAM_STATIC_STRINGS));
+
+	g_object_class_install_property (g_object_class,
+	                                 PROP_SUBJECT_EMAIL,
+	                                 g_param_spec_string ("subject-email",
+	                                                      "Email",
+	                                                      "The email of the entity that signed the document",
+	                                                      NULL,
+	                                                      G_PARAM_READWRITE |
+	                                                          G_PARAM_CONSTRUCT_ONLY |
+	                                                          G_PARAM_STATIC_STRINGS));
+
+	g_object_class_install_property (g_object_class,
+	                                 PROP_SUBJECT_ORGANIZATION,
+	                                 g_param_spec_string ("subject-organization",
+	                                                      "Organization",
+	                                                      "The organization of the entity that owns the document",
+	                                                      NULL,
+	                                                      G_PARAM_READWRITE |
+	                                                          G_PARAM_CONSTRUCT_ONLY |
+	                                                          G_PARAM_STATIC_STRINGS));
+
+	g_object_class_install_property (g_object_class,
+	                                 PROP_ISSUER_COMMON_NAME,
+	                                 g_param_spec_string ("issuer-common-name",
+	                                                      "Certificate Issuer's Common Name",
+	                                                      "The name of the entity that issued the certificate",
+	                                                      NULL,
+	                                                      G_PARAM_READWRITE |
+	                                                          G_PARAM_CONSTRUCT_ONLY |
+	                                                          G_PARAM_STATIC_STRINGS));
+
+	g_object_class_install_property (g_object_class,
+	                                 PROP_ISSUER_EMAIL,
+	                                 g_param_spec_string ("issuer-email",
+	                                                      "Certificate Issuer's Email",
+	                                                      "The email of the issuer of the certificate",
+	                                                      NULL,
+	                                                      G_PARAM_READWRITE |
+	                                                          G_PARAM_CONSTRUCT_ONLY |
+	                                                          G_PARAM_STATIC_STRINGS));
+
+	g_object_class_install_property (g_object_class,
+	                                 PROP_ISSUER_ORGANIZATION,
+	                                 g_param_spec_string ("issuer-organization",
+	                                                      "Certificate Issuer's Organization Name",
+	                                                      "The name of the organization that issued the certificate",
+	                                                      NULL,
+	                                                      G_PARAM_READWRITE |
+	                                                          G_PARAM_CONSTRUCT_ONLY |
+	                                                          G_PARAM_STATIC_STRINGS));
+
+	g_object_class_install_property (g_object_class,
+	                                 PROP_ISSUANCE_TIME,
+	                                 g_param_spec_boxed ("issuance-time",
+	                                                     "IssuanceTime",
+	                                                     "The time when the certificate was issued",
+	                                                     G_TYPE_DATE_TIME,
+	                                                     G_PARAM_READWRITE |
+	                                                         G_PARAM_CONSTRUCT_ONLY |
+	                                                         G_PARAM_STATIC_STRINGS));
+
+	g_object_class_install_property (g_object_class,
+	                                 PROP_EXPIRATION_TIME,
+	                                 g_param_spec_boxed ("expiration-time",
+	                                                     "ExpirationTime",
+	                                                     "The time when the certificate expires",
+	                                                     G_TYPE_DATE_TIME,
+	                                                     G_PARAM_READWRITE |
+	                                                         G_PARAM_CONSTRUCT_ONLY |
+	                                                         G_PARAM_STATIC_STRINGS));
+
+	g_object_class_install_property (g_object_class,
+	                                 PROP_STATUS,
+	                                 g_param_spec_enum ("status",
+	                                                    "Status",
+	                                                    "Status of the certificate",
+	                                                    PPS_TYPE_CERTIFICATE_STATUS,
+	                                                    PPS_CERTIFICATE_STATUS_NOT_VERIFIED,
+	                                                    G_PARAM_READWRITE |
+	                                                        G_PARAM_CONSTRUCT_ONLY |
+	                                                        G_PARAM_STATIC_STRINGS));
+
+	g_object_class->finalize = pps_certificate_info_finalize;
+}

libdocument/pps-certificate-info.h
@@ -1,0 +1,53 @@
+/* pps-certificate-info.h
+ * this file is part of papers, a gnome document viewer
+ *
+ * Copyright (C) 2024 Jan-Michael Brummer <jan-michael.brummer1@volkswagen.de>
+ * Copyright (C) 2024 Marek Kasik <mkasik@redhat.com>
+ *
+ * Papers is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Papers is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#pragma once
+
+#if !defined(__PPS_PAPERS_DOCUMENT_H_INSIDE__) && !defined(PAPERS_COMPILATION)
+#error "Only <papers-document.h> can be included directly."
+#endif
+
+#include "pps-document.h"
+
+G_BEGIN_DECLS
+
+#define PPS_TYPE_CERTIFICATE_INFO (pps_certificate_info_get_type ())
+
+PPS_PUBLIC
+G_DECLARE_FINAL_TYPE (PpsCertificateInfo, pps_certificate_info, PPS, CERTIFICATE_INFO, GObject);
+
+struct _PpsCertificateInfo {
+	GObject base_instance;
+};
+
+typedef enum {
+	PPS_CERTIFICATE_STATUS_TRUSTED = 0,
+	PPS_CERTIFICATE_STATUS_UNTRUSTED_ISSUER,
+	PPS_CERTIFICATE_STATUS_UNKNOWN_ISSUER,
+	PPS_CERTIFICATE_STATUS_REVOKED,
+	PPS_CERTIFICATE_STATUS_EXPIRED,
+	PPS_CERTIFICATE_STATUS_GENERIC_ERROR,
+	PPS_CERTIFICATE_STATUS_NOT_VERIFIED
+} PpsCertificateStatus;
+
+PPS_PUBLIC
+PpsCertificateInfo *pps_certificate_info_new (const char *id,
+                                              const char *subject_common_name);

libdocument/pps-signature.h
@@ -25,6 +25,7 @@
 #ifndef PPS_SIGNATURE_H
 #define PPS_SIGNATURE_H
 
+#include "pps-certificate-info.h"
 #include "pps-document.h"
 #include <gdk/gdk.h>
 
@@ -35,12 +36,6 @@ G_BEGIN_DECLS
 PPS_PUBLIC
 G_DECLARE_FINAL_TYPE (PpsSignature, pps_signature, PPS, SIGNATURE, GObject);
 
-typedef struct
-{
-	char *id;
-	char *subject_common_name;
-} PpsCertificateInfo;
-
 typedef enum {
 	PPS_SIGNATURE_STATUS_VALID = 0,
 	PPS_SIGNATURE_STATUS_INVALID,
@@ -49,53 +44,11 @@ typedef enum {
 	PPS_SIGNATURE_STATUS_GENERIC_ERROR
 } PpsSignatureStatus;
 
-typedef enum {
-	PPS_CERTIFICATE_STATUS_TRUSTED = 0,
-	PPS_CERTIFICATE_STATUS_UNTRUSTED_ISSUER,
-	PPS_CERTIFICATE_STATUS_UNKNOWN_ISSUER,
-	PPS_CERTIFICATE_STATUS_REVOKED,
-	PPS_CERTIFICATE_STATUS_EXPIRED,
-	PPS_CERTIFICATE_STATUS_GENERIC_ERROR,
-	PPS_CERTIFICATE_STATUS_NOT_VERIFIED
-} PpsCertificateStatus;
-
-/* Certificate Information */
-#define PPS_TYPE_CERTIFICATE_INFO (pps_certificate_info_get_type ())
-
-PPS_PUBLIC
-GType pps_certificate_info_get_type (void) G_GNUC_CONST;
-
-PPS_PUBLIC
-PpsCertificateInfo *pps_certificate_info_new (const char *id,
-                                              const char *subject_common_name);
-
-PPS_PUBLIC
-PpsCertificateInfo *pps_certificate_info_copy (const PpsCertificateInfo *certificate_info);
-
-PPS_PUBLIC
-void pps_certificate_info_free (PpsCertificateInfo *certificate_info);
-
-PPS_PUBLIC
-const char *pps_certificate_info_get_id (const PpsCertificateInfo *certificate_info);
-
-PPS_PUBLIC
-const char *pps_certificate_info_get_subject_common_name (const PpsCertificateInfo *certificate_info);
-
-G_DEFINE_AUTOPTR_CLEANUP_FUNC (PpsCertificateInfo, pps_certificate_info_free)
-
 /* Signature */
 
 struct _PpsSignature {
 	GObject base_instance;
 };
-
-PPS_PUBLIC
-void pps_signature_set_certificate_info (PpsSignature *self,
-                                         const PpsCertificateInfo *certificate);
-
-PPS_PUBLIC
-PpsCertificateInfo *
-pps_signature_get_certificate_info (PpsSignature *self);
 
 PPS_PUBLIC
 void pps_signature_set_destination_file (PpsSignature *self,

commit 2ddb9da9f392dcc3507377d640f4a38710f3785c

libdocument: Don't leak PpsCertificateInfo
Valgrind reports the following memory loss:

```console
==26016== 235 (32 direct, 203 indirect) bytes in 1 blocks are definitely lost in loss record 18,986 of 21,003
==26016==    at 0x6A8647B: g_type_create_instance (gtype.c:1879)
==26016==    by 0x6A6B677: g_object_new_internal.part.0 (gobject.c:2629)
==26016==    by 0x6A6D8FA: UnknownInlinedFun (gobject.c:2946)
==26016==    by 0x6A6D8FA: g_object_new_valist (gobject.c:2968)
==26016==    by 0x6A6DCFF: g_object_new (gobject.c:2441)
==26016==    by 0x21CEE9C4: pdf_document_signatures_get_signatures (../libdocument/backend/pdf/pps-poppler.c:4629)
==26016==    by 0x6C934F1: pps_document_signatures_get_signatures (../libdocument/pps-document-signatures.c:177)
==26016==    by 0x6C2C9F3: pps_job_signatures_run (../libview/pps-jobs.c:1687)
==26016==    by 0x6C2678C: pps_job_run (../libview/pps-job.c:170)
==26016==    by 0x6C2CD5F: pps_job_thread (../libview/pps-job-scheduler.c:74)
==26016==    by 0x6B3F182: g_thread_pool_thread_proxy (gthreadpool.c:336)
==26016==    by 0x6B3D83B: g_thread_proxy (gthread.c:893)
==26016==    by 0x6E829CA: start_thread (pthread_create.c:448)
==26016==
```

Fixes: b8f21fa94885 ("libdocument: Make PpsCertificateInfo a class")

libdocument/backend/pdf/pps-poppler.c
@@ -4559,7 +4559,7 @@ pdf_document_signatures_get_signatures (PpsDocumentSignatures *document)
 
 	for (iter = signature_fields; iter != NULL; iter = iter->next) {
 		PopplerFormField *field = iter->data;
-		PopplerSignatureInfo *info = NULL;
+		g_autoptr (PopplerSignatureInfo) info = NULL;
 		PpsSignature *signature = NULL;
 		PpsSignatureStatus signature_status;
 		PpsCertificateStatus certificate_status;
@@ -4652,7 +4652,6 @@ pdf_document_signatures_get_signatures (PpsDocumentSignatures *document)
 		} else {
 			g_warning ("Could not get certificate info for a signature!");
 		}
-		poppler_signature_info_free (info);
 	}
 
 	g_clear_list (&signature_fields, g_object_unref);

libdocument/pps-signature.c
@@ -456,7 +456,7 @@ gboolean
 pps_signature_is_valid (PpsSignature *self)
 {
 	PpsSignaturePrivate *priv = GET_SIG_PRIVATE (self);
-	PpsCertificateInfo *certificate_info = NULL;
+	g_autoptr (PpsCertificateInfo) certificate_info = NULL;
 	PpsCertificateStatus certificate_status = PPS_CERTIFICATE_STATUS_NOT_VERIFIED;
 
 	g_object_get (self, "certificate-info", &certificate_info, NULL);