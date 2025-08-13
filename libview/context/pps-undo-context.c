GNOME/papers

commit f1f6a696d85cee173550dadfb5565afd09d3922a

libview, shell: add an undo context to undo annotations changes
This comprises several parts:
- an undo context, similar to the annotation one. It keeps an undo and a redo stack of actions.
- an interface UndoHandler which may be implemented by objects that want to register actions to be undoable
- the annotation context implements undoable actions: when an annotation is modified (listened to via
  notify signals) it creates a struct to represent an undoable action that is pushed to the undo context

libview/context/pps-undo-context.c
@@ -1,0 +1,280 @@
+/* pps-undo-context.c
+ *  this file is part of papers, a gnome document viewer
+ *
+ * Copyright (C) 2024 Lucas Baudin
+ *
+ * Papers is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Papers is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "pps-undo-context.h"
+
+/**
+ * PpsUndoContext:
+ *
+ * This context singleton manages two stacks of #PpsUndoAction
+ * that contain actions that may be undone or redone.
+ *
+ * These actions may be added by a number of objects that implement
+ * PpsUndoHandler (e.g. the annotations context). Each handler provides a pointer
+ * to an opaque structure that contains data to undo the action.
+ *
+ * The switch between the undo/redo stacks is done transparently: actions are added
+ * to the redo stack if and only if another action is being undone (i.e.
+ * `pps_undo_context_undo` is being called).
+ */
+
+#ifdef G_LOG_DOMAIN
+#undef G_LOG_DOMAIN
+#endif
+#define G_LOG_DOMAIN "PpsUndoCtx"
+
+typedef enum {
+	PPS_UNDO_CONTEXT_STATE_NORMAL,
+	PPS_UNDO_CONTEXT_STATE_UNDOING,
+	PPS_UNDO_CONTEXT_STATE_REDOING
+} PpsUndoContextState;
+
+struct _PpsUndoContext {
+	GObject parent_instance;
+	GQueue *undo_stack;
+	GQueue *redo_stack;
+	/* This tracks whether the user asked to undo/redo an action
+	or is doing some other normal thing. This is used to add new undoable actions
+	to the correct stack */
+	PpsUndoContextState undo_state;
+	PpsDocumentModel *document_model;
+};
+
+typedef struct {
+	PpsUndoHandler *handler;
+	gpointer data;
+} PpsUndoAction;
+
+enum {
+	PROP_0,
+	PROP_DOCUMENT_MODEL,
+	NUM_PROPERTIES
+};
+G_DEFINE_TYPE (PpsUndoContext, pps_undo_context, G_TYPE_OBJECT)
+
+static void
+pps_undo_action_free (PpsUndoAction *action)
+{
+	pps_undo_handler_free_action (action->handler, action->data);
+	g_object_unref (action->handler);
+	g_free (action);
+}
+
+static void
+pps_undo_context_document_changed (GObject *gobject, GParamSpec *pspec, gpointer user_data)
+{
+	PpsUndoContext *context = PPS_UNDO_CONTEXT (user_data);
+	g_queue_clear_full (context->undo_stack, (GDestroyNotify) pps_undo_action_free);
+	g_queue_clear_full (context->redo_stack, (GDestroyNotify) pps_undo_action_free);
+}
+
+static void
+pps_undo_context_finalize (GObject *object)
+{
+	PpsUndoContext *context = PPS_UNDO_CONTEXT (object);
+	g_queue_clear_full (context->undo_stack, (GDestroyNotify) pps_undo_action_free);
+	g_queue_clear_full (context->redo_stack, (GDestroyNotify) pps_undo_action_free);
+	g_object_unref (context->document_model);
+	G_OBJECT_CLASS (pps_undo_context_parent_class)->finalize (object);
+}
+
+static void
+pps_undo_context_set_property (GObject *object, guint property_id, const GValue *value, GParamSpec *pspec)
+{
+	PpsUndoContext *context = PPS_UNDO_CONTEXT (object);
+
+	switch (property_id) {
+	case PROP_DOCUMENT_MODEL:
+		context->document_model = g_value_dup_object (value);
+		g_signal_connect_object (context->document_model, "notify::document",
+		                         G_CALLBACK (pps_undo_context_document_changed),
+		                         context, G_CONNECT_DEFAULT);
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+		break;
+	}
+}
+
+static void
+pps_undo_context_get_property (GObject *object, guint property_id, GValue *value, GParamSpec *pspec)
+{
+	PpsUndoContext *context = PPS_UNDO_CONTEXT (object);
+
+	switch (property_id) {
+	case PROP_DOCUMENT_MODEL:
+		g_value_set_object (value, context->document_model);
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+		break;
+	}
+}
+
+static void
+pps_undo_context_class_init (PpsUndoContextClass *klass)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+	object_class->finalize = pps_undo_context_finalize;
+	object_class->set_property = pps_undo_context_set_property;
+	object_class->get_property = pps_undo_context_get_property;
+
+	g_object_class_install_property (object_class,
+	                                 PROP_DOCUMENT_MODEL,
+	                                 g_param_spec_object ("document-model",
+	                                                      "Document Model",
+	                                                      "The document model associated with this undo context",
+	                                                      PPS_TYPE_DOCUMENT_MODEL,
+	                                                      G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE));
+}
+
+static void
+pps_undo_context_init (PpsUndoContext *context)
+{
+	context->undo_stack = g_queue_new ();
+	context->redo_stack = g_queue_new ();
+	context->undo_state = PPS_UNDO_CONTEXT_STATE_NORMAL;
+}
+
+PpsUndoContext *
+pps_undo_context_new (PpsDocumentModel *document_model)
+{
+	return g_object_new (PPS_TYPE_UNDO_CONTEXT, "document-model", document_model, NULL);
+}
+
+/**
+ * pps_undo_context_add_action:
+ * @context: a #PpsUndoContext
+ * @handler: an object implementing #PpsUndoHandler
+ * @data: a pointer to a struct that @handler will use to undo an action
+ *
+ * This method adds a new action to the undo (or redo if it is used while
+ * undoing) stack. If it is undone in the future, `pps_undo_handler_undo`
+ * will be called on @handler with @data.
+ */
+void
+pps_undo_context_add_action (PpsUndoContext *context, PpsUndoHandler *handler, gpointer data)
+{
+	g_return_if_fail (PPS_IS_UNDO_CONTEXT (context));
+	g_return_if_fail (PPS_IS_UNDO_HANDLER (handler));
+
+	PpsUndoAction *action = g_new (PpsUndoAction, 1);
+	GQueue *queue;
+
+	if (context->undo_state == PPS_UNDO_CONTEXT_STATE_UNDOING) {
+		queue = context->redo_stack;
+	} else {
+		queue = context->undo_stack;
+	}
+	if (context->undo_state == PPS_UNDO_CONTEXT_STATE_NORMAL) {
+		g_queue_clear_full (context->redo_stack, (GDestroyNotify) pps_undo_action_free);
+		g_debug ("Clearing the redo stack");
+	}
+	action->handler = g_object_ref (handler);
+	action->data = data;
+
+	g_debug ("Adding action to the undo/redo stack");
+	g_queue_push_head (queue, action);
+}
+
+/**
+ * pps_undo_context_undo:
+ * @context: a #PpsUndoContext
+ *
+ * This pops the last action on the undo stack and undoes it by calling the
+ * undo interface method of the `PpsUndoHandler` associated to the action.
+ * While undoing, the `PpsUndoHandler` should add a new action with
+ * `pps_undo_context_add_action` to redo what has just been undone, it will be
+ * added to the redo stack by the undo context.
+ */
+void
+pps_undo_context_undo (PpsUndoContext *context)
+{
+	g_return_if_fail (PPS_IS_UNDO_CONTEXT (context));
+
+	PpsUndoAction *action = g_queue_pop_head (context->undo_stack);
+	if (action) {
+		context->undo_state = PPS_UNDO_CONTEXT_STATE_UNDOING;
+		pps_undo_handler_undo (action->handler, action->data);
+		pps_undo_handler_free_action (action->handler, action->data);
+		g_free (action);
+		context->undo_state = PPS_UNDO_CONTEXT_STATE_NORMAL;
+	} else {
+		g_debug ("Undo stack empty");
+	}
+}
+
+/**
+ * pps_undo_context_redo:
+ * @context: a #PpsUndoContext
+ *
+ * Similar to the undo method, but taking an action from the redo stack.
+ */
+void
+pps_undo_context_redo (PpsUndoContext *context)
+{
+	g_return_if_fail (PPS_IS_UNDO_CONTEXT (context));
+
+	PpsUndoAction *action = g_queue_pop_head (context->redo_stack);
+	if (action) {
+		context->undo_state = PPS_UNDO_CONTEXT_STATE_REDOING;
+		pps_undo_handler_undo (action->handler, action->data);
+		pps_undo_handler_free_action (action->handler, action->data);
+		g_free (action);
+		context->undo_state = PPS_UNDO_CONTEXT_STATE_NORMAL;
+	} else {
+		g_debug ("Redo stack empty");
+	}
+}
+
+/* The next two functions may be used to "squash" actions on the last
+one registered by the context. This is used by the annotations context
+but should be avoided in newly written code, i.e. an action should always
+represent a complete user action. */
+PpsUndoHandler *
+pps_undo_context_get_last_handler (PpsUndoContext *context)
+{
+	PpsUndoAction *last_action;
+
+	if (g_queue_is_empty (context->undo_stack))
+		return NULL;
+
+	last_action = g_queue_peek_head (context->undo_stack);
+	return last_action->handler;
+}
+
+gpointer
+pps_undo_context_get_last_action (PpsUndoContext *context)
+{
+	PpsUndoAction *last_action;
+	GQueue *queue;
+	if (context->undo_state == PPS_UNDO_CONTEXT_STATE_UNDOING) {
+		queue = context->redo_stack;
+	} else {
+		queue = context->undo_stack;
+	}
+
+	if (g_queue_is_empty (queue))
+		return NULL;
+
+	last_action = g_queue_peek_head (queue);
+	return last_action->data;
+}

libdocument/pps-annotation.c
@@ -1824,7 +1824,6 @@ pps_annotation_text_markup_set_property (GObject *object,
                                          GParamSpec *pspec)
 {
 	PpsAnnotationTextMarkup *annot = PPS_ANNOTATION_TEXT_MARKUP (object);
-	PpsAnnotationTextMarkupPrivate *priv = GET_ANNOT_TEXT_MARKUP_PRIVATE (annot);
 
 	switch (prop_id) {
 	case PROP_TEXT_MARKUP_TYPE:

libview/context/pps-annotations-context.c
@@ -40,18 +40,55 @@ static guint signals[N_SIGNALS];
 enum {
 	PROP_0,
 	PROP_DOCUMENT_MODEL,
+	PROP_UNDO_CONTEXT,
 	NUM_PROPERTIES
 };
+
+typedef enum {
+	PPS_ANNOTATIONS_MODIFIED,
+	PPS_ANNOTATIONS_ADDED,
+	PPS_ANNOTATIONS_REMOVED
+} PpsAnnotationsContextUndoableAction;
+
+typedef struct {
+	PpsAnnotationsContextUndoableAction action;
+	/* a union whose value depends on @type. */
+	union {
+		struct {
+			/* list of changed properties, one for every item of @annots */
+			GList *changes;
+		} modified;
+		struct {
+			/* list of initial annots areas (as they may be modified by
+			poppler when added to the doc for instance), one for every
+			item of @annots */
+			GList *areas;
+		} added;
+	};
+	/* list of modified annotations */
+	GList *annots;
+
+	GDateTime *time;
+} PpsAnnotationsContextUndoable;
+
+typedef struct {
+	const gchar *property;
+	GValue value;
+} PpsAnnotationChangedProperty;
 
 typedef struct
 {
 	PpsDocumentModel *model;
 	PpsJobAnnots *job;
+	PpsUndoContext *undo_context;
 
 	GListStore *annots_model;
+	gboolean next_squash_forbidden;
 } PpsAnnotationsContextPrivate;
 
-G_DEFINE_TYPE_WITH_PRIVATE (PpsAnnotationsContext, pps_annotations_context, G_TYPE_OBJECT)
+static void
+pps_annotations_context_undo_handler_init (PpsUndoHandlerInterface *iface);
+G_DEFINE_TYPE_WITH_CODE (PpsAnnotationsContext, pps_annotations_context, G_TYPE_OBJECT, G_ADD_PRIVATE (PpsAnnotationsContext) G_IMPLEMENT_INTERFACE (PPS_TYPE_UNDO_HANDLER, pps_annotations_context_undo_handler_init))
 
 #define GET_PRIVATE(o) pps_annotations_context_get_instance_private (o)
 
@@ -165,6 +202,9 @@ pps_annotations_context_set_property (GObject *object,
 	case PROP_DOCUMENT_MODEL:
 		priv->model = g_value_get_object (value);
 		break;
+	case PROP_UNDO_CONTEXT:
+		priv->undo_context = g_value_get_object (value);
+		break;
 	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
 	}
@@ -202,6 +242,12 @@ pps_annotations_context_class_init (PpsAnnotationsContextClass *klass)
 	                         "The document model",
 	                         PPS_TYPE_DOCUMENT_MODEL,
 	                         G_PARAM_WRITABLE | G_PARAM_CONSTRUCT_ONLY | G_PARAM_STATIC_STRINGS);
+	props[PROP_UNDO_CONTEXT] =
+	    g_param_spec_object ("undo-context",
+	                         "UndoContext",
+	                         "The undo context",
+	                         PPS_TYPE_UNDO_CONTEXT,
+	                         G_PARAM_WRITABLE | G_PARAM_CONSTRUCT_ONLY | G_PARAM_STATIC_STRINGS);
 
 	g_object_class_install_properties (gobject_class, NUM_PROPERTIES, props);
 
@@ -233,10 +279,11 @@ pps_annotations_context_class_init (PpsAnnotationsContextClass *klass)
 }
 
 PpsAnnotationsContext *
-pps_annotations_context_new (PpsDocumentModel *model)
+pps_annotations_context_new (PpsDocumentModel *model, PpsUndoContext *undo_context)
 {
 	return PPS_ANNOTATIONS_CONTEXT (g_object_new (PPS_TYPE_ANNOTATIONS_CONTEXT,
 	                                              "document-model", model,
+	                                              "undo-context", undo_context,
 	                                              NULL));
 }
 
@@ -270,6 +317,227 @@ compare_annot (const PpsAnnotation *a,
 		return 0;
 
 	return index_a > index_b ? 1 : -1;
+}
+
+static void
+pps_annotation_changed_property_free (PpsAnnotationChangedProperty *changed)
+{
+	/* `property` is not owned by the changed property struct, so no need to free it */
+	g_value_unset (&changed->value);
+	g_free (changed);
+}
+
+static void
+pps_annotations_context_free_action (gpointer data)
+{
+	PpsAnnotationsContextUndoable *undoable = data;
+	if (undoable->action == PPS_ANNOTATIONS_MODIFIED) {
+		g_list_free_full (undoable->modified.changes, (GDestroyNotify) pps_annotation_changed_property_free);
+	} else if (undoable->action == PPS_ANNOTATIONS_ADDED) {
+		g_list_free_full (undoable->added.areas, (GDestroyNotify) g_free);
+	}
+	g_list_free_full (undoable->annots, g_object_unref);
+	g_date_time_unref (undoable->time);
+	g_free (undoable);
+}
+
+static gboolean
+pps_annotations_context_try_squash (PpsAnnotationsContext *context, PpsAnnotationsContextUndoable *undoable1, PpsAnnotationsContextUndoable *undoable2)
+{
+	if (g_date_time_difference (undoable2->time, undoable1->time) > 1000000) {
+		return FALSE;
+	}
+
+	if (undoable1->action == PPS_ANNOTATIONS_MODIFIED && undoable2->action == PPS_ANNOTATIONS_MODIFIED) {
+		/* to squash undoable2 into undoable1, we iterate over all changes
+		   of undoable2 and add them to undoable1 if and only if there is no
+		   other related change in undoable1 */
+		GList *annots2 = undoable2->annots;
+		for (GList *c2 = undoable2->modified.changes; c2; c2 = g_list_next (c2)) {
+			PpsAnnotationChangedProperty *data2 = c2->data;
+			gboolean in_undoable1 = FALSE;
+			g_debug ("Squashing %s", data2->property);
+			GList *annots1 = undoable1->annots;
+			for (GList *c1 = undoable1->modified.changes; c1; c1 = g_list_next (c1)) {
+				PpsAnnotationChangedProperty *data1 = c1->data;
+				if (annots2->data == annots1->data && !g_strcmp0 (data1->property, data2->property)) {
+					in_undoable1 = TRUE;
+					break;
+				}
+				annots1 = g_list_next (annots1);
+			}
+			if (!in_undoable1) {
+				undoable1->modified.changes = g_list_prepend (undoable1->modified.changes, data2);
+				undoable1->annots = g_list_prepend (undoable1->annots, annots2->data);
+			} else {
+				pps_annotation_changed_property_free (data2);
+				g_object_unref (G_OBJECT (annots2->data));
+			}
+			annots2 = g_list_next (annots2);
+		}
+		/* we can't use pps_annotations_context_free_action here since
+		   contents of changes of undoable2 must not be freed since
+		   they were copied over to undoable1 */
+		g_list_free (undoable2->modified.changes);
+		g_list_free (undoable2->annots);
+		g_free (undoable2);
+		return TRUE;
+	}
+	return FALSE;
+}
+
+static void
+save_annotation (PpsAnnotationsContext *self,
+                 PpsAnnotation *annot,
+                 GList *changes)
+{
+	PpsAnnotationsContextPrivate *priv = GET_PRIVATE (self);
+	PpsAnnotationsContextUndoable *undoable;
+	gboolean squashed = FALSE;
+
+	/* no change, we can stop here */
+	if (!changes) {
+		g_debug ("No change in annotation");
+		return;
+	}
+
+	undoable = g_new0 (PpsAnnotationsContextUndoable, 1);
+	undoable->time = g_date_time_new_now_local ();
+
+	for (GList *c = changes; c; c = g_list_next (c)) {
+		PpsAnnotationChangedProperty *data = (PpsAnnotationChangedProperty *) c->data;
+		g_debug ("Change in %s", data->property);
+		undoable->annots = g_list_prepend (undoable->annots, g_object_ref (annot));
+	}
+	undoable->action = PPS_ANNOTATIONS_MODIFIED;
+	undoable->modified.changes = changes;
+
+	if (!priv->next_squash_forbidden) {
+		if (pps_undo_context_get_last_handler (priv->undo_context) == PPS_UNDO_HANDLER (self)) {
+			PpsAnnotationsContextUndoable *last_undo = (PpsAnnotationsContextUndoable *) pps_undo_context_get_last_action (priv->undo_context);
+			if (pps_annotations_context_try_squash (self, last_undo, undoable)) {
+				squashed = TRUE;
+			}
+		}
+	}
+	priv->next_squash_forbidden = FALSE;
+	if (!squashed) {
+		pps_undo_context_add_action (priv->undo_context, PPS_UNDO_HANDLER (self), undoable);
+	}
+}
+
+static void
+annot_prop_changed_cb (PpsAnnotation *annot, GParamSpec *pspec, PpsAnnotationsContext *self)
+{
+	PpsAnnotationChangedProperty *changed = g_new0 (PpsAnnotationChangedProperty, 1);
+	changed->property = pspec->name;
+	g_value_init (&changed->value, pspec->value_type);
+	pps_annotation_get_value_last_property (annot, &changed->value);
+
+	save_annotation (self, annot, g_list_append (NULL, changed));
+}
+
+static void
+add_annotation (PpsAnnotationsContext *self, PpsAnnotation *annot)
+{
+	PpsAnnotationsContextPrivate *priv = GET_PRIVATE (self);
+	PpsDocument *document = pps_document_model_get_document (priv->model);
+	PpsAnnotationsContextUndoable *undoable = g_new0 (PpsAnnotationsContextUndoable, 1);
+
+	undoable->action = PPS_ANNOTATIONS_ADDED;
+	undoable->time = g_date_time_new_now_local ();
+	undoable->annots = g_list_append (NULL, g_object_ref (annot));
+	PpsRectangle rect;
+	pps_annotation_get_area (annot, &rect);
+	undoable->added.areas = g_list_append (NULL, pps_rectangle_copy (&rect));
+	pps_undo_context_add_action (priv->undo_context, PPS_UNDO_HANDLER (self), undoable);
+
+	pps_document_annotations_add_annotation (PPS_DOCUMENT_ANNOTATIONS (document), annot);
+
+	g_signal_emit (self, signals[SIGNAL_ANNOT_ADDED], 0, annot);
+
+	g_list_store_insert_sorted (priv->annots_model, annot,
+	                            (GCompareDataFunc) compare_annot, NULL);
+
+	g_signal_connect_data (annot,
+	                       "notify::area",
+	                       G_CALLBACK (annot_prop_changed_cb),
+	                       self, (GClosureNotify) NULL,
+	                       G_CONNECT_DEFAULT);
+	g_signal_connect_data (annot,
+	                       "notify::contents",
+	                       G_CALLBACK (annot_prop_changed_cb),
+	                       self, (GClosureNotify) NULL,
+	                       G_CONNECT_DEFAULT);
+	g_signal_connect_data (annot,
+	                       "notify::rgba",
+	                       G_CALLBACK (annot_prop_changed_cb),
+	                       self, (GClosureNotify) NULL,
+	                       G_CONNECT_DEFAULT);
+	g_signal_connect_data (annot,
+	                       "notify::hidden",
+	                       G_CALLBACK (annot_prop_changed_cb),
+	                       self, (GClosureNotify) NULL,
+	                       G_CONNECT_DEFAULT);
+
+	if (PPS_IS_ANNOTATION_MARKUP (annot)) {
+		g_signal_connect_data (annot,
+		                       "notify::label",
+		                       G_CALLBACK (annot_prop_changed_cb),
+		                       self, (GClosureNotify) NULL,
+		                       G_CONNECT_DEFAULT);
+		g_signal_connect_data (annot,
+		                       "notify::opacity",
+		                       G_CALLBACK (annot_prop_changed_cb),
+		                       self, (GClosureNotify) NULL,
+		                       G_CONNECT_DEFAULT);
+		g_signal_connect_data (annot,
+		                       "notify::rectangle",
+		                       G_CALLBACK (annot_prop_changed_cb),
+		                       self, (GClosureNotify) NULL,
+		                       G_CONNECT_DEFAULT);
+		g_signal_connect_data (annot,
+		                       "notify::popup-is-open",
+		                       G_CALLBACK (annot_prop_changed_cb),
+		                       self, (GClosureNotify) NULL,
+		                       G_CONNECT_DEFAULT);
+	}
+
+	if (PPS_IS_ANNOTATION_TEXT (annot)) {
+		g_signal_connect_data (annot,
+		                       "notify::icon",
+		                       G_CALLBACK (annot_prop_changed_cb),
+		                       self, (GClosureNotify) NULL,
+		                       G_CONNECT_DEFAULT);
+		g_signal_connect_data (annot,
+		                       "notify::is-open",
+		                       G_CALLBACK (annot_prop_changed_cb),
+		                       self, (GClosureNotify) NULL,
+		                       G_CONNECT_DEFAULT);
+	}
+
+	if (PPS_IS_ANNOTATION_TEXT_MARKUP (annot)) {
+		g_signal_connect_data (annot,
+		                       "notify::type",
+		                       G_CALLBACK (annot_prop_changed_cb),
+		                       self, (GClosureNotify) NULL,
+		                       G_CONNECT_DEFAULT);
+	}
+
+#ifdef HAVE_FREE_TEXT
+	if (PPS_IS_ANNOTATION_FREE_TEXT (annot)) {
+		g_signal_connect_data (annot,
+		                       "notify::font-rgba",
+		                       G_CALLBACK (annot_prop_changed_cb),
+		                       self, (GClosureNotify) NULL,
+		                       G_CONNECT_DEFAULT);
+		g_signal_connect_data (annot,
+		                       "notify::font-desc",
+		                       G_CALLBACK (annot_prop_changed_cb),
+		                       self, (GClosureNotify) NULL,
+		                       G_CONNECT_DEFAULT);
+	}
+#endif
 }
 
 /**
@@ -338,12 +606,7 @@ pps_annotations_context_add_annotation_sync (PpsAnnotationsContext *self,
 	              "opacity", 1.0,
 	              NULL);
 
-	pps_document_annotations_add_annotation (PPS_DOCUMENT_ANNOTATIONS (document), annot);
-
-	g_list_store_insert_sorted (priv->annots_model, annot,
-	                            (GCompareDataFunc) compare_annot, NULL);
-
-	g_signal_emit (self, signals[SIGNAL_ANNOT_ADDED], 0, annot);
+	add_annotation (self, annot);
 
 	return annot;
 }
@@ -355,9 +618,15 @@ pps_annotations_context_remove_annotation (PpsAnnotationsContext *self,
 	PpsAnnotationsContextPrivate *priv = GET_PRIVATE (self);
 	PpsDocument *document = pps_document_model_get_document (priv->model);
 	guint position;
+	PpsAnnotationsContextUndoable *undoable;
 
 	g_return_if_fail (PPS_IS_ANNOTATIONS_CONTEXT (self));
 	g_return_if_fail (PPS_IS_ANNOTATION (annot));
+
+	undoable = g_new0 (PpsAnnotationsContextUndoable, 1);
+	undoable->time = g_date_time_new_now_local ();
+	undoable->action = PPS_ANNOTATIONS_REMOVED;
+	undoable->annots = g_list_append (NULL, g_object_ref (annot));
 
 	pps_document_annotations_remove_annotation (PPS_DOCUMENT_ANNOTATIONS (document),
 	                                            annot);
@@ -367,4 +636,53 @@ pps_annotations_context_remove_annotation (PpsAnnotationsContext *self,
 
 	g_list_store_remove (priv->annots_model, position);
 	g_signal_emit (self, signals[SIGNAL_ANNOT_REMOVED], 0, annot);
+
+	g_signal_handlers_disconnect_by_func (annot, annot_prop_changed_cb, self);
+
+	pps_undo_context_add_action (priv->undo_context, PPS_UNDO_HANDLER (self), undoable);
+}
+
+static void
+pps_annotations_context_undo (PpsUndoHandler *self, gpointer data)
+{
+	PpsAnnotationsContextUndoable *undoable = data;
+	PpsAnnotationsContext *context = PPS_ANNOTATIONS_CONTEXT (self);
+	PpsAnnotationsContextPrivate *priv = GET_PRIVATE (context);
+
+	priv->next_squash_forbidden = TRUE;
+
+	switch (undoable->action) {
+	case PPS_ANNOTATIONS_MODIFIED:
+		GList *annots = undoable->annots;
+		for (GList *c = undoable->modified.changes; c; c = g_list_next (c)) {
+			PpsAnnotationChangedProperty *data = (PpsAnnotationChangedProperty *) c->data;
+			g_debug ("Undoing %s", data->property);
+			g_object_set_property (G_OBJECT (annots->data), data->property, &data->value);
+			annots = g_list_next (annots);
+		}
+		break;
+	case PPS_ANNOTATIONS_ADDED:
+		GList *areas = undoable->added.areas;
+		for (GList *a = undoable->annots; a; a = g_list_next (a)) {
+			pps_annotations_context_remove_annotation (context, g_object_ref (a->data));
+			/* the area may be changed by poppler when the annotation
+			   is added to the document, so we restore it now to its initial value */
+			pps_annotation_set_area (PPS_ANNOTATION (a->data), areas->data);
+			areas = g_list_next (areas);
+		}
+		break;
+	case PPS_ANNOTATIONS_REMOVED:
+		for (GList *a = undoable->annots; a; a = g_list_next (a)) {
+			add_annotation (context, g_object_ref (a->data));
+		}
+		break;
+	}
+	priv->next_squash_forbidden = FALSE;
+}
+
+static void
+pps_annotations_context_undo_handler_init (PpsUndoHandlerInterface *iface)
+{
+	iface->undo = pps_annotations_context_undo;
+	iface->free_action = pps_annotations_context_free_action;
 }

libview/context/pps-annotations-context.h
@@ -25,6 +25,7 @@
 #error "Only <papers-view.h> can be included directly."
 #endif
 
+#include "pps-undo-context.h"
 #include <glib-object.h>
 
 #include "pps-document-model.h"
@@ -44,8 +45,8 @@ struct _PpsAnnotationsContextClass {
 	GObjectClass parent_class;
 };
 
-PPS_PUBLIC
-PpsAnnotationsContext *pps_annotations_context_new (PpsDocumentModel *model);
+PPS_PUBLIC PpsAnnotationsContext *
+pps_annotations_context_new (PpsDocumentModel *model, PpsUndoContext *undo_context);
 PPS_PUBLIC
 GListModel *pps_annotations_context_get_annots_model (PpsAnnotationsContext *self);
 PPS_PUBLIC

commit 33fbf40684ea567f1675f5fdf981d613b3705ce5

libview: Don't leak GQueue
Valgrind reports the following memory loss:

```console
==94851== 24 bytes in 1 blocks are definitely lost in loss record 5,366 of 23,707
==94851==    at 0x5A417A8: malloc (vg_replace_malloc.c:446)
==94851==    by 0x6B0DC3A: g_malloc (gmem.c:100)
==94851==    by 0x6B1DE42: g_queue_new (gqueue.c:44)
==94851==    by 0x6C26595: pps_undo_context_init (pps-undo-context.c:152)
==94851==    by 0x6A883B0: g_type_create_instance (gtype.c:1901)
==94851==    by 0x6A6D677: g_object_new_internal.part.0 (gobject.c:2629)
==94851==    by 0x6A6ECE6: UnknownInlinedFun (gobject.c:2626)
==94851==    by 0x6A6ECE6: g_object_new_with_properties (gobject.c:2792)
==94851==    by 0x600DF6D: UnknownInlinedFun (gtkbuilder.c:1012)
==94851==    by 0x600DF6D: builder_construct.lto_priv.0 (gtkbuilderparser.c:474)
==94851==    by 0x601AC5A: end_element (gtkbuilderparser.c:1958)
==94851==    by 0x6004375: proxy_end_element.lto_priv.0 (gtkbuilderparser.c:104)
==94851==    by 0x60131B8: UnknownInlinedFun (gtkbuilderprecompile.c:660)
==94851==    by 0x60131B8: UnknownInlinedFun (gtkbuilderprecompile.c:742)
==94851==    by 0x60131B8: UnknownInlinedFun (gtkbuilderparser.c:191)
==94851==    by 0x60131B8: _gtk_builder_parser_parse_buffer (gtkbuilderparser.c:2222)
==94851==    by 0x6005AAC: gtk_builder_extend_with_template (gtkbuilder.c:1579)
==94851==
==94851== 24 bytes in 1 blocks are definitely lost in loss record 5,367 of 23,707
==94851==    at 0x5A417A8: malloc (vg_replace_malloc.c:446)
==94851==    by 0x6B0DC3A: g_malloc (gmem.c:100)
==94851==    by 0x6B1DE42: g_queue_new (gqueue.c:44)
==94851==    by 0x6C265A2: pps_undo_context_init (pps-undo-context.c:153)
==94851==    by 0x6A883B0: g_type_create_instance (gtype.c:1901)
==94851==    by 0x6A6D677: g_object_new_internal.part.0 (gobject.c:2629)
==94851==    by 0x6A6ECE6: UnknownInlinedFun (gobject.c:2626)
==94851==    by 0x6A6ECE6: g_object_new_with_properties (gobject.c:2792)
==94851==    by 0x600DF6D: UnknownInlinedFun (gtkbuilder.c:1012)
==94851==    by 0x600DF6D: builder_construct.lto_priv.0 (gtkbuilderparser.c:474)
==94851==    by 0x601AC5A: end_element (gtkbuilderparser.c:1958)
==94851==    by 0x6004375: proxy_end_element.lto_priv.0 (gtkbuilderparser.c:104)
==94851==    by 0x60131B8: UnknownInlinedFun (gtkbuilderprecompile.c:660)
==94851==    by 0x60131B8: UnknownInlinedFun (gtkbuilderprecompile.c:742)
==94851==    by 0x60131B8: UnknownInlinedFun (gtkbuilderparser.c:191)
==94851==    by 0x60131B8: _gtk_builder_parser_parse_buffer (gtkbuilderparser.c:2222)
==94851==    by 0x6005AAC: gtk_builder_extend_with_template (gtkbuilder.c:1579)
==94851==
```

Fixes: f1f6a696d85c

libview/context/pps-undo-context.c
@@ -89,8 +89,8 @@ static void
 pps_undo_context_finalize (GObject *object)
 {
 	PpsUndoContext *context = PPS_UNDO_CONTEXT (object);
-	g_queue_clear_full (context->undo_stack, (GDestroyNotify) pps_undo_action_free);
-	g_queue_clear_full (context->redo_stack, (GDestroyNotify) pps_undo_action_free);
+	g_queue_free_full (context->undo_stack, (GDestroyNotify) pps_undo_action_free);
+	g_queue_free_full (context->redo_stack, (GDestroyNotify) pps_undo_action_free);
 	g_object_unref (context->document_model);
 	G_OBJECT_CLASS (pps_undo_context_parent_class)->finalize (object);
 }