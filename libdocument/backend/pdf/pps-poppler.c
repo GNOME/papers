GNOME/papers

commit 5412e0750d1de5f1b7cd620b20b4106b7b0522e6

libdocument: simplify and not leak memory while fetching attachments
The result from poppler_document_get_attachments must be freed!

libdocument/backend/pdf/pps-poppler.c
@@ -3755,20 +3755,17 @@ static GList *
 pdf_document_attachments_get_attachments (PpsDocumentAttachments *document)
 {
 	PdfDocument *self = PDF_DOCUMENT (document);
-	GList *attachments;
 	GList *list;
 	GList *retval = NULL;
 
-	attachments = poppler_document_get_attachments (self->document);
+	list = poppler_document_get_attachments (self->document);
 
-	for (list = attachments; list; list = list->next) {
-		PopplerAttachment *attachment;
+	while (list) {
+		PopplerAttachment *attachment = (PopplerAttachment *) list->data;
 		PpsAttachment *pps_attachment;
 		gchar *data = NULL;
 		gsize size;
 		GError *error = NULL;
-
-		attachment = (PopplerAttachment *) list->data;
 
 		if (attachment_save_to_buffer (attachment, &data, &size, &error)) {
 			GDateTime *mtime, *ctime;
@@ -3791,8 +3788,10 @@ pdf_document_attachments_get_attachments (PpsDocumentAttachments *document)
 			}
 		}
 
-		g_object_unref (attachment);
+		list = list->next;
 	}
+
+	g_list_free_full (list, g_object_unref);
 
 	return g_list_reverse (retval);
 }

commit 3a7d892278a58f2efd2c519720ec5ee31cc528e4

libdocument: Don't leak PpsAttachment list
Valgrind reports the following memory loss:

```console
==22606== 34,985 (24 direct, 34,961 indirect) bytes in 1 blocks are definitely lost in loss record 23,283 of 23,414
==22606==    at 0x5A417A8: malloc (vg_replace_malloc.c:446)
==22606==    by 0x6B0DC3A: g_malloc (gmem.c:100)
==22606==    by 0x6B01B0B: g_list_prepend (glist.c:253)
==22606==    by 0x286C3DF9: poppler_document_get_attachments (poppler-document.cc:874)
==22606==    by 0x28678B05: pdf_document_attachments_get_attachments (pps-poppler.c:4127)
==22606==    by 0x6C91E83: pps_document_attachments_get_attachments (pps-document-attachments.c:39)
==22606==    by 0x6C2B6CE: pps_job_attachments_run (pps-jobs.c:195)
==22606==    by 0x6C29C2D: pps_job_run (pps-job.c:170)
==22606==    by 0x6C2F9F8: pps_job_thread (pps-job-scheduler.c:74)
==22606==    by 0x6B41182: g_thread_pool_thread_proxy (gthreadpool.c:336)
==22606==    by 0x6B3F83B: g_thread_proxy (gthread.c:893)
==22606==    by 0x6E859CA: start_thread (pthread_create.c:448)
==22606==
```

Fixes: 5412e0750d1d ("libdocument: simplify and not leak memory while fetching attachments")

libdocument/backend/pdf/pps-poppler.c
@@ -4119,12 +4119,12 @@ static GList *
 pdf_document_attachments_get_attachments (PpsDocumentAttachments *document)
 {
 	PdfDocument *self = PDF_DOCUMENT (document);
-	GList *list;
+	GList *list, *attachment_list;
 	GList *retval = NULL;
 
 	g_rw_lock_reader_lock (&self->rwlock);
 
-	list = poppler_document_get_attachments (self->document);
+	attachment_list = list = poppler_document_get_attachments (self->document);
 
 	while (list) {
 		PopplerAttachment *attachment = (PopplerAttachment *) list->data;
@@ -4157,7 +4157,7 @@ pdf_document_attachments_get_attachments (PpsDocumentAttachments *document)
 		list = list->next;
 	}
 
-	g_clear_list (&list, g_object_unref);
+	g_clear_list (&attachment_list, g_object_unref);
 
 	g_rw_lock_reader_unlock (&self->rwlock);
 