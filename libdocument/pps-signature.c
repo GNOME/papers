GNOME/papers

commit c9b7bbf6b3adc6c2a24dccb8f9329df2854068f1

libdocument: drop unnecessary pps_rectangle_free
It is just a wrapper around g_free. But since PpsRectangle is a boxed
type, we can just use helpers that unref the object, which indeed
calls g_object_unref

libdocument/pps-signature.c
@@ -71,7 +71,7 @@ pps_signature_finalize (GObject *object)
 	g_clear_pointer (&priv->password, g_free);
 	g_clear_pointer (&priv->signature, g_free);
 	g_clear_pointer (&priv->signature_left, g_free);
-	g_clear_pointer (&priv->rect, pps_rectangle_free);
+	g_clear_object (&priv->rect);
 	g_clear_pointer (&priv->document_owner_password, g_free);
 	g_clear_pointer (&priv->document_user_password, g_free);
 	g_clear_pointer (&priv->signature_time, g_date_time_unref);
@@ -242,7 +242,7 @@ pps_signature_set_rect (PpsSignature *self,
 {
 	PpsSignaturePrivate *priv = GET_SIG_PRIVATE (self);
 
-	g_clear_pointer (&priv->rect, pps_rectangle_free);
+	g_clear_object (&priv->rect);
 	priv->rect = pps_rectangle_copy ((PpsRectangle *) rectangle);
 }
 

libdocument/backend/djvu/djvu-document.c
@@ -676,7 +676,7 @@ djvu_get_selection_region (DjvuDocument *djvu_document,
 	region = cairo_region_create ();
 	for (l = rects; l && l->data; l = g_list_next (l)) {
 		cairo_rectangle_int_t rect;
-		PpsRectangle *r = (PpsRectangle *) l->data;
+		g_autofree PpsRectangle *r = (PpsRectangle *) l->data;
 		gdouble tmp;
 
 		tmp = r->y1;
@@ -690,7 +690,6 @@ djvu_get_selection_region (DjvuDocument *djvu_document,
 		rect.width = (gint) ((r->x2 * scale_x) + 0.5) - rect.x;
 		rect.height = (gint) ((r->y2 * scale_y) + 0.5) - rect.y;
 		cairo_region_union_rectangle (region, &rect);
-		pps_rectangle_free (r);
 	}
 	g_list_free (l);
 
@@ -918,7 +917,7 @@ djvu_document_find_find_text (PpsDocumentFind *document,
 
 	document_get_page_size (djvu_document, page->index, &width, &height, &dpi);
 	for (l = matches; l && l->data; l = g_list_next (l)) {
-		PpsRectangle *r = (PpsRectangle *) l->data;
+		g_autofree PpsRectangle *r = (PpsRectangle *) l->data;
 		gdouble tmp = r->y1;
 
 		r->x1 *= 72.0 / dpi;
@@ -933,7 +932,6 @@ djvu_document_find_find_text (PpsDocumentFind *document,
 		pps_rect->y1 = r->y1;
 		pps_rect->y2 = r->y2;
 
-		pps_rectangle_free (r);
 		l->data = pps_rect;
 	}
 

libdocument/pps-document.c
@@ -918,7 +918,7 @@ pps_document_find_page_by_label (PpsDocument *document,
 }
 
 /* PpsRectangle */
-G_DEFINE_BOXED_TYPE (PpsRectangle, pps_rectangle, pps_rectangle_copy, pps_rectangle_free)
+G_DEFINE_BOXED_TYPE (PpsRectangle, pps_rectangle, pps_rectangle_copy, g_free)
 
 PpsRectangle *
 pps_rectangle_new (void)
@@ -937,12 +937,6 @@ pps_rectangle_copy (PpsRectangle *rectangle)
 	*new_rectangle = *rectangle;
 
 	return new_rectangle;
-}
-
-void
-pps_rectangle_free (PpsRectangle *rectangle)
-{
-	g_free (rectangle);
 }
 
 /* PpsMapping */

libdocument/pps-document.h
@@ -56,7 +56,6 @@ typedef enum {
 } PpsDocumentError;
 
 typedef struct _PpsPoint PpsPoint;
-typedef struct _PpsRectangle PpsRectangle;
 typedef struct _PpsMapping PpsMapping;
 typedef struct _PpsDocumentBackendInfo PpsDocumentBackendInfo;
 
@@ -204,17 +203,14 @@ gboolean pps_document_find_page_by_label (PpsDocument *document,
                                           const gchar *page_label,
                                           gint *page_index);
 
-PPS_PUBLIC
-gint pps_rect_cmp (PpsRectangle *a,
-                   PpsRectangle *b);
-
+/* PpsRectangle */
 #define PPS_TYPE_RECTANGLE (pps_rectangle_get_type ())
-struct _PpsRectangle {
+typedef struct {
 	gdouble x1;
 	gdouble y1;
 	gdouble x2;
 	gdouble y2;
-};
+} PpsRectangle;
 
 PPS_PUBLIC
 GType pps_rectangle_get_type (void) G_GNUC_CONST;
@@ -223,8 +219,9 @@ PpsRectangle *pps_rectangle_new (void);
 PPS_PUBLIC
 PpsRectangle *pps_rectangle_copy (PpsRectangle *pps_rect);
 PPS_PUBLIC
-void pps_rectangle_free (PpsRectangle *pps_rect);
+gint pps_rect_cmp (PpsRectangle *a, PpsRectangle *b);
 
+/* PpsMapping */
 struct _PpsMapping {
 	PpsRectangle area;
 	gpointer data;

commit a43898ec5b8dddd7265e4629ea03078edac73786

libdocument: Fix a potential memory leak in pps_signature_set_rect
The PpsRectangle is not a GObject, freeing it with g_clear_pointer may
lead to a memory leak.

Additionally, remove the redundant code.

Fixes: c9b7bbf6b3ad ("libdocument: drop unnecessary pps_rectangle_free")

libdocument/pps-signature.c
@@ -184,8 +184,6 @@ pps_signature_class_init (PpsSignatureClass *klass)
 	                                                      G_PARAM_READWRITE |
 	                                                          G_PARAM_CONSTRUCT_ONLY |
 	                                                          G_PARAM_STATIC_STRINGS));
-
-	g_object_class->finalize = pps_signature_finalize;
 }
 
 void
@@ -229,7 +227,7 @@ pps_signature_set_rect (PpsSignature *self,
 {
 	PpsSignaturePrivate *priv = GET_SIG_PRIVATE (self);
 
-	g_clear_object (&priv->rect);
+	g_clear_pointer (&priv->rect, g_free);
 	priv->rect = pps_rectangle_copy ((PpsRectangle *) rectangle);
 }
 