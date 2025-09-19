GNOME/papers

commit 4344ffc65aa9a3314ede5656de5c0f7345954d41

Add digital signing support
Add option to sign documents using a smartcard/pkcs 11 certificate.

libdocument/backend/pdf/pps-poppler.c
@@ -51,6 +51,7 @@
 #include "pps-document-print.h"
 #include "pps-document-annotations.h"
 #include "pps-document-attachments.h"
+#include "pps-document-signatures.h"
 #include "pps-document-text.h"
 #include "pps-font-description.h"
 #include "pps-form-field-private.h"
@@ -121,6 +122,7 @@ static void pdf_selection_iface_init                     (PpsSelectionInterface 
 static void pdf_document_page_transition_iface_init      (PpsDocumentTransitionInterface  *iface);
 static void pdf_document_text_iface_init                 (PpsDocumentTextInterface        *iface);
 static int  pdf_document_get_n_pages			 (PpsDocument                     *document);
+static void pdf_document_signatures_iface_init           (PpsDocumentSignaturesInterface  *iface);
 
 static PpsLinkDest *pps_link_dest_from_dest    (PdfDocument       *pdf_document,
 					      PopplerDest       *dest);
@@ -164,7 +166,9 @@ G_DEFINE_TYPE_WITH_CODE (PdfDocument,
 			 G_IMPLEMENT_INTERFACE (PPS_TYPE_DOCUMENT_TRANSITION,
 						pdf_document_page_transition_iface_init)
 			 G_IMPLEMENT_INTERFACE (PPS_TYPE_DOCUMENT_TEXT,
-						pdf_document_text_iface_init));
+						pdf_document_text_iface_init)
+			 G_IMPLEMENT_INTERFACE (PPS_TYPE_DOCUMENT_SIGNATURES,
+						pdf_document_signatures_iface_init));
 
 static void
 pdf_document_dispose (GObject *object)
@@ -4024,4 +4028,172 @@ GType
 pps_backend_query_type (void)
 {
 	return PDF_TYPE_DOCUMENT;
+}
+
+/* Signatures */
+
+static PopplerCertificateInfo *
+find_poppler_certificate_info (PpsCertificateInfo *certificate_info)
+{
+	GList *signing_certificates = poppler_get_available_signing_certificates ();
+	PopplerCertificateInfo *ret = NULL;
+	const char *certificate_id = pps_certificate_info_get_id (certificate_info);
+
+	for (GList *list = signing_certificates; list != NULL && list->data != NULL; list = list->next) {
+		PopplerCertificateInfo *certificate_info = list->data;
+
+		if (g_strcmp0 (certificate_id, poppler_certificate_info_get_id (certificate_info)) == 0) {
+			ret = poppler_certificate_info_copy (certificate_info);
+			break;
+		}
+	}
+
+	g_clear_list (&signing_certificates, (GDestroyNotify) poppler_certificate_info_free);
+
+	return ret;
+}
+
+static void
+pdf_document_signatures_sign (PpsDocumentSignatures *document,
+                              PpsSignature          *signature,
+                              GCancellable          *cancellable,
+                              GAsyncReadyCallback    callback,
+                              gpointer               user_data)
+{
+	PdfDocument *pdf_document = PDF_DOCUMENT (document);
+	PopplerSigningData *signing_data = poppler_signing_data_new ();
+	PopplerCertificateInfo *cert_info = find_poppler_certificate_info (pps_signature_get_certificate_info (signature));
+	PopplerRectangle signing_rect;
+	PpsRectangle *rect;
+	PopplerColor color;
+	gdouble height;
+	GdkRGBA rgba;
+
+	g_assert (cert_info);
+
+	poppler_signing_data_set_certificate_info (signing_data, cert_info);
+	poppler_signing_data_set_page (signing_data, pps_signature_get_page (signature));
+	poppler_signing_data_set_field_partial_name(signing_data, g_uuid_string_random());
+	poppler_signing_data_set_destination_filename (signing_data, pps_signature_get_destination_file (signature));
+
+	if (pps_signature_get_password (signature))
+		poppler_signing_data_set_password (signing_data, pps_signature_get_password (signature));
+
+	poppler_signing_data_set_signature_text (signing_data, pps_signature_get_signature (signature));
+	poppler_signing_data_set_signature_text_left (signing_data, pps_signature_get_signature_left (signature));
+
+	pps_signature_get_font_color (signature, &rgba);
+	color.red = rgba.red * 255;
+	color.green = rgba.green * 255;
+	color.blue = rgba.blue * 255;
+	poppler_signing_data_set_font_color (signing_data, &color);
+
+	pps_signature_get_border_color (signature, &rgba);
+	color.red = rgba.red * 255;
+	color.green = rgba.green * 255;
+	color.blue = rgba.blue * 255;
+	poppler_signing_data_set_border_color (signing_data, &color);
+
+	pps_signature_get_background_color (signature, &rgba);
+	color.red = rgba.red * 255;
+	color.green = rgba.green * 255;
+	color.blue = rgba.blue * 255;
+	poppler_signing_data_set_background_color (signing_data, &color);
+
+	/* TODO: Add auto font calculation once poppler is ready */
+	poppler_signing_data_set_font_size (signing_data, pps_signature_get_font_size (signature));
+	poppler_signing_data_set_left_font_size (signing_data, pps_signature_get_left_font_size (signature));
+	poppler_signing_data_set_border_width (signing_data, pps_signature_get_border_width (signature));
+
+	if (pps_signature_get_owner_password (signature))
+		poppler_signing_data_set_document_owner_password (signing_data, pps_signature_get_owner_password (signature));
+
+	if (pps_signature_get_user_password (signature))
+		poppler_signing_data_set_document_user_password (signing_data, pps_signature_get_user_password (signature));
+
+	pps_document_get_page_size (PPS_DOCUMENT (document), pps_signature_get_page (signature), NULL, &height);
+
+	rect = pps_signature_get_rect (signature);
+	signing_rect.x1 = rect->x1;
+	signing_rect.y1 = height - rect->y1;
+	signing_rect.x2 = rect->x2;
+	signing_rect.y2 = height - rect->y2;
+
+	poppler_signing_data_set_signature_rectangle (signing_data, &signing_rect);
+
+	poppler_document_sign (POPPLER_DOCUMENT (pdf_document->document), signing_data, cancellable, callback, user_data);
+}
+
+static gboolean
+pdf_document_signatures_sign_finish (PpsDocumentSignatures  *document_signatures,
+                                     GAsyncResult           *result,
+                                     GError                **error)
+{
+	PdfDocument *pdf_document = PDF_DOCUMENT (document_signatures);
+
+	return poppler_document_sign_finish (POPPLER_DOCUMENT (pdf_document->document), result, error);
+}
+
+static gboolean
+pdf_document_signatures_can_sign (PpsDocumentSignatures *document)
+{
+	return TRUE;
+}
+
+static void
+pdf_document_set_password_callback (PpsDocumentSignatures        *document,
+				    PpsSignaturePasswordCallback  cb)
+{
+	poppler_set_nss_password_callback (cb);
+}
+
+static GList *
+pdf_document_get_available_signing_certifcates (PpsDocumentSignatures *document)
+{
+	GList *signing_certs = poppler_get_available_signing_certificates();
+	GList *ev_certs = NULL;
+
+	for (GList *list = signing_certs; list != NULL && list->data != NULL; list = list->next) {
+		PopplerCertificateInfo *info = list->data;
+		PpsCertificateInfo *cert_info = pps_certificate_info_new (poppler_certificate_info_get_id (info), poppler_certificate_info_get_subject_common_name (info));
+
+		ev_certs = g_list_append (ev_certs, cert_info);
+	}
+
+	g_clear_list (&signing_certs, (GDestroyNotify) poppler_certificate_info_free);
+
+	return ev_certs;
+}
+
+static PpsCertificateInfo *
+pdf_document_get_certificate_info (PpsDocumentSignatures *document,
+				   const char           *id)
+{
+	g_autolist (PpsCertificateInfo) list = NULL;
+	PpsCertificateInfo *info = NULL;
+
+	if (!id || strlen (id) == 0)
+		return NULL;
+
+	for (list = pdf_document_get_available_signing_certifcates (document); list != NULL && list->data != NULL; list = list->next) {
+		PpsCertificateInfo *cert_info = list->data;
+
+		if (g_strcmp0 (pps_certificate_info_get_id (cert_info), id) == 0) {
+			info = pps_certificate_info_copy (cert_info);
+			break;
+		}
+	}
+
+	return info;
+}
+
+static void
+pdf_document_signatures_iface_init (PpsDocumentSignaturesInterface *iface)
+{
+	iface->set_password_callback = pdf_document_set_password_callback;
+	iface->get_available_signing_certificates = pdf_document_get_available_signing_certifcates;
+	iface->get_certificate_info = pdf_document_get_certificate_info;
+	iface->sign = pdf_document_signatures_sign;
+	iface->sign_finish = pdf_document_signatures_sign_finish;
+	iface->can_sign = pdf_document_signatures_can_sign;
 }

libdocument/meson.build
@@ -19,6 +19,7 @@ headers = files(
   'pps-document-misc.h',
   'pps-document-print.h',
   'pps-document-security.h',
+  'pps-document-signatures.h',
   'pps-document-text.h',
   'pps-document-transition.h',
   'pps-document.h',
@@ -40,6 +41,7 @@ headers = files(
   'pps-portal.h',
   'pps-render-context.h',
   'pps-selection.h',
+  'pps-signature.h',
   'pps-transition-effect.h',
 )
 
@@ -67,6 +69,7 @@ sources = files(
   'pps-document-misc.c',
   'pps-document-print.c',
   'pps-document-security.c',
+  'pps-document-signatures.c',
   'pps-document-text.c',
   'pps-document-transition.c',
   'pps-file-exporter.c',
@@ -86,6 +89,7 @@ sources = files(
   'pps-portal.c',
   'pps-render-context.c',
   'pps-selection.c',
+  'pps-signature.c',
   'pps-transition-effect.c',
   'pps-xmp.c',
   'pps-xmp.h',

libdocument/pps-document-signatures.c
@@ -1,0 +1,158 @@
+/* pps-document-signatures.c
+ * this file is part of papers, a gnome document viewer
+ *
+ * Copyright (C) 2022-2024 Jan-Michael Brummer <jan-michael.brummer1@volkswagen.de>
+ *
+ * Papers is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Papers is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "config.h"
+
+#include "pps-document-signatures.h"
+
+G_DEFINE_INTERFACE (PpsDocumentSignatures, pps_document_signatures, 0)
+
+static void
+pps_document_signatures_default_init (PpsDocumentSignaturesInterface *klass)
+{
+}
+
+/**
+ * pps_document_signatures_set_password_callback:
+ * @document_signatures: an #PpsDocumentSignatures
+ * @cb: (scope call): an #PpsSignaturePasswordCallback
+ *
+ * Popplers crypto backend asks for certificate password before access,
+ * so set a helper function to actually allow the user to enter his password.
+ */
+void
+pps_document_signatures_set_password_callback (PpsDocumentSignatures        *document_signatures,
+                                               PpsSignaturePasswordCallback  cb)
+{
+	PpsDocumentSignaturesInterface *iface = PPS_DOCUMENT_SIGNATURES_GET_IFACE (document_signatures);
+
+	if (iface->set_password_callback)
+		iface->set_password_callback (document_signatures, cb);
+}
+
+/**
+ * pps_document_signatures_get_available_signing_certificates:
+ * @document_signatures: an #PpsDocumentSignatures
+ *
+ * Returns: (transfer full) (element-type PpsCertificateInfo): a list of #PpsCertificateInfo objects or %NULL
+ */
+GList *
+pps_document_signatures_get_available_signing_certificates (PpsDocumentSignatures *document_signatures)
+{
+	PpsDocumentSignaturesInterface *iface = PPS_DOCUMENT_SIGNATURES_GET_IFACE (document_signatures);
+
+	if (iface->get_available_signing_certificates)
+		return iface->get_available_signing_certificates (document_signatures);
+
+	return NULL;
+}
+
+/**
+ * pps_document_signature_get_certificate: (skip)
+ * @document_signatures: an #PpsDocumentSignatures
+ * @nick_name: certificate nick name
+ *
+ * Returns: (transfer full): a new #PpsCertificateInfo, or %NULL
+ */
+PpsCertificateInfo *
+pps_document_signature_get_certificate_info (PpsDocumentSignatures *document_signatures,
+                                             const char            *id)
+{
+	PpsDocumentSignaturesInterface *iface = PPS_DOCUMENT_SIGNATURES_GET_IFACE (document_signatures);
+
+	if (iface->get_certificate_info)
+		return iface->get_certificate_info (document_signatures, id);
+
+	return NULL;
+}
+
+/**
+ * pps_document_signatures_sign:
+ * @document_signatures: an #PpsDocumentSignatures
+ * @signature: a #PpsSignature
+ * @cancellable: a #GCancellable
+ * @callback: a #GAsyncReadyCallback
+ * @user_data: callback user data
+ *
+ * Start the final async process of signing a document using the prepared signature.
+ *
+ * Returns: %TRUE if signing has been started,otherwise %FALSE
+ */
+gboolean
+pps_document_signatures_sign (PpsDocumentSignatures *document_signatures,
+                              PpsSignature          *signature,
+                              GCancellable          *cancellable,
+                              GAsyncReadyCallback    callback,
+                              gpointer               user_data)
+{
+	PpsDocumentSignaturesInterface *iface = PPS_DOCUMENT_SIGNATURES_GET_IFACE (document_signatures);
+
+	if (iface->sign) {
+		iface->sign (document_signatures, signature, cancellable, callback, user_data);
+		return TRUE;
+	}
+
+	return FALSE;
+}
+
+/**
+ * pps_document_signatures_sign_finish:
+ * @document_signatures: a #PpsDocumentSignatures
+ * @result: a #GAsyncResult
+ * @error: a #Gerror
+ *
+ * Finish `pps_document_signatures_sign` operation,
+ *
+ * Returns: %TRUE if signing finish has been started,otherwise %FALSE
+ */
+gboolean
+pps_document_signatures_sign_finish (PpsDocumentSignatures  *document_signatures,
+                                     GAsyncResult           *result,
+                                     GError                **error)
+{
+	PpsDocumentSignaturesInterface *iface = PPS_DOCUMENT_SIGNATURES_GET_IFACE (document_signatures);
+
+	if (iface->sign_finish) {
+		iface->sign_finish (document_signatures, result, error);
+		return TRUE;
+	}
+
+	return FALSE;
+}
+
+/**
+ * pps_document_signatures_can_sign:
+ * @document_signatures: a #PpsDocumentSignatures
+ *
+ * Checks whether document supports digital signing.
+ *
+ * Returns: %TRUE if signing is supported,otherwise %FALSE
+ */
+gboolean
+pps_document_signatures_can_sign (PpsDocumentSignatures *document_signatures)
+{
+	PpsDocumentSignaturesInterface *iface = PPS_DOCUMENT_SIGNATURES_GET_IFACE (document_signatures);
+
+	if (iface->can_sign)
+		return iface->can_sign (document_signatures);
+
+	return FALSE;
+}
+

libdocument/pps-document-signatures.h
@@ -1,0 +1,93 @@
+/* pps-document-signatures.h
+ * this file is part of papers, a gnome document viewer
+ *
+ * Copyright (C) 2022-2024 Jan-Michael Brummer <jan-michael.brummer1@volkswagen.de>
+ *
+ * Papers is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Papers is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#if !defined (__PPS_PAPERS_DOCUMENT_H_INSIDE__) && !defined (PAPERS_COMPILATION)
+#error "Only <ppsince-document.h> can be included directly."
+#endif
+
+#ifndef PPS_DOCUMENT_SIGNATURES_H
+#define PPS_DOCUMENT_SIGNATURES_H
+
+#include <gdk/gdk.h>
+#include "pps-document.h"
+#include "pps-signature.h"
+
+G_BEGIN_DECLS
+
+#define PPS_TYPE_DOCUMENT_SIGNATURES            (pps_document_signatures_get_type ())
+
+PPS_PUBLIC
+G_DECLARE_INTERFACE (PpsDocumentSignatures, pps_document_signatures, PPS, DOCUMENT_SIGNATURES, GObject)
+
+
+typedef char * (*PpsSignaturePasswordCallback)(const gchar *text);
+
+struct _PpsDocumentSignaturesInterface
+{
+	GTypeInterface base_iface;
+
+	/* Methods  */
+	void      (*set_password_callback) (PpsDocumentSignatures        *document_signatures,
+	                                    PpsSignaturePasswordCallback  cb);
+	GList    *(*get_available_signing_certificates) (PpsDocumentSignatures *document_signatures);
+	PpsCertificateInfo *(*get_certificate_info) (PpsDocumentSignatures *document_signatures,
+	                                             const char            *nick_name);
+	void  (*sign) (PpsDocumentSignatures *document_signatures,
+	               PpsSignature          *signature,
+	               GCancellable          *cancellable,
+	               GAsyncReadyCallback    callback,
+	               gpointer               user_data);
+	gboolean (*sign_finish) (PpsDocumentSignatures  *document_signatures,
+	                         GAsyncResult           *result,
+	                         GError                **error);
+	gboolean  (*can_sign) (PpsDocumentSignatures *document_signatures);
+};
+
+PPS_PUBLIC
+GList *pps_document_signatures_get_available_signing_certificates (PpsDocumentSignatures *document_signatures);
+
+PPS_PUBLIC
+void pps_document_signatures_set_password_callback (PpsDocumentSignatures        *document_signatures,
+                                                    PpsSignaturePasswordCallback  cb);
+
+PPS_PUBLIC
+PpsCertificateInfo *pps_document_signature_get_certificate_info (PpsDocumentSignatures *document_signatures,
+                                                                 const char            *id);
+
+PPS_PUBLIC
+gboolean
+pps_document_signatures_sign (PpsDocumentSignatures *document_signatures,
+                              PpsSignature          *signature,
+                              GCancellable          *cancellable,
+                              GAsyncReadyCallback    callback,
+                              gpointer               user_data);
+
+PPS_PUBLIC
+gboolean pps_document_signatures_can_sign (PpsDocumentSignatures *document_signatures);
+
+PPS_PUBLIC
+gboolean
+pps_document_signatures_sign_finish (PpsDocumentSignatures  *document_signatures,
+                                     GAsyncResult           *result,
+                                     GError                **error);
+
+G_END_DECLS
+
+#endif /* PPS_DOCUMENT_SIGNATURES_H */

commit 1ca38c03bef73491517ba0482adb22e0704639d8

libdocument: Don't leak uuid string
Valgrind reports the following memory loss:

```console
==1498935== 37 bytes in 1 blocks are definitely lost in loss record 14,104 of 37,086
==1498935==    at 0x5B3D7A8: malloc (vg_replace_malloc.c:446)
==1498935==    by 0x6DB8A1F: __vasprintf_internal (vasprintf.c:116)
==1498935==    by 0x6BAFB05: UnknownInlinedFun (stdio2.h:275)
==1498935==    by 0x6BAFB05: g_vasprintf (gprintf.c:342)
==1498935==    by 0x6B78A50: g_strdup_vprintf (gstrfuncs.c:515)
==1498935==    by 0x6B78B11: g_strdup_printf (gstrfuncs.c:545)
==1498935==    by 0x6B9BDC0: UnknownInlinedFun (guuid.c:58)
==1498935==    by 0x6B9BDC0: g_uuid_string_random (guuid.c:188)
==1498935==    by 0x145205F5: pdf_document_signatures_sign (pps-poppler.c:4406)
==1498935==    by 0x6CE01FF: pps_document_signatures_sign (pps-document-signatures.c:97)
==1498935==    by 0x4795563: papers_document::auto::document_signatures::DocumentSignaturesExt::sign (document_signatures.rs:135)
==1498935==    by 0x4794BCC: papers_document::auto::document_signatures::DocumentSignaturesExt::sign_future::{{closure}} (document_signatures.rs:151)
==1498935==    by 0x4593030: <gio::gio_future::GioFuture<F,O,T> as core::future::future::Future>::poll (gio_future.rs:80)
==1498935==    by 0x4691F04: <core::pin::Pin<P> as core::future::future::Future>::poll (future.rs:133)
==1498935==
```

Fixes: 4344ffc65aa9 ("Add digital signing support")

libdocument/backend/pdf/pps-poppler.c
@@ -4388,6 +4388,7 @@ pdf_document_signatures_sign (PpsDocumentSignatures *document,
 	g_autoptr (PopplerCertificateInfo) cert_info = NULL;
 	g_autoptr (PpsCertificateInfo) cinfo = NULL;
 	g_autoptr (GTask) task = NULL;
+	g_autofree gchar *uuid = NULL;
 	PopplerRectangle signing_rect;
 	PpsRectangle *rect;
 	PpsPage *page = pps_document_get_page (PPS_DOCUMENT (document),
@@ -4397,13 +4398,14 @@ pdf_document_signatures_sign (PpsDocumentSignatures *document,
 
 	g_rw_lock_writer_lock (&self->rwlock);
 
+	uuid = g_uuid_string_random ();
 	g_object_get (signature, "certificate-info", &cinfo, NULL);
 	cert_info = find_poppler_certificate_info (cinfo);
 	g_assert (cert_info);
 
 	poppler_signing_data_set_certificate_info (signing_data, cert_info);
 	poppler_signing_data_set_page (signing_data, pps_signature_get_page (signature));
-	poppler_signing_data_set_field_partial_name (signing_data, g_uuid_string_random ());
+	poppler_signing_data_set_field_partial_name (signing_data, uuid);
 	poppler_signing_data_set_destination_filename (signing_data, pps_signature_get_destination_file (signature));
 
 	if (pps_signature_get_password (signature))