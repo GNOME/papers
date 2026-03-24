30822c4c0c300416be641cf504d83a609b5f967d
Nelson Benítez León <nbenitezl@gmail.com>
2026-03-24 17:35:51 +0000

    libview: add new function pps_css_utils_filter_class_chars()
    Add new util function for removing from a string
    any character not allowed in a css class name.
    
    We add it as public api because it's used
    by both libview code and Papers app rust code.
    
    This function is needed to fix #655
    
    Part of #655

--- libview/pps-css-utils.h
+++ libview/pps-css-utils.h
@@ -4,8 +4,20 @@
  *
  * Copyright (C) 2025 Lucas Baudin <lbaudin@gnome.org>
  */
+#pragma once
 
+#if !defined(__PPS_PAPERS_VIEW_H_INSIDE__) && !defined(PAPERS_COMPILATION)
+#error "Only <papers-view.h> can be included directly."
+#endif
+
+#include "pps-macros.h"
 #include <pango/pango.h>
 
-gchar *
-dzl_pango_font_description_to_css (const PangoFontDescription *font_desc);
+G_BEGIN_DECLS
+
+gchar *dzl_pango_font_description_to_css (const PangoFontDescription *font_desc);
+
+PPS_PUBLIC
+char *pps_css_utils_filter_class_chars (const char *str);
+
+G_END_DECLS
